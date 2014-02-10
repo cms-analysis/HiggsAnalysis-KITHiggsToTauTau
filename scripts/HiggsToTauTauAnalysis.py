10d1ba33e783d1ce0332c98336e837dc3b156f86
Thomas Müller <tmueller@ekp.uni-karlsruhe.de>
2014-02-10 17:33:39 +0100

    improved exit code handling of artus wrapper

--- scripts/HiggsToTauTauAnalysis.py
+++ scripts/HiggsToTauTauAnalysis.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
 import Artus.Configuration.artusWrapper as artusWrapper
 
 
 if __name__ == "__main__":
-	artusWrapper.artusWrapper("HiggsToTauTauAnalysis")
+	sys.exit(artusWrapper.artusWrapper("HiggsToTauTauAnalysis"))
 
