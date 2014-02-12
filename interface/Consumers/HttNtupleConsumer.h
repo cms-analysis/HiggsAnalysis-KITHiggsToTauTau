d8ba0a8ca679cd8006bfaf20f59589899aaa81e1
Thomas Müller <tmueller@ekp.uni-karlsruhe.de>
2014-02-12 19:05:26 +0100

    adjusted NtupleConsumer to new pointer structure in the product

--- interface/Consumers/HttNtupleConsumer.h
+++ interface/Consumers/HttNtupleConsumer.h
@@ -20,17 +20,13 @@ private:
 	float returnvalue(std::string string, HttEvent const& event,
 			HttProduct const& product ) ARTUS_CPP11_OVERRIDE
 	{
-	
-		/*for(auto muon = event.m_muons->begin(); muon != event.m_muons->end(); muon++) {
-			double pt = muon->p4()->Pt();
-		}*/
 		
 		if(event.m_muons->size() > 1) {
-			if (string == "hardLepPt") return product.m_validMuons.at(0).p4.Pt();
-			else if (string == "hardLepEta") return product.m_validMuons.at(0).p4.Eta();
-			else if (string == "softLepPt") return product.m_validMuons.at(1).p4.Pt();
-			else if (string == "softLepEta") return product.m_validMuons.at(1).p4.Eta();
-			else if (string == "diLepMass") return (product.m_validMuons.at(0).p4 + product.m_validMuons.at(1).p4).mass();
+			if (string == "hardLepPt") return product.m_validMuons.at(0)->p4.Pt();
+			else if (string == "hardLepEta") return product.m_validMuons.at(0)->p4.Eta();
+			else if (string == "softLepPt") return product.m_validMuons.at(1)->p4.Pt();
+			else if (string == "softLepEta") return product.m_validMuons.at(1)->p4.Eta();
+			else if (string == "diLepMass") return (product.m_validMuons.at(0)->p4 + product.m_validMuons.at(1)->p4).mass();
 			else {
 				LOG_FATAL("The quantity " << string << " could not be added to the Ntuple")
 			
