993c00cf74538d4a231df981263be37bebd03819
Raphael <Raphael.Friese@cern.ch>
2014-02-19 16:19:11 +0100

    Change all paths relative to CMSSW_BASE

--- scripts/ini_KITHiggsToTauTauAnalysis.sh
+++ scripts/ini_KITHiggsToTauTauAnalysis.sh
@@ -2,16 +2,15 @@
 
 # set the environment
 export BOOSTPATH=$(ls /afs/cern.ch/cms/${SCRAM_ARCH}/external/boost/* -d | tail -n 1)/
-export ANALYSIS_BASE=$CMSSW_BASE/src/
-export ARTUS_BASE=$ANALYSIS_BASE/Artus
-export KAPPAPATH=$ANALYSIS_BASE/Kappa
-export KAPPATOOLSPATH=$ANALYSIS_BASE/KappaTools
+export ARTUS_BASE=$CMSSW_BASE/src/Artus
+export KAPPAPATH=$CMSSW_BASE/src/Kappa
+export KAPPATOOLSPATH=$CMSSW_BASE/KappaTools
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$EXCALIBUR_BASE:$EXCALIBUR_BASE/../Kappa/lib:$EXCALIBUR_BASE/../KappaTools/lib:$BOOSTPATH/lib
 #export PATH=$EXCALIBUR_BASE/scripts:$PATH
 #export PYTHONPATH=$EXCALIBUR_BASE/plotting:$EXCALIBUR_BASE/plotting/tools:$EXCALIBUR_BASE/cfg/artus:$PYTHONPATH
 export USERPC=`who am i | sed 's/.*(\([^]]*\)).*/\1/g'`
 
 # activate pre-commit hooks
-ln -sf $ANALYSIS_BASE/HiggsAnalysis/KITHiggsToTauTau/scripts/pre-commit  $ANALYSIS_BASE/HiggsAnalysis/KITHiggsToTauTau/.git/hooks/
+ln -sf $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/scripts/pre-commit  $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/.git/hooks/
 
 
