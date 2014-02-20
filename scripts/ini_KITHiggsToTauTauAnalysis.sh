f20dba585b3c426656cdb2a284786183f0eef18f
Thomas Mueller <tmuller@cern.ch>
2014-02-20 14:58:13 +0100

    clean up build files and ini script

--- scripts/ini_KITHiggsToTauTauAnalysis.sh
+++ scripts/ini_KITHiggsToTauTauAnalysis.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # set the environment
-export BOOSTPATH=$(ls /afs/cern.ch/cms/${SCRAM_ARCH}/external/boost/* -d | tail -n 1)/
-export ARTUS_BASE=$CMSSW_BASE/src/Artus
 export KAPPAPATH=$CMSSW_BASE/src/Kappa
 export KAPPATOOLSPATH=$CMSSW_BASE/KappaTools
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$EXCALIBUR_BASE:$EXCALIBUR_BASE/../Kappa/lib:$EXCALIBUR_BASE/../KappaTools/lib:$BOOSTPATH/lib
-#export PATH=$EXCALIBUR_BASE/scripts:$PATH
-#export PYTHONPATH=$EXCALIBUR_BASE/plotting:$EXCALIBUR_BASE/plotting/tools:$EXCALIBUR_BASE/cfg/artus:$PYTHONPATH
+export ARTUSPATH=$CMSSW_BASE/src/Artus
+export KITHIGGSTOTAUTAUPATH=$CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau
+
+# configurations needed for compilation of C++ code
+#export BOOSTPATH=$(ls /afs/cern.ch/cms/${SCRAM_ARCH}/external/boost/* -d | tail -n 1)/
+export LD_LIBRARY_PATH="$CMSSW_BASE/src/KappaTools/lib/:$CMSSW_BASE/src/Kappa/lib/:$LD_LIBRARY_PATH"
+
+# useful to rediect messages
 export USERPC=`who am i | sed 's/.*(\([^]]*\)).*/\1/g'`
 
 # activate pre-commit hooks
 ln -sf $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/scripts/pre-commit  $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/.git/hooks/
-
 
