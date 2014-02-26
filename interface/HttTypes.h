68d4c435d46f6b2b62f6d394836eda55adbb5c2d
Roger <roger.wolf@cern.ch>
2014-02-26 18:54:37 +0100

    added/doxygenized comments

--- interface/HttTypes.h
+++ interface/HttTypes.h
@@ -13,30 +13,43 @@
 #include "HttProduct.h"
 #include "HttPipelineSettings.h"
 
-// all data types which are used for this analysis
+/**
+   HttTypes HiggsAnalysis/KITHiggsToTauTau/interface/HttTypes.h
+
+   Adjusts common Artus tools to the data formats used for Htt specific analyses. These types are 
+   defined in HttTypes. Corresponding Artus templates are then expanded to use the specific types. 
+
+   The final analysis workflow is organized by Producers, Filters and Consumers. Common philosophy 
+   is to keep Consumers free of the analysis logic, which should be implemented in form of Filters 
+   and Producers. Several Consumers could be defined to detemrine what quantities will be available 
+   for plotting after Artus is run.
+*/
+
+/// all data types which are used for Htt analyses
 struct HttTypes {
+        /// content of the kappa ntuple
 	typedef HttEvent event_type;
+        /// objects that have been produced for plotting within Artus (i.e. logic implemented at analysis level)
 	typedef HttProduct product_type;
+        /// configuration settings which are Htt specific? 
 	typedef HttPipelineSettings setting_type;
+        /// global configration settings (which could also be Htt specific?)
 	typedef HttGlobalSettings global_setting_type;
 };
 
-// Pass the template parameters for the Producers
+/// producers of global analysis objects
 typedef GlobalProducerBase<HttTypes> HttGlobalProducerBase;
+/// producers of local analysis objects
 typedef LocalProducerBase<HttTypes> HttLocalProducerBase;
-
-// Pass the template parameters for the Consumer
-typedef ConsumerBase<HttTypes> HttConsumerBase;
-
-// Pass the template parameters for the Filter
+/// event filters at final analysis level
 typedef FilterBase<HttTypes> HttFilterBase;
-
-//Pass the template parameters for the Pipeline
+/// base class for a consumer (for final plotting)
+typedef ConsumerBase<HttTypes> HttConsumerBase;
+/// custom class for a consumer (for final plotting)
+typedef LambdaNtupleConsumer<HttTypes> HttLambdaNtupleConsumer;
+/// custom pipeline (several pipeline of this format could exist)
 typedef Pipeline<HttTypes> HttPipeline;
-
-//Setup our custom pipeline runner
+/// custom pipeline runner (to manage running of pipelines in parallel)
 typedef KappaPipelineRunner<HttTypes, HttPipeline, HttGlobalProducerBase> HttPipelineRunner;
 
-//Setup our custom ntuple producer
-typedef LambdaNtupleConsumer<HttTypes> HttLambdaNtupleConsumer;
 
