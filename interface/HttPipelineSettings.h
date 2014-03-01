9e8f320d22a3b611d3f9d51bd23145597a26b786
Thomas Mueller <tmuller@cern.ch>
2014-03-01 17:05:12 +0100

    remove obsolete config options

--- interface/HttPipelineSettings.h
+++ interface/HttPipelineSettings.h
@@ -6,16 +6,10 @@
 class HttPipelineSettings: public KappaPipelineSettings {
 public:
 	IMPL_SETTING_DEFAULT(std::string, Channel, "")
-
-	IMPL_SETTING_DEFAULT(float, FilterPtLow, 0.0)
-	IMPL_SETTING_DEFAULT(float, FilterPtHigh, 0.0)
-
 };
 
 class HttGlobalSettings: public KappaGlobalSettings {
 public:
-
-	IMPL_SETTING(float, ProducerPtCorrectionFactor)
 	IMPL_SETTING(bool, InputIsData)
 
 };
