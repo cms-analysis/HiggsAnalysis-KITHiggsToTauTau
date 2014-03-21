c8ba152cb3564b891624e3c5a95ca62e7c52bd53
rfriese <raphael.friese@cern.ch>
2014-03-21 23:10:15 +0100

    removed Roger's testing stuff, to be replaced by new producer output

--- src/Consumers/HttLambdaNtupleConsumer.cc
+++ src/Consumers/HttLambdaNtupleConsumer.cc
@@ -14,11 +14,5 @@ HttLambdaNtupleConsumer::HttLambdaNtupleConsumer() : LambdaNtupleConsumerBase<Ht
 	m_valueExtractorMap["diLepMass"] = [](HttEvent const& event, HttProduct const& product) { return (*(product.m_ptOrderedLeptons[0]) + *(product.m_ptOrderedLeptons[1])).mass(); };
 	m_valueExtractorMap["decayChannelIndex"] = [](HttEvent const& event, HttProduct const& product) { return EnumHelper::toUnderlyingValue(product.m_decayChannel); };
 
-	// RW's own stuff for testing
-	m_valueExtractorMap["genTauPt"] = [](HttEvent const& event, HttProduct const& product) { return product.m_genTauDecay.size() > 0 ? product.m_genTauDecay.at(0)->p4.Pt() : UNDEFINED_VALUE; };
-	m_valueExtractorMap["genTauPdgId"] = [](HttEvent const& event, HttProduct const& product) { return product.m_genTauDecay.size() > 0 ? product.m_genTauDecay.at(0)->pdgId() : UNDEFINED_VALUE; };
-	m_valueExtractorMap["genTauStatus"] = [](HttEvent const& event, HttProduct const& product) { return product.m_genTauDecay.size() > 0 ? product.m_genTauDecay.at(0)->status() : UNDEFINED_VALUE; };
-	m_valueExtractorMap["genTauDirDaugs"] = [](HttEvent const& event, HttProduct const& product) { return product.m_genTauDecay.size() > 0 ? product.m_genTauDecay.at(0)->daughterIndices.size() : UNDEFINED_VALUE; };
-	m_valueExtractorMap["genTauAllDaugs"] = [](HttEvent const& event, HttProduct const& product) { return product.m_genTauDecay.size() - 1.; };
 }
 
