4621e9712227f9d0e4f952daf95d5676f9edc61b
rfriese <raphael.friese@cern.ch>
2014-05-10 01:10:39 +0200

    update of remote executable

--- scripts/remoteExecutable.sh
+++ scripts/remoteExecutable.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+##################################################
+# remote executable that is called by grid-control
+# functionality is kept to a minimum. Most of it is
+# done inside the wrapper script.
+##################################################
 #DATASETNICK="DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV"
 #FILE_NAMES="/storage/a/friese/skimming/2014-03-05-sync-exercise/DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV/kappa_DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV_100.root /storage/a/friese/skimming/2014-03-05-sync-exercise/DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV/kappa_DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV_101.root /storage/a/friese/skimming/2014-03-05-sync-exercise/DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV/kappa_DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV_52.root /storage/a/friese/skimming/2014-03-05-sync-exercise/DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV/kappa_DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV_53.root /storage/a/friese/skimming/2014-03-05-sync-exercise/DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV/kappa_DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV_54.root /storage/a/friese/skimming/2014-03-05-sync-exercise/DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV/kappa_DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV_55.root /storage/a/friese/skimming/2014-03-05-sync-exercise/DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV/kappa_DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV_56.root /storage/a/friese/skimming/2014-03-05-sync-exercise/DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV/kappa_DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV_57.root /storage/a/friese/skimming/2014-03-05-sync-exercise/DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV/kappa_DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV_58.root /storage/a/friese/skimming/2014-03-05-sync-exercise/DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV/kappa_DoubleMu_PFembedded_Run2012A_22Jan2013_muelec_8TeV_59.root"
-#CONFIG="../data/ArtusWrapperConfigs/smHtautau.cfg"
-CONFIG="smHtautau.cfg"
+#CONFIG_FILE="../data/ArtusWrapperConfigs/smHtautau.cfg"
 
-COMMAND="HiggsToTauTauAnalysis.py" #Auslesen
+# construct command
+COMMAND=$EXECUTABLE".py" #Auslesen
 COMMAND=$COMMAND" "
-COMMAND=$COMMAND"@"$CONFIG
+COMMAND=$COMMAND"@"$CONFIG_FILE
 COMMAND=$COMMAND" "
 COMMAND=$COMMAND"--nick "$DATASETNICK
 COMMAND=$COMMAND" "
@@ -14,42 +19,24 @@ COMMAND=$COMMAND"-i "${FILE_NAMES//[,\"]/}
 COMMAND=$COMMAND" "
 COMMAND=$COMMAND"-s jobconfig.json"
 
+# debug output that is save in gc.stdout
 echo "-------------------------------------------------"
 echo "Starting main executable with the following command:"
 echo $COMMAND
 echo "The logfile of this job is called "$DATASETNICK"_job_"$MY_JOBID"_log.txt"
 echo "-------------------------------------------------"
 
+# create logfile that will be copied together with the output and pipe everythin into it
 LOGFILE=log.txt
 touch $LOGFILE
 
+#Set searchpath for external libraries # might be replaced by executing a part of the ini scripts
+LD_LIBRARY_PATH="$CMSSW_BASE/src/KappaTools/lib/:$CMSSW_BASE/src/Kappa/lib/:$LD_LIBRARY_PATH"
 
-export LD_LIBRARY_PATH=""$PWD:$LD_LIBRARY_PATH""
-
-echo "command:">>$LOGFILE
+echo "start wrapper with the command:">>$LOGFILE
 eval $COMMAND>>$LOGFILE
-echo $?>>$LOGFILE
-# Execute binary externally
-eval "./HiggsToTauTauAnalysis jobconfig.json"
-
-echo "remoteExecutable.sh finished">>$LOGFILE
+echo "wrapper ended with status:"$?>>$LOGFILE
 
 exit 0
-
-#chmod u+x ini_KITHiggsToTauTauAnalysis.sh
-#eval "./ini_KITHiggsToTauTauAnalysis.sh"
-#Pfad des artus ini-scripts anpassen
-
-#echo "files in this folder $PWD:">>$LOGFILE
-#ls -al>>$LOGFILE
-
-#run ini_KitHiggsToTauTau script?
-#echo "Nick:">>$LOGFILE
-#echo $DATASETNICK>>$LOGFILE
-#echo "Nick without special chars:">>$LOGFILE
-#echo ${DATASETNICK//[,\"]/}>>$LOGFILE
-#echo $MY_JOBID>>$LOGFILE
-#echo "File names:">>$LOGFILE
-#echo $FILE_NAMES>>$LOGFILE
 
 
