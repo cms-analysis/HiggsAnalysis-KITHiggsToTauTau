af0763923b46c62114629672744e83a5f99878a2
Thomas Mueller <tmuller@cern.ch>
2014-05-20 01:51:48 +0200

    HttValidTausProducer: implemented usage of raw discriminators as documented by the Htt group

--- src/Producers/HttValidTausProducer.cc
+++ src/Producers/HttValidTausProducer.cc
@@ -5,11 +5,19 @@
 void HttValidTausProducer::InitGlobal(global_setting_type const& globalSettings)
 {
 	ValidTausProducer::InitGlobal(globalSettings);
+	
+	tauDiscriminatorIsolationCut = globalSettings.GetTauDiscriminatorIsolationCut();
+	tauDiscriminatorAntiElectronMvaCuts = globalSettings.GetTauDiscriminatorAntiElectronMvaCuts();
+	tauDiscriminatorAntiElectronMvaCutsLeptonIndices = globalSettings.GetTauDiscriminatorAntiElectronMvaCutsLeptonIndices();
 }
 
 void HttValidTausProducer::InitLocal(setting_type const& settings)
 {
 	ValidTausProducer::InitLocal(settings);
+	
+	tauDiscriminatorIsolationCut = settings.GetTauDiscriminatorIsolationCut();
+	tauDiscriminatorAntiElectronMvaCuts = settings.GetTauDiscriminatorAntiElectronMvaCuts();
+	tauDiscriminatorAntiElectronMvaCutsLeptonIndices = settings.GetTauDiscriminatorAntiElectronMvaCutsLeptonIndices();
 }
 
 bool HttValidTausProducer::AdditionalCriteria(KDataPFTau* tau,
@@ -17,6 +25,26 @@ bool HttValidTausProducer::AdditionalCriteria(KDataPFTau* tau,
                                               product_type& product) const
 {
 	bool validTau = ValidTausProducer::AdditionalCriteria(tau, event, product);
+	
+	// custom isolation cut
+	validTau = validTau && tau->getDiscriminator("hpsPFTauDiscriminationByRawCombinedIsolationDBSumPtCorr3Hits", event.m_tauDiscriminatorMetadata) < tauDiscriminatorIsolationCut;
+	
+	// custom electron rejection
+	if (validTau && (! tauDiscriminatorAntiElectronMvaCuts.empty())) {
+		if(tauDiscriminatorAntiElectronMvaCutsLeptonIndices.empty()) {
+			validTau = validTau && tau->getDiscriminator("hpsPFTauDiscriminationByMVA3rawElectronRejection", event.m_tauDiscriminatorMetadata) > tauDiscriminatorAntiElectronMvaCuts[tau->getDiscriminator("hpsPFTauDiscriminationByMVA3rawElectronRejectioncategory", event.m_tauDiscriminatorMetadata)];
+		}
+		else {
+			int currentTauIndex = product.m_validTaus.size();
+			for (std::vector<int>::const_iterator tauIndex = tauDiscriminatorAntiElectronMvaCutsLeptonIndices.begin();
+			     tauIndex != tauDiscriminatorAntiElectronMvaCutsLeptonIndices.end(); ++tauIndex)
+			{
+				if (currentTauIndex == *tauIndex) {
+					validTau = validTau && tau->getDiscriminator("hpsPFTauDiscriminationByMVA3rawElectronRejection", event.m_tauDiscriminatorMetadata) > tauDiscriminatorAntiElectronMvaCuts[tau->getDiscriminator("hpsPFTauDiscriminationByMVA3rawElectronRejectioncategory", event.m_tauDiscriminatorMetadata)];
+				}
+			}
+		}
+	}
 	
 	return validTau;
 }
