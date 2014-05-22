12931f7ff2fd24850072d98543e358e49df4c26d
Thomas Mueller <tmuller@cern.ch>
2014-05-22 13:43:49 +0200

    plotting: started to introduce official HttStyles

--- python/plotting/plotroothtt.py
+++ python/plotting/plotroothtt.py
@@ -14,5 +14,18 @@ import Artus.HarryPlotter.plotroot as plotroot
 
 
 class PlotRootHtt(plotroot.PlotRoot):
-	pass
+	def __init__(self):
+		plotroot.PlotRoot.__init__(self)
+		
+		# load HttStyles
+		ROOT.gROOT.LoadMacro(os.path.expandvars("$CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/src/HttStyles.cc")+"+")
+	
+	def run(self, plotData):
+		# apply HttStyles
+		ROOT.SetStyle()
+		
+		plotroot.PlotRoot.run(self, plotData)
+
+	def create_canvas(self, plotData):
+		self.canvas = ROOT.MakeCanvas("canvas", "")
 
