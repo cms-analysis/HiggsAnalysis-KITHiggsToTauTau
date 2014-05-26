07459b1407c3cbb712126bd205f13e8e061c26f7
Thomas Mueller <tmuller@cern.ch>
2014-05-26 15:16:04 +0200

    plotting: implemented plotting of ratios

--- python/plotting/plotroothtt.py
+++ python/plotting/plotroothtt.py
@@ -33,8 +33,22 @@ class PlotRootHtt(plotroot.PlotRoot):
 
 	def create_canvas(self, plotData):
 		self.canvas = ROOT.MakeCanvas("canvas", "")
+		
+		if plotData.plotdict["ratio"]:
+			plot_ratio_slider_y = 0.35
+			self.canvas.cd()
+			self.plot_pad = ROOT.TPad("plot_pad", "", 0.0, plot_ratio_slider_y, 1.0, 1.0)
+			self.ratio_pad = ROOT.TPad("ratio_pad", "", 0.0, 0.0, 1.0, plot_ratio_slider_y)
+			self.plot_pad.SetNumber(1)
+			self.ratio_pad.SetNumber(2)
+			self.plot_pad.Draw()
+			self.ratio_pad.Draw()
+			ROOT.InitSubPad(self.canvas, 1)
+			ROOT.InitSubPad(self.canvas, 2)
+   		
+		plotroot.PlotRoot.create_canvas(self, plotData)
 
 	def prepare_histograms(self, plotData):
-		for root_histogram in plotData.plotdict["root_histos"].values():
+		for root_histogram in plotData.plotdict["root_histos"].values() + plotData.plotdict["root_ratio_histos"]:
 			ROOT.InitHist(root_histogram, root_histogram.GetTitle())
 
