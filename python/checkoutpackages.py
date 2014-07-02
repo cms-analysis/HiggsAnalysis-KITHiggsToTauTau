0d879cc5628e3d63bfd31313b99bbcf3110c1c99
Thomas Mueller <tmuller@cern.ch>
2014-07-02 15:43:02 +0200

    added checkout scripts

---
+++ python/checkoutpackages.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import os
+import sys
+
+import Artus.KappaAnalysis.checkoutpackages as checkoutpackages
+
+
+def checkout_packages(max_n_trials=2):
+	commands = [
+		"cd $CMSSW_BASE/src",
+		
+		# needed by the JetEnergyCorrectionProducer
+		"git clone https://github.com:veelken/SVfit_standalone.git TauAnalysis/SVfitStandalone",
+	]
+	checkoutpackages.execute_commands(commands, max_n_trials=max_n_trials)
+
