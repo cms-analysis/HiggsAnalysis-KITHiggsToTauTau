064acd289e957568085aabb53fb657856b844265
Thomas Mueller <tmuller@cern.ch>
2014-07-12 01:00:24 +0200

    DecayChannelFilter: small improvements

--- interface/Filters/DecayChannelFilter.h
+++ interface/Filters/DecayChannelFilter.h
@@ -1,10 +1,16 @@
 
 #pragma once
 
-#include "Artus/Core/interface/Cpp11Support.h"
+#include "Artus/Core/interface/FilterBase.h"
 
-#include "../HttTypes.h"
+#include "HiggsAnalysis/KITHiggsToTauTau/interface/HttTypes.h"
+#include "HiggsAnalysis/KITHiggsToTauTau/interface/HttEnumTypes.h"
 
+
+/** Filter for the decay channel.
+ *  Required config tag:
+ *  - Channel
+ */
 class DecayChannelFilter: public FilterBase<HttTypes> {
 public:
 
@@ -15,9 +21,16 @@ public:
 	virtual std::string GetFilterId() const ARTUS_CPP11_OVERRIDE {
             return "decay_channel";
     }
+    
+    virtual void Init(setting_type const& settings) ARTUS_CPP11_OVERRIDE;
 
 	virtual bool DoesEventPass(event_type const& event, product_type const& product,
 	                           setting_type const& settings) const ARTUS_CPP11_OVERRIDE;
+
+
+private:
+	HttEnumTypes::DecayChannel m_decayChannel;
+
 };
 
 
