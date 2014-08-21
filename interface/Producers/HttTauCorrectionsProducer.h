2c5c3618bb31474f64c0295c2718753c5196cba6
Felix <felix.frensch2@kit.edu>
2014-08-21 17:40:25 +0200

    tau energy correction for new tau ID included

--- interface/Producers/HttTauCorrectionsProducer.h
+++ interface/Producers/HttTauCorrectionsProducer.h
@@ -29,6 +29,7 @@ public:
 	static TauEnergyCorrection ToTauEnergyCorrection(std::string const& tauEnergyCorrection)
 	{
 		if (tauEnergyCorrection == "summer2013") return TauEnergyCorrection::SUMMER2013;
+		else if(tauEnergyCorrection == "newTauID") return TauEnergyCorrection::NEWTAUID;
 		else return TauEnergyCorrection::NONE;
 	}
 	
