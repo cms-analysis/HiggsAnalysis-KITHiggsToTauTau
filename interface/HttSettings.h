78907705878ee5fb5d8b051110a239c738709d79
fcolombo <fabio.colombo@cern.ch>
2014-10-14 13:59:41 +0200

    TTH: jet definitions and jet-tau overlap removal

--- interface/HttSettings.h
+++ interface/HttSettings.h
@@ -154,6 +154,7 @@ public:
 	IMPL_SETTING_DEFAULT(float, TauElectronLowerDeltaRCut, 0.5);
 	IMPL_SETTING_DEFAULT(float, TauMuonLowerDeltaRCut, 0.5);
 	IMPL_SETTING_DEFAULT(float, TauTauLowerDeltaRCut, 0.5);
+	IMPL_SETTING_DEFAULT(float, JetTauLowerDeltaRCut, 0.5);
 	
 	IMPL_SETTING(std::string, TauSpinnerSettingsPDF);
 	IMPL_SETTING(float, TauSpinnerSettingsCmsEnergy);
--- src/Producers/HttValidJetsProducer.cc
+++ src/Producers/HttValidJetsProducer.cc
@@ -17,6 +17,17 @@ bool HttValidTaggedJetsProducer::AdditionalCriteria(KDataPFTaggedJet* jet,
                                                     setting_type const& settings) const
 {
 	bool validJet = ValidTaggedJetsProducer::AdditionalCriteria(jet, event, product, settings);
+	
+	HttSettings const& specSettings = static_cast<HttSettings const&>(settings);
+	HttProduct const& specProduct = static_cast<HttProduct const&>(product);
+	
+	// remove taus from list of jets via simple DeltaR isolation
+	// (targeted at ttH analysis, harmless if m_validTTHTaus is not filled)
+	for (std::vector<KDataPFTau*>::const_iterator tau = specProduct.m_validTTHTaus.begin();
+		validJet && tau != specProduct.m_validTTHTaus.end(); ++tau)
+		{
+			validJet = validJet && ROOT::Math::VectorUtil::DeltaR(jet->p4, (*tau)->p4) > specSettings.GetJetTauLowerDeltaRCut();
+		}
 
 	return validJet;
 }
