3de724face64e482a388ae4ec3bc2971ca0c4735
Thomas Mueller <tmuller@cern.ch>
2014-11-12 15:27:00 +0100

    estimateztt.py: some generalisations

--- python/plotting/modules/estimateztt.py
+++ python/plotting/modules/estimateztt.py
@@ -20,28 +20,39 @@ class EstimateZtt(estimatebase.EstimateBase):
 		super(EstimateZtt, self).modify_argument_parser(parser, args)
 		
 		self.estimate_ztt_options = parser.add_argument_group("ZTT estimation options")
-		self.estimate_ztt_options.add_argument("--ztt-from-mc", action="store_true", default=False,
+		self.estimate_ztt_options.add_argument("--ztt-from-mc", nargs="+", type="bool", default=False,
 		                                       help="Estimate ZTT from MC samples.")
+		self.estimate_ztt_options.add_argument("--ztt-plot-nicks", nargs="+", default=["ztt"],
+		                                       help="Nicks for histogram to plot. [Default: %(default)s]")
+		self.estimate_ztt_options.add_argument("--ztt-mc-inc-nicks", nargs="+", default=["noplot_ztt_mc_inc"],
+		                                       help="Nicks for inclusive MC histogram. [Default: %(default)s]")
+		self.estimate_ztt_options.add_argument("--ztt-emb-inc-nicks", nargs="+", default=["noplot_ztt_emb_inc"],
+		                                       help="Nicks for inclusive embedding histogram. [Default: %(default)s]")
 
 	def prepare_args(self, parser, plotData):
 		super(EstimateZtt, self).prepare_args(parser, plotData)
 		
+		self._plotdict_keys = ["ztt_from_mc", "ztt_plot_nicks", "ztt_mc_inc_nicks", "ztt_emb_inc_nicks"]
+		self.prepare_list_args(plotData, self._plotdict_keys)
+		
 		# make sure that all necessary histograms are available
-		assert isinstance(plotData.plotdict["root_objects"].get("ztt"), ROOT.TH1)
-		if not plotData.plotdict["ztt_from_mc"]:
-			assert isinstance(plotData.plotdict["root_objects"].get("noplot_ztt_mc_inc"), ROOT.TH1)
-			assert isinstance(plotData.plotdict["root_objects"].get("noplot_ztt_emb_inc"), ROOT.TH1)
+		for ztt_from_mc, ztt_plot_nick, ztt_mc_inc_nick, ztt_emb_inc_nick in zip(*[plotData.plotdict[key] for key in self._plotdict_keys]):
+			assert isinstance(plotData.plotdict["root_objects"].get(ztt_plot_nick), ROOT.TH1)
+			if not ztt_from_mc:
+				assert isinstance(plotData.plotdict["root_objects"].get(ztt_mc_inc_nick), ROOT.TH1)
+				assert isinstance(plotData.plotdict["root_objects"].get(ztt_emb_inc_nick), ROOT.TH1)
 	
 	def run(self, plotData=None):
 		super(EstimateZtt, self).run(plotData)
 		
-		if not plotData.plotdict["ztt_from_mc"]:
-			yield_inclusive_mc = plotData.plotdict["root_objects"]["noplot_ztt_mc_inc"].Integral()
-			yield_inclusive_emb = plotData.plotdict["root_objects"]["noplot_ztt_emb_inc"].Integral()
+		for ztt_from_mc, ztt_plot_nick, ztt_mc_inc_nick, ztt_emb_inc_nick in zip(*[plotData.plotdict[key] for key in self._plotdict_keys]):
+			if not ztt_from_mc:
+				yield_inclusive_mc = plotData.plotdict["root_objects"][ztt_mc_inc_nick].Integral()
+				yield_inclusive_emb = plotData.plotdict["root_objects"][ztt_emb_inc_nick].Integral()
 			
-			assert yield_inclusive_emb != 0.0
-			plotData.plotdict["root_objects"]["ztt"].Scale(yield_inclusive_mc / yield_inclusive_emb)
-		
-		else:
-			pass # TODO
+				assert yield_inclusive_emb != 0.0
+				plotData.plotdict["root_objects"][ztt_plot_nick].Scale(yield_inclusive_mc / yield_inclusive_emb)
+			
+			else:
+				pass # TODO
 
