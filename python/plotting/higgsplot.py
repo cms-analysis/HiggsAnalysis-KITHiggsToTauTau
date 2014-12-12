621a34b073e48fd395236ee01045239c7eff6b81
Thomas Mueller <tmuller@cern.ch>
2014-12-12 18:03:44 +0100

    higgsplot: implemented support for multiplot functionalilty

--- python/plotting/higgsplot.py
+++ python/plotting/higgsplot.py
@@ -5,21 +5,20 @@ import logging
 import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
+import Artus.HarryPlotter.harry as harry
 import Artus.HarryPlotter.core as harrycore
 
 
-def higgs_plot(args_from_script = None):
-	"""
-	Main plotting function
+class HiggsPlotter(harry.HarryPlotter):
+
+	def __init__(self, plots=None, n_processes=4):
+		super(HiggsPlotter, self).__init__(plots, n_processes=n_processes)
 	
-	Can be called from within python scripts by passing the arguments as a string
-	as it would be done by calling this script in the bash.
-	"""
-	
-	harry_core = harrycore.HarryCore(args_from_script=args_from_script)
-	
-	harry_core.register_modules_dir("$CMSSW_BASE/python/HiggsAnalysis/KITHiggsToTauTau/plotting/modules/analysis_modules")
-	harry_core.register_modules_dir("$CMSSW_BASE/python/HiggsAnalysis/KITHiggsToTauTau/plotting/modules/plot_modules")
-	
-	harry_core.run()
+	def plot(self, harry_args):
+		harry_core = harrycore.HarryCore(args_from_script=harry_args)
+		
+		harry_core.register_modules_dir("$CMSSW_BASE/python/HiggsAnalysis/KITHiggsToTauTau/plotting/modules/analysis_modules")
+		harry_core.register_modules_dir("$CMSSW_BASE/python/HiggsAnalysis/KITHiggsToTauTau/plotting/modules/plot_modules")
+		
+		harry_core.run()
 
