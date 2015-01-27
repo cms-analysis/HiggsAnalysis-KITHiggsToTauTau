38b0b838fdfa53eaf90d8b90bb577fddf2630dd7
Thomas Mueller <tmuller@cern.ch>
2015-01-27 16:54:00 +0100

    mt.py: added option to swich of separate contributions

--- python/plotting/configs/mt.py
+++ python/plotting/configs/mt.py
@@ -4,56 +4,64 @@
 import Artus.Utility.jsonTools as jsonTools
 
 class MT(object):
-	def __init__(self):
+	def __init__(self, add_data=True, add_ztt=True, add_zl=True, add_zj=True, add_ttj=True, add_diboson=True, add_wjets=True, add_qcd=True):
 		self.config = jsonTools.JsonDict({})
 		
 		# Data
-		self.add_input("Tau*_Run2012?_22Jan2013_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "data")
+		if add_data:
+			self.add_input("Tau*_Run2012?_22Jan2013_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "data")
 		
 		# DY->tautau
-		self.add_input("*_PFembedded_Run2012?_22Jan2013_mt_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "ztt")
-		self.add_input("*_PFembedded_Run2012?_22Jan2013_mt_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_ztt_emb_inc")
-		self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_ztt_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_ztt_mc_inc")
-		self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_ztt_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_ztt_mc")
+		if add_ztt:
+			self.add_input("*_PFembedded_Run2012?_22Jan2013_mt_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "ztt")
+			self.add_input("*_PFembedded_Run2012?_22Jan2013_mt_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_ztt_emb_inc")
+			self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_ztt_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_ztt_mc_inc")
+			self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_ztt_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_ztt_mc")
 		
 		# ZL
-		self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_zl_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "zl")
+		if add_zl:
+			self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_zl_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "zl")
 		
 		# ZJ
-		self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_zj_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "zj")
+		if add_zj:
+			self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_zj_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "zj")
 		
 		# TTJets
-		self.add_input("TTJets*_madgraph_tauola_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "ttj")
+		if add_ttj:
+			self.add_input("TTJets*_madgraph_tauola_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "ttj")
 		
 		# Dibosons
-		self.add_input("??_pythia_tauola_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "diboson")
+		if add_diboson:
+			self.add_input("??_pythia_tauola_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "diboson")
 		
 		# WJets
-		self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "wjets")
-		self.add_input("Tau*_Run2012?_22Jan2013_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_wjets_data_control")
-		self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_ztt_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_ztt_mc_wjet_control")
-		self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_zl_tauEsNom/ntuple mt_os_dirIso_zj_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_zll_wjet_control")
-		self.add_input("TTJets*_madgraph_tauola_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_ttj_wjet_control")
-		self.add_input("??_pythia_tauola_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_diboson_wjet_control")
-		self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_wjets_mc_signal")
-		self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_wjets_mc_control")
+		if add_wjets:
+			self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "wjets")
+			self.add_input("Tau*_Run2012?_22Jan2013_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_wjets_data_control")
+			self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_ztt_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_ztt_mc_wjet_control")
+			self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_os_dirIso_zl_tauEsNom/ntuple mt_os_dirIso_zj_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_zll_wjet_control")
+			self.add_input("TTJets*_madgraph_tauola_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_ttj_wjet_control")
+			self.add_input("??_pythia_tauola_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_diboson_wjet_control")
+			self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_wjets_mc_signal")
+			self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_os_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_wjets_mc_control")
 		
-		# WJets (SS, for substraction in QCD estimation)
-		self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_wjets_ss")
-		self.add_input("Tau*_Run2012?_22Jan2013_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_wjets_ss_data_control")
-		self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_ss_dirIso_ztt_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_ztt_ss_mc_wjet_control")
-		self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_ss_dirIso_zl_tauEsNom/ntuple mt_ss_dirIso_zj_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_zll_ss_wjet_control")
-		self.add_input("TTJets*_madgraph_tauola_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_ttj_ss_wjet_control")
-		self.add_input("??_pythia_tauola_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_diboson_ss_wjet_control")
-		self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_wjets_ss_mc_signal")
-		self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_wjets_ss_mc_control")
+		if add_qcd:
+			# WJets (SS, for substraction in QCD estimation)
+			self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_wjets_ss")
+			self.add_input("Tau*_Run2012?_22Jan2013_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_wjets_ss_data_control")
+			self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_ss_dirIso_ztt_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_ztt_ss_mc_wjet_control")
+			self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_ss_dirIso_zl_tauEsNom/ntuple mt_ss_dirIso_zj_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_zll_ss_wjet_control")
+			self.add_input("TTJets*_madgraph_tauola_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_ttj_ss_wjet_control")
+			self.add_input("??_pythia_tauola_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_diboson_ss_wjet_control")
+			self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_wjets_ss_mc_signal")
+			self.add_input("WJetsToLN_madgraph_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt>70.0)", "noplot_wjets_ss_mc_control")
 		
-		# QCD
-		self.add_input("Tau*_Run2012?_22Jan2013_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "qcd")
-		self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_ss_dirIso_ztt_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_ztt_mc_qcd_control")
-		self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_ss_dirIso_zl_tauEsNom/ntuple mt_ss_dirIso_zj_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_zll_qcd_control")
-		self.add_input("TTJets*_madgraph_tauola_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_ttj_qcd_control")
-		self.add_input("??_pythia_tauola_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_diboson_qcd_control")
+			# QCD
+			self.add_input("Tau*_Run2012?_22Jan2013_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 1.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "qcd")
+			self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_ss_dirIso_ztt_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_ztt_mc_qcd_control")
+			self.add_input("DYJetsToLL_M_50_madgraph_8TeV/*.root", "mt_ss_dirIso_zl_tauEsNom/ntuple mt_ss_dirIso_zj_tauEsNom/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_zll_qcd_control")
+			self.add_input("TTJets*_madgraph_tauola_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_ttj_qcd_control")
+			self.add_input("??_pythia_tauola_8TeV/*.root", "mt_ss_dirIso_z_tauEs/ntuple", 19712.0, "eventWeight*(pt_2>30.0)*(lep1MetMt<30.0)", "noplot_diboson_qcd_control")
 		
 		# additional settings
 		self.config["analysis_modules"] = [
