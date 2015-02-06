de69977dae54166a7162e86577c5ef021fdb5f2a
Thomas Mueller <tmuller@cern.ch>
2015-02-06 16:58:26 +0100

    checkoutpackages.py: updates for statistics tools, twiki updated, requires CMSSW 715

--- python/checkoutpackages.py
+++ python/checkoutpackages.py
@@ -14,8 +14,15 @@ def checkout_packages(max_n_trials=2):
 	commands = [
 		"cd $CMSSW_BASE/src",
 		
+		"svn co https://ekptrac.physik.uni-karlsruhe.de/svn/KITHiggsToTauTau-auxiliaries/trunk HiggsAnalysis/KITHiggsToTauTau/auxiliaries",
+		
 		# needed by the JetEnergyCorrectionProducer
 		"git clone https://github.com/veelken/SVfit_standalone.git TauAnalysis/SVfitStandalone",
+		
+		# needed for plotting and statistical inference
+		"git clone https://github.com/cms-analysis/HiggsAnalysis-HiggsToTauTau.git -d development HiggsAnalysis/HiggsToTauTau",
+		"git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit",
+		"git clone https://github.com/roger-wolf/HiggsAnalysis-HiggsToTauTau-auxiliaries.git auxiliaries",
 	]
 	checkoutpackages.execute_commands(commands, max_n_trials=max_n_trials)
 
