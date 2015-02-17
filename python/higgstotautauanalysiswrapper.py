58b194509223603707b9f95306f52253b8fbd6f7
Thomas Mueller <tmuller@cern.ch>
2015-02-17 13:40:23 +0100

    higgstotautauanalysiswrapper.py: implemented setting of LD_LIBRARY_PATH in the wrapper

--- python/higgstotautauanalysiswrapper.py
+++ python/higgstotautauanalysiswrapper.py
@@ -4,10 +4,10 @@ import logging
 import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
-import Artus.Configuration.artusWrapper as artusWrapper
+import Artus.KappaAnalysis.kappaanalysiswrapper as kappaanalysiswrapper
 
 
-class HiggsToTauTauAnalysisWrapper(artusWrapper.ArtusWrapper):
+class HiggsToTauTauAnalysisWrapper(kappaanalysiswrapper.KappaAnalysisWrapper):
 
 	def __init__(self):
 		super(HiggsToTauTauAnalysisWrapper, self).__init__("HiggsToTauTauAnalysis")
@@ -15,4 +15,5 @@ class HiggsToTauTauAnalysisWrapper(artusWrapper.ArtusWrapper):
 	def _initArgumentParser(self, userArgParsers=None):
 		super(HiggsToTauTauAnalysisWrapper, self)._initArgumentParser(userArgParsers)
 		self._parser.set_defaults(gc_config_includes=["$CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/gc/htt_includes.conf"])
+		self._parser.set_defaults(ld_library_paths=["$CMSSW_BASE/src/HiggsAnalysis/HiggsToTauTau/CombineHarvester/CombineTools/lib/"]+self._parser.get_default("ld_library_paths"))
 
