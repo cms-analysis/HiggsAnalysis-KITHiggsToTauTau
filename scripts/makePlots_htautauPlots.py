c3ac40340cdd19850a09d6700c84bef823646503
Thomas Mueller <tmuller@cern.ch>
2015-03-24 00:38:13 +0100

    makePlots_htautauPlots.py: added plotting script for plots with original sample settings

---
+++ scripts/makePlots_htautauPlots.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import argparse
+import os
+import shlex
+
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gErrorIgnoreLevel = ROOT.kError
+from HiggsAnalysis.HiggsToTauTau.utils import parseArgs
+
+import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.samples as samples
+import HiggsAnalysis.KITHiggsToTauTau.plotting.higgsplot as higgsplot
+
+
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(description="Make Htautau plots with central sample estimation.",
+	                                 parents=[logger.loggingParser])
+
+	parser.add_argument("-i", "--input-dir", required=True,
+	                    help="Input directory.")
+	parser.add_argument("-s", "--samples", nargs="+",
+	                    default=["ztt", "zl", "zj", "ttj", "vv", "wj", "qcd", "htt", "data"],
+	                    choices=["ztt", "zl", "zj", "ttj", "vv", "wj", "qcd", "ggh", "qqh", "vh", "htt", "data"],
+	                    help="Samples. [Default: %(default)s]")
+	parser.add_argument("--channels", nargs="+", required=True,
+	                    choices=["ee", "em", "et", "mm", "mt", "tt"],
+	                    help="Channels.")
+	parser.add_argument("--categories", nargs="+", default=[None],
+	                    help="Categories. [Default: %(default)s]")
+	parser.add_argument("-w", "--weight", default="1.0",
+	                    help="Additional weight (cut) expression. [Default: %(default)s]")
+	parser.add_argument("-m", "--higgs-masses", nargs="+", default=["125"],
+	                    help="Higgs masses. [Default: %(default)s]")
+	parser.add_argument("--analysis-modules", default=[], nargs="+",
+	                    help="Additional analysis Modules. [Default: %(default)s]")
+	parser.add_argument("-a", "--args", default="--plot-modules PlotRootHtt",
+	                    help="Additional Arguments for HarryPlotter. [Default: %(default)s]")
+	parser.add_argument("-n", "--n-processes", type=int, default=1,
+	                    help="Number of (parallel) processes. [Default: %(default)s]")
+
+	args = parser.parse_args()
+	logger.initLogger(args)
+	args.higgs_masses = parseArgs(args.higgs_masses)
+	
+	list_of_samples = [getattr(samples.Sample, sample) for sample in args.samples]
+	sample_settings = samples.Sample()
+	
+	args.categories = [None if category == "None" else category for category in args.categories]
+	
+	configs = []
+	for channel in args.channels:
+		for category in args.categories:
+			
+			configs.append(sample_settings.get_config(
+					samples=list_of_samples,
+					channel=channel,
+					category=category,
+					higgs_masses=args.higgs_masses,
+					normalise_signal_to_one_pb=False
+			))
+			
+			configs[-1].setdefault("analysis_modules", []).append(args.analysis_modules)
+			configs[-1]["directories"] = [args.input_dir]
+			configs[-1]["weights"] = ["({w1})*({w2})".format(w1=weight, w2=args.weight) for weight in configs[-1].get("weights", ["1.0"])]
+	
+	higgs_plotter = higgsplot.HiggsPlotter(list_of_config_dicts=configs, list_of_args_strings=[args.args], n_processes=args.n_processes)
+
