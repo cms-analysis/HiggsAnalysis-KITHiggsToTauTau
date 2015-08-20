a49971821a3238d32257bbe11dd42576caa68d39
Thomas Mueller <tmuller@cern.ch>
2015-08-20 14:35:14 +0200

    HiggsAnalysis-KITHiggsToTauTau_tests.sh: updated tests

--- test/HiggsAnalysis-KITHiggsToTauTau_tests.sh
+++ test/HiggsAnalysis-KITHiggsToTauTau_tests.sh
@@ -4,7 +4,7 @@
 HiggsToTauTauAnalysis.py @${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/ArtusWrapperConfigs/SM_Htautau.cfg -i ${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/Samples/DCAP_sample_DYJetsToLL_M_50_madgraph_8TeV_recent.txt -f 1 && \
 
 # 13 TeV sync analysis
-HiggsToTauTauAnalysis.py @${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/ArtusWrapperConfigs/Sync13TeV.cfg -i ${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/Samples/DCAP_sample_SUSYGluGluToHToTauTauM160_RunIISpring15DR74_Asympt25ns_13TeV_MINIAODSIM_recent.txt -f 1 && \
+HiggsToTauTauAnalysis.py @${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/ArtusWrapperConfigs/Sync13TeV.cfg -i ${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/Samples/DCAP_sample_SUSYGluGluToHToTauTauM160_RunIISpring15DR74_Asympt25ns_13TeV_MINIAOD_pythia8_recent.txt -f 1 && \
 
 # 13 TeV run2 analysis
 HiggsToTauTauAnalysis.py @${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/ArtusWrapperConfigs/Run2Analysis.cfg -i ${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/Samples/DCAP_sample_SUSYGluGluToHToTauTauM160_RunIISpring15DR74_Asympt25ns_13TeV_MINIAODSIM_recent.txt -f 1
