b64bd7d98705c52ea9a5ea4628f265acf433dd75
Fabio Colombo <fabio.colombo@cern.ch>
2015-09-25 13:22:52 +0200

    ZTT physics models: chenge in efficiency model

--- python/datacards/zttmodels.py
+++ python/datacards/zttmodels.py
@@ -42,28 +42,38 @@ class ZttEffAndXsec(PhysicsModel):
 class ZttEff(PhysicsModel):
 	def __init__(self):
 		self.verbose = False
+
+		# efficiency of discriminator under study (eff)
+		self.efficiency = 0.0
+
 	def setPhysicsOptions(self, physOptions):
 		for po in physOptions:
 			if po.startswith("verbose"):
 				self.verbose = True
+			if po.startswith("eff="):
+				self.efficiency = po.replace("eff=","") 
+
 	def doParametersOfInterest(self):
 		"""Create POI and other parameters, and define the POI set."""
-		# --- Higgs Mass as other parameter ----
-		self.modelBuilder.doVar("eff[1,0,2]")
-		self.modelBuilder.factory_('expr::fail("(1 - @0)", eff)')
+		# POI is the efficiency scale factor (r = eff'/eff)
+		self.modelBuilder.doVar("r[1,0,2]")
+		self.modelBuilder.factory_('expr::failExpr("(1 - @0 * @1)/(1 - @1)", r, %s)' % (self.efficiency))
+
 		if self.options.mass != 0:
 			if self.modelBuilder.out.var("MH"):
 			  self.modelBuilder.out.var("MH").removeRange()
 			  self.modelBuilder.out.var("MH").setVal(self.options.mass)
 			else:
-			  self.modelBuilder.doVar("MH[%g]" % self.options.mass); 
-		self.modelBuilder.doSet("POI","eff")
+			  self.modelBuilder.doVar("MH[%g]" % self.options.mass)
+
+		self.modelBuilder.doSet("POI","r")
+
 	def getYieldScale(self,bin,process):
 		if self.DC.isSignal[process]:
 			if "pass" in bin:
-				return "eff"
+				return "r"
 			elif "fail" in bin:
-				return "fail"
+				return "failExpr"
 			else:
 				return 1
 		else:
