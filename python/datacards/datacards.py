62901eb02a9ffbd73b35ac73dc15a24167ac28dc
Thomas Mueller <tmuller@cern.ch>
2015-10-17 16:33:01 +0200

    datacards.py: better filenames for pre-/postfit plots

--- python/datacards/datacards.py
+++ python/datacards/datacards.py
@@ -377,7 +377,7 @@ class Datacards(object):
 							config["legend"] = [0.7, 0.6, 0.9, 0.88]
 							
 							config["output_dir"] = os.path.join(os.path.dirname(datacard), "plots")
-							config["filename"] = category+"_"+level+("_"+fit_type if level == "postfit" else "")
+							config["filename"] = level+("_"+fit_type if level == "postfit" else "")+"_"+category
 							
 							if not "NormalizeByBinWidth" in config.get("analysis_modules", []):
 								config.setdefault("analysis_modules", []).append("NormalizeByBinWidth")
