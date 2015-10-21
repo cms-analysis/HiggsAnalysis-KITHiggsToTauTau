163dbc5ec3f8bfaf888ee149f20c2c38d83def9c
stefan <stefan.wayand@gmail.com>
2015-10-21 13:20:48 +0200

    bugfix and add expample script

---
+++ data/ArtusConfigs/Embedding_run2/run_example/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+HiggsToTauTauAnalysis.py @$CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/ArtusWrapperConfigs/Run2embedding.cfg -i /nfs/dust/cms/user/swayand/Kappa_out/tast_skim/embedded/embedded_job_15_kappaTuple.root --nick=13TeV
+#HiggsToTauTauAnalysis.py @$CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/ArtusWrapperConfigs/Run2embedding.cfg -i /nfs/dust/cms/user/swayand/embedd_save/Embed_RH_mt88_noisefix2/K2Skim_DYJetsToMuMu_Embed_RH_229.root --nick=13TeV
