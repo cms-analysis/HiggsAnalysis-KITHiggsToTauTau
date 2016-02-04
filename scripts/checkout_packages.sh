f0b427bc14bce59ee56a2d8a7c33f48ae78f0980
Raphael Friese <raphael.friese@cern.ch>
2016-02-04 18:08:55 +0100

    proper branch

--- scripts/checkout_packages.sh
+++ scripts/checkout_packages.sh
@@ -13,7 +13,7 @@ git cms-addpkg CondFormats/JetMETObjects
 
 # From Kappa, only the DataFormats are needed
 # Mind that for certain skims, you need exactly the Kappa git tag that has been used for the production
-git clone https://github.com/KappaAnalysis/Kappa.git -b generatorFlags
+git clone https://github.com/KappaAnalysis/Kappa.git -b CMSSW_7_6_X
 cd Kappa
 echo docs/ >> .git/info/sparse-checkout
 echo DataFormats/ >> .git/info/sparse-checkout
