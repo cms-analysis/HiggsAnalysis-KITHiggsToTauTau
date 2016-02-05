9dec61032537c060aa1f43d59dae488e32a53b47
Rene Caspart <rcaspart@cern.ch>
2016-02-05 11:32:44 +0100

    checkout_script: added branch for KITHiggsToTauTau, replaced cmsrel and cmsenv with explicit command

--- scripts/checkout_packages.sh
+++ scripts/checkout_packages.sh
@@ -5,8 +5,8 @@ export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
 # set up CMSSW release area
-cmsrel CMSSW_7_1_5; cd CMSSW_7_1_5/src # slc6 # Combine requires this version
-cmsenv
+scramv1 project CMSSW CMSSW_7_1_5; cd CMSSW_7_1_5/src # slc6 # Combine requires this version
+eval `scramv1 runtime -sh`
 
 # JEC
 git cms-addpkg CondFormats/JetMETObjects
@@ -30,15 +30,15 @@ git clone https://github.com/artus-analysis/Artus.git -b Kappa_2_1
 git clone https://github.com/artus-analysis/Artus.wiki.git Artus/Core/doc/wiki
 
 # checkout KITHiggsToTauTau CMSSW analysis package
-git clone https://github.com/cms-analysis/HiggsAnalysis-KITHiggsToTauTau HiggsAnalysis/KITHiggsToTauTau
+git clone https://github.com/cms-analysis/HiggsAnalysis-KITHiggsToTauTau HiggsAnalysis/KITHiggsToTauTau -b Kappa_2_1
 git clone https://github.com/cms-analysis/HiggsAnalysis-KITHiggsToTauTau.wiki.git HiggsAnalysis/KITHiggsToTauTau/doc/wiki
 svn co https://ekptrac.physik.uni-karlsruhe.de/svn/KITHiggsToTauTau-auxiliaries/trunk HiggsAnalysis/KITHiggsToTauTau/auxiliaries
-echo '<use   name="rootrflx"/>'>> TauAnalysis/SVfitStandalone/BuildFile.xml
 
 # Svfit and HHKinFit
 git clone https://github.com/veelken/SVfit_standalone.git TauAnalysis/SVfitStandalone
 cd TauAnalysis/SVfitStandalone
 git checkout dd7cf43e3f930040959f7d700cef976307d7cec3 -b current
+echo '<use   name="rootrflx"/>'>> TauAnalysis/SVfitStandalone/BuildFile.xml
 cd $CMSSW_BASE/src
 git clone https://github.com/thomas-mueller/HHKinFit2.git
 
