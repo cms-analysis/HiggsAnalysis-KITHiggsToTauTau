5627b9c06f182dc34abdfb7412f95ab9f3a8a1fa
Marcus Schmitt <Marcus.Schmitt@cern.ch>
2016-03-17 17:45:32 +0100

    7_6 Adaption for TMVAProducers

--- interface/HttSettings.h
+++ interface/HttSettings.h
@@ -7,7 +7,7 @@
 
 /**
    \brief Reads settings for all parts of the KappaAnalysis code from a prepared
-   json configuration file. 
+   json configuration file.
 
    Defines settings that can be obtained from the json configuration file.
 */
@@ -20,85 +20,85 @@ public:
 	IMPL_SETTING_DEFAULT(std::string, MvaMetMT, "");
 	IMPL_SETTING_DEFAULT(std::string, MvaMetET, "");
 	IMPL_SETTING_DEFAULT(std::string, MvaMetEM, "");
-	
+
 	/// names of (new) MVA MET collection in kappa tuple
 	IMPL_SETTING_DEFAULT(std::string, MvaMetsTT, "");
 	IMPL_SETTING_DEFAULT(std::string, MvaMetsMT, "");
 	IMPL_SETTING_DEFAULT(std::string, MvaMetsET, "");
 	IMPL_SETTING_DEFAULT(std::string, MvaMetsEM, "");
 	IMPL_SETTING_DEFAULT(std::string, MvaMets, "");
-	
+
 	/// htt decay channel and event category
 	IMPL_SETTING_DEFAULT(std::string, Channel, "");
 	IMPL_SETTING_DEFAULT(std::string, Category, "");
-	
+
 	IMPL_SETTING(bool, OSChargeLeptons);
-	
+
 	IMPL_SETTING_STRINGLIST_DEFAULT(TriggerEfficiencyData, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(TriggerEfficiencyMc, {});
 	IMPL_SETTING_DEFAULT(std::string, TriggerEfficiencyHistogram, "triggerEfficiency");
 	IMPL_SETTING_DEFAULT(std::string, TriggerEfficiencyMode, "multiply_weights");
-	
+
 	IMPL_SETTING_STRINGLIST_DEFAULT(IdentificationEfficiencyData, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(IdentificationEfficiencyMc, {});
 	IMPL_SETTING_DEFAULT(std::string, IdentificationEfficiencyHistogram, "identificationEfficiency");
 	IMPL_SETTING_DEFAULT(std::string, IdentificationEfficiencyMode, "multiply_weights");
-	
+
 	std::vector<std::string> EleTauFakeRateHistograms = {"antiEVLoose", "antiELoose", "antiEMedium", "antiETight", "antiEVTight"};
 	IMPL_SETTING_STRINGLIST_DEFAULT(EleTauFakeRateWeightFile, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(EleTauFakeRateHistograms, EleTauFakeRateHistograms);
 	IMPL_SETTING_STRINGLIST_DEFAULT(MuonTauFakeRateWeightFile, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(MuonTauFakeRateHistograms, {});
-	
+
 	IMPL_SETTING(std::string, ElectronIDType);
-	
+
 	IMPL_SETTING_DEFAULT(float, ElectronChargedIsoVetoConeSizeEB, 0.0);
 	IMPL_SETTING_DEFAULT(float, ElectronChargedIsoVetoConeSizeEE, 0.0);
 	IMPL_SETTING_DEFAULT(float, ElectronNeutralIsoVetoConeSize, 0.0);
 	IMPL_SETTING_DEFAULT(float, ElectronPhotonIsoVetoConeSizeEB, 0.0);
 	IMPL_SETTING_DEFAULT(float, ElectronPhotonIsoVetoConeSizeEE, 0.0);
 	IMPL_SETTING_DEFAULT(float, ElectronDeltaBetaIsoVetoConeSize, 0.0);
-	
+
 	IMPL_SETTING_DEFAULT(float, ElectronChargedIsoPtThreshold, 0.0);
 	IMPL_SETTING_DEFAULT(float, ElectronNeutralIsoPtThreshold, 0.0);
 	IMPL_SETTING_DEFAULT(float, ElectronPhotonIsoPtThreshold, 0.0);
 	IMPL_SETTING_DEFAULT(float, ElectronDeltaBetaIsoPtThreshold, 0.0);
-	
+
 	IMPL_SETTING(float, ElectronIsoSignalConeSize);
 	IMPL_SETTING(float, ElectronDeltaBetaCorrectionFactor);
 	IMPL_SETTING_DEFAULT(float, ElectronIsoPtSumOverPtLowerThresholdEB, -1.0);
 	IMPL_SETTING_DEFAULT(float, ElectronIsoPtSumOverPtLowerThresholdEE, -1.0);
 	IMPL_SETTING_DEFAULT(float, ElectronIsoPtSumOverPtUpperThresholdEB, std::numeric_limits<float>::max());
 	IMPL_SETTING_DEFAULT(float, ElectronIsoPtSumOverPtUpperThresholdEE, std::numeric_limits<float>::max());
-	
+
 	IMPL_SETTING_DEFAULT(float, MuonChargedIsoVetoConeSize, 0.0);
 	IMPL_SETTING_DEFAULT(float, MuonNeutralIsoVetoConeSize, 0.0);
 	IMPL_SETTING_DEFAULT(float, MuonPhotonIsoVetoConeSize, 0.0);
 	IMPL_SETTING_DEFAULT(float, MuonDeltaBetaIsoVetoConeSize, 0.0);
-	
+
 	IMPL_SETTING_DEFAULT(float, MuonChargedIsoPtThreshold, 0.0);
 	IMPL_SETTING_DEFAULT(float, MuonNeutralIsoPtThreshold, 0.0);
 	IMPL_SETTING_DEFAULT(float, MuonPhotonIsoPtThreshold, 0.0);
 	IMPL_SETTING_DEFAULT(float, MuonDeltaBetaIsoPtThreshold, 0.0);
-	
+
 	IMPL_SETTING(float, MuonIsoSignalConeSize);
 	IMPL_SETTING(float, MuonDeltaBetaCorrectionFactor);
 	IMPL_SETTING_DEFAULT(float, MuonIsoPtSumOverPtLowerThresholdEB, -1.0);
 	IMPL_SETTING_DEFAULT(float, MuonIsoPtSumOverPtLowerThresholdEE, -1.0);
 	IMPL_SETTING_DEFAULT(float, MuonIsoPtSumOverPtUpperThresholdEB, std::numeric_limits<float>::max());
 	IMPL_SETTING_DEFAULT(float, MuonIsoPtSumOverPtUpperThresholdEE, std::numeric_limits<float>::max());
-	
+
 	IMPL_SETTING_DEFAULT(float, ElectronTrackDxyCut, -1.0);
 	IMPL_SETTING_DEFAULT(float, ElectronTrackDzCut, -1.0);
 	IMPL_SETTING_DEFAULT(float, MuonTrackDxyCut, -1.0);
 	IMPL_SETTING_DEFAULT(float, MuonTrackDzCut, -1.0);
 	IMPL_SETTING_DEFAULT(float, TauTrackDzCut, -1.0);
-	
+
 	IMPL_SETTING(std::string, LooseElectronID);
 	IMPL_SETTING(std::string, LooseElectronIDType);
 	IMPL_SETTING(std::string, LooseElectronReco);
 	IMPL_SETTING(std::string, LooseMuonID);
-	
+
 	IMPL_SETTING(std::string, LooseElectronIsoType);
 	IMPL_SETTING(std::string, LooseElectronIso);
 	IMPL_SETTING_DEFAULT(float, LooseElectronIsoPtSumOverPtLowerThresholdEB, -1.0);
@@ -118,17 +118,17 @@ public:
 	IMPL_SETTING_DEFAULT(float, LooseElectronTrackDzCut, -1.0);
 	IMPL_SETTING_DEFAULT(float, LooseMuonTrackDxyCut, -1.0);
 	IMPL_SETTING_DEFAULT(float, LooseMuonTrackDzCut, -1.0);
-	
+
 	IMPL_SETTING_STRINGLIST_DEFAULT(LooseElectronLowerPtCuts, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(LooseMuonLowerPtCuts, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(LooseElectronUpperAbsEtaCuts, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(LooseMuonUpperAbsEtaCuts, {});
-	
+
 	IMPL_SETTING(std::string, VetoElectronID);
 	IMPL_SETTING(std::string, VetoElectronIDType);
 	IMPL_SETTING(std::string, VetoElectronReco);
 	IMPL_SETTING(std::string, VetoMuonID);
-	
+
 	IMPL_SETTING(std::string, VetoElectronIsoType);
 	IMPL_SETTING(std::string, VetoElectronIso);
 	IMPL_SETTING_DEFAULT(float, VetoElectronIsoPtSumOverPtLowerThresholdEB, -1.0);
@@ -141,60 +141,60 @@ public:
 	IMPL_SETTING_DEFAULT(float, VetoMuonIsoPtSumOverPtLowerThresholdEE, -1.0);
 	IMPL_SETTING_DEFAULT(float, VetoMuonIsoPtSumOverPtUpperThresholdEB, std::numeric_limits<float>::max());
 	IMPL_SETTING_DEFAULT(float, VetoMuonIsoPtSumOverPtUpperThresholdEE, std::numeric_limits<float>::max());
-	
+
 	IMPL_SETTING_STRINGLIST_DEFAULT(VetoElectronLowerPtCuts, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(VetoMuonLowerPtCuts, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(VetoElectronUpperAbsEtaCuts, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(VetoMuonUpperAbsEtaCuts, {});
-	
+
 	IMPL_SETTING(std::string, DiVetoElectronVetoMode);
 	IMPL_SETTING_DEFAULT(float, DiVetoElectronMinDeltaRCut, -1.0);
-	
+
 	IMPL_SETTING(std::string, DiVetoMuonVetoMode);
 	IMPL_SETTING_DEFAULT(float, DiVetoMuonMinDeltaRCut, -1.0);
-	
+
 	IMPL_SETTING_DEFAULT(std::string, ElectronEnergyCorrection, "none");
 	IMPL_SETTING_DEFAULT(float, ElectronEnergyCorrectionShift, 1.0);
-	
+
 	IMPL_SETTING(float, RecoMuonInElectronConeLowerPtCut);
 	IMPL_SETTING(float, RecoMuonInElectronConeUpperAbsEtaCut);
 	IMPL_SETTING(float, RecoMuonInElectronConeSize);
-	
+
 	IMPL_SETTING(std::string, ZBosonVetoType);
 	IMPL_SETTING(float, MetLowerPtCuts);
 	IMPL_SETTING(float, MetUpperPtCuts);
-	
+
 	IMPL_SETTING_DEFAULT(std::string, TauDiscriminatorIsolationName, "hpsPFTauDiscriminationByRawCombinedIsolationDBSumPtCorr3Hits");
 	IMPL_SETTING_DEFAULT(float, TauDiscriminatorIsolationCut, std::numeric_limits<float>::max());
 	IMPL_SETTING_STRINGLIST_DEFAULT(TauDiscriminatorMvaIsolation, {});
 	IMPL_SETTING_FLOATLIST_DEFAULT(TauDiscriminatorAntiElectronMvaCuts, {});
 	IMPL_SETTING_INTLIST_DEFAULT(TauDiscriminatorAntiElectronMvaCutsLeptonIndices, {});
-	
+
 	IMPL_SETTING_DEFAULT(float, TauLowerZImpactCut, std::numeric_limits<float>::max());
 	IMPL_SETTING_DEFAULT(float, TauUpperZImpactCut, std::numeric_limits<float>::max());
-	
+
 	IMPL_SETTING_DEFAULT(float, TauLeadingTrackPtCut, -1.0);
 	IMPL_SETTING_DEFAULT(float, TauTrackMultiplicityCut, -1.0);
-	
+
 	IMPL_SETTING_DEFAULT(std::string, TauEnergyCorrection, "none");
 	IMPL_SETTING_DEFAULT(float, TauEnergyCorrectionShift, 1.0);
-	
+
 	IMPL_SETTING_DEFAULT(float, SvfitMassShift, 1.0);
-	
+
 	IMPL_SETTING(int, NLooseElectrons);
 	IMPL_SETTING(int, NLooseMuons);
-	
+
 	IMPL_SETTING(int, MaxNLooseElectrons);
 	IMPL_SETTING(int, MaxNLooseMuons);
-	
+
 	IMPL_SETTING(float, LowerCutHardLepPt);
-	
+
 	IMPL_SETTING_DEFAULT(float, DiTauPairMinDeltaRCut, -1.0);
 	IMPL_SETTING_STRINGLIST_DEFAULT(DiTauPairLepton1LowerPtCuts, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(DiTauPairLepton2LowerPtCuts, {});
 	IMPL_SETTING_SORTED_STRINGLIST_DEFAULT(DiTauPairHltPathsWithoutCommonMatchRequired, {});
 	IMPL_SETTING_DEFAULT(bool, DiTauPairIsTauIsoMVA, false);
-	
+
 	IMPL_SETTING(std::string, SvfitIntegrationMethod);
 	IMPL_SETTING_DEFAULT(std::string, SvfitCacheFile, "");
 	IMPL_SETTING_DEFAULT(std::string, SvfitCacheTree, "svfitCache");
@@ -202,14 +202,14 @@ public:
 	IMPL_SETTING_DEFAULT(std::string, SvfitOutFile, "");
 	IMPL_SETTING_DEFAULT(bool, GenerateSvFitInput, false);
 	IMPL_SETTING_DEFAULT(int, SvFitInputCutOff, 5000)
-	
+
 	IMPL_SETTING(std::string, TauTauRestFrameReco);
 
 	IMPL_SETTING_DEFAULT(float, TauElectronLowerDeltaRCut, 0.5);
 	IMPL_SETTING_DEFAULT(float, TauMuonLowerDeltaRCut, 0.5);
 	IMPL_SETTING_DEFAULT(float, TauTauLowerDeltaRCut, 0.5);
 	IMPL_SETTING_DEFAULT(float, JetTauLowerDeltaRCut, 0.5);
-	
+
 	IMPL_SETTING(std::string, TauSpinnerSettingsPDF);
 	IMPL_SETTING(float, TauSpinnerSettingsCmsEnergy);
 	IMPL_SETTING(bool, TauSpinnerSettingsIpp);
@@ -217,7 +217,7 @@ public:
 	IMPL_SETTING(int, TauSpinnerSettingsNonSM2);
 	IMPL_SETTING(int, TauSpinnerSettingsNonSMN);
 	IMPL_SETTING(bool, TauSpinnerSettingsBoost);
-	
+
 	IMPL_SETTING_FLOATLIST_DEFAULT(TauSpinnerMixingAnglesOverPiHalf, {});
 	IMPL_SETTING_DEFAULT(float, TauSpinnerMixingAnglesOverPiHalfSample, -1.0);
 
@@ -233,17 +233,18 @@ public:
     IMPL_SETTING_STRINGLIST_DEFAULT(AntiTtbarTmvaInputQuantities, {});
     IMPL_SETTING_STRINGLIST_DEFAULT(AntiTtbarTmvaMethods, {});
     IMPL_SETTING_STRINGLIST_DEFAULT(AntiTtbarTmvaWeights, {});
-    
+
     //MVATestMethodsProducer settings
     IMPL_SETTING_STRINGLIST_DEFAULT(MVATestMethodsInputQuantities, {});
     IMPL_SETTING_STRINGLIST_DEFAULT(MVATestMethodsMethods, {});
     IMPL_SETTING_STRINGLIST_DEFAULT(MVATestMethodsNames, {});
+	IMPL_SETTING_INTLIST_DEFAULT(MVATestMethodsNFolds, {});
     IMPL_SETTING_STRINGLIST_DEFAULT(MVATestMethodsWeights, {});
-    
+
 	// settings for TriggerTagAndProbeProducers
 	IMPL_SETTING_STRINGLIST_DEFAULT(TagLeptonHltPaths, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(ProbeLeptonHltPaths, {});
-	
+
 	IMPL_SETTING_STRINGLIST_DEFAULT(TagLeptonTriggerFilterNames, {});
 	IMPL_SETTING_STRINGLIST_DEFAULT(ProbeLeptonTriggerFilterNames, {});
 
@@ -253,10 +254,10 @@ public:
 	IMPL_SETTING_DEFAULT(int, IsoPtSumBinning, 200);
 	IMPL_SETTING_DEFAULT(float, IsoPtSumMaximum, 50);
 	IMPL_SETTING_DEFAULT(float, IsoPtSumOverPtMaximum, 0.4);
-	
+
 	// setting for mass smearing applied in DiLeptonQuantitiesProducer
 	IMPL_SETTING_DEFAULT(float, MassSmearing, 0.20);
-	
+
 	// settings for the acceptance cuts in GenAcceptanceProducer
 	IMPL_SETTING_DEFAULT(float, Lepton1AcceptancePtCut, -1.0);
 	IMPL_SETTING_DEFAULT(float, Lepton2AcceptancePtCut, -1.0);
--- src/Producers/MVATestMethodsProducer.cc
+++ src/Producers/MVATestMethodsProducer.cc
@@ -5,7 +5,7 @@
 #include "HiggsAnalysis/KITHiggsToTauTau/interface/Utility/Quantities.h"
 #include "Artus/Utility/interface/DefaultValues.h"
 
-	
+
 MVATestMethodsProducer::MVATestMethodsProducer() :
 	TmvaClassificationMultiReaderBase<HttTypes>(&spec_setting_type::GetMVATestMethodsInputQuantities,
 											&spec_setting_type::GetMVATestMethodsMethods,
@@ -16,17 +16,49 @@ MVATestMethodsProducer::MVATestMethodsProducer() :
 
 void MVATestMethodsProducer::Init(spec_setting_type const& settings)
 {
-	
-	for (uint mvaMethodIndex = 0; mvaMethodIndex < (settings.GetMVATestMethodsNames)().size(); ++mvaMethodIndex)
+	uint output_index = 0;
+	for (uint NFoldIndex = 0; NFoldIndex < settings.GetMVATestMethodsNFolds().size(); ++NFoldIndex)
+	{
+		if (settings.GetMVATestMethodsNFolds()[NFoldIndex] <= 1)
 		{
-//			 std::string bdt_out_name = "MVATestMethod_" + boost::lexical_cast<std::string>(mvaMethodIndex);
-			std::string bdt_out_name = settings.GetMVATestMethodsNames()[mvaMethodIndex];
-			LambdaNtupleConsumer<HttTypes>::AddFloatQuantity(bdt_out_name, [bdt_out_name, mvaMethodIndex](spec_event_type const& event, spec_product_type const& product)
-			{	
-				return ((product.m_MVATestMethodsDiscriminators.size() > 0 ) ? product.m_MVATestMethodsDiscriminators[mvaMethodIndex] : DefaultValues::UndefinedFloat);
+			std::string bdt_out_name = settings.GetMVATestMethodsNames()[NFoldIndex];
+			LambdaNtupleConsumer<HttTypes>::AddFloatQuantity(bdt_out_name, [bdt_out_name, output_index](spec_event_type const& event, spec_product_type const& product)
+			{
+				return ((product.m_MVATestMethodsDiscriminators.size() > 0 ) ? product.m_MVATestMethodsDiscriminators[output_index] : DefaultValues::UndefinedFloat);
+			});
+			output_index += 1;
+		}
+		else
+		{
+			for (int TrainingIndex = 1; TrainingIndex <= settings.GetMVATestMethodsNFolds()[NFoldIndex]; ++TrainingIndex)
+			{
+				std::string bdt_out_name = "T" + boost::lexical_cast<std::string>(TrainingIndex) + settings.GetMVATestMethodsNames()[NFoldIndex];
+				LambdaNtupleConsumer<HttTypes>::AddFloatQuantity(bdt_out_name, [bdt_out_name, output_index](spec_event_type const& event, spec_product_type const& product)
+				{
+					return ((product.m_MVATestMethodsDiscriminators.size() > 0 ) ? product.m_MVATestMethodsDiscriminators[output_index] : DefaultValues::UndefinedFloat);
+				});
+				output_index += 1;
+			}
+
+			std::string bdt_out_name = settings.GetMVATestMethodsNames()[NFoldIndex];
+			LambdaNtupleConsumer<HttTypes>::AddFloatQuantity(bdt_out_name, [bdt_out_name, output_index, settings, NFoldIndex](spec_event_type const& event, spec_product_type const& product)
+			{
+				int ts_value = (event.m_eventInfo->nEvent)%100, width = 100/settings.GetMVATestMethodsNFolds()[NFoldIndex];
+				for (int finalIndex = 1; finalIndex <= settings.GetMVATestMethodsNFolds()[NFoldIndex]; ++finalIndex)
+				{
+					if( ((finalIndex-1)*width <= ts_value) and (ts_value < finalIndex*width))
+					{
+						return ((product.m_MVATestMethodsDiscriminators.size() > 0 ) ? product.m_MVATestMethodsDiscriminators[output_index-finalIndex] : DefaultValues::UndefinedFloat);
+					}
+					else if (((settings.GetMVATestMethodsNFolds()[NFoldIndex]-1)*width <= ts_value) and (ts_value < 100)){
+						return ((product.m_MVATestMethodsDiscriminators.size() > 0 ) ? product.m_MVATestMethodsDiscriminators[output_index-settings.GetMVATestMethodsNFolds()[NFoldIndex]] : DefaultValues::UndefinedFloat);
+					}
+				}
+				return -2.0;
 			});
 		}
-	
+	}
+
 	// has to be called at the end of the subclass function
 	TmvaClassificationMultiReaderBase<HttTypes>::Init(settings);
 }
@@ -39,7 +71,7 @@ void  MVATestMethodsProducer::Produce(spec_event_type const& event,
 	assert(event.m_tjets);
 	assert(event.m_jetMetadata);
 	assert(product.m_met);
-	
+
 	// has to be called at the end of the subclass function
 	TmvaClassificationMultiReaderBase<HttTypes>::Produce(event, product, settings);
 }
