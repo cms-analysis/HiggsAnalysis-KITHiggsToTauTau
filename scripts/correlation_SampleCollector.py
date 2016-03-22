89853b8d090ab0966d03d39344659d69e24cf930
Marcus Schmitt <Marcus.Schmitt@cern.ch>
2016-03-22 10:55:17 +0100

    Correlation calculation and plotting

---
+++ scripts/correlation_SampleCollector.py
@@ -0,0 +1,129 @@
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import argparse
+import copy
+import os
+import re
+
+import Artus.Utility.jsonTools as jsonTools
+import sys
+import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.samples_run2 as samples
+import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.binnings as binnings
+import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.labels as labels
+import ROOT
+import glob
+import itertools
+import ROOT
+import matplotlib.pyplot as plt
+from matplotlib import cm
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+def plot_correlations(parameters, correlation_dict, sample, channel, dir_path, outname):
+	corr_vars = {}
+	ws = correlation_dict["weight_sum"]
+	labeldict = labels.LabelsDict()
+	for varxy in correlation_dict.iterkeys():
+		try:
+			varx, vary = map(str, varxy.split("+-+"))
+		except ValueError:
+			continue
+		corr_vars["var_%s"%varx] = (correlation_dict["var_%s"%varx] - (correlation_dict[varx]**2.)/ws)/ws
+		corr_vars["var_%s"%vary] = (correlation_dict["var_%s"%vary] - (correlation_dict[vary]**2.)/ws)/ws
+		try:
+			corr_vars[varxy] = (correlation_dict[varxy]-correlation_dict[varx]*correlation_dict[vary]/ws)/ws/(
+				corr_vars["var_%s"%varx])**0.5/(corr_vars["var_%s"%vary])**0.5
+		except ZeroDivisionError:
+			log.error("ZeroDivisonError: %s" %varxy)
+			corr_vars[varxy] = None
+		except ValueError:
+			log.error("ValueError: %s" %varxy)
+			corr_vars[varxy] = None
+	jsonTools.JsonDict(corr_vars).save(os.path.join(dir_path,"%s_correlations.json"%channel),indent=4)
+	whole = len(parameters)/5
+	param_lists = []
+	for i in range(whole):
+		param_lists.append([])
+		for j in range(5):
+			param_lists[i].append(parameters.pop(0))
+	param_lists.append(parameters)
+	for i in range(len(param_lists)):
+		for j in range(i,len(param_lists)):
+			x_params = param_lists[i]
+			y_params = param_lists[j]
+			x_vals = []
+			y_vals = []
+			weights = []
+			for pairs in itertools.product(x_params, y_params):
+				x_vals.append(x_params.index(pairs[0])+0.5)
+				y_vals.append(y_params.index(pairs[1])+0.5)
+				try:
+					weights.append(corr_vars["+-+".join(pairs)])
+				except KeyError:
+					weights.append(corr_vars["+-+".join((pairs[1],pairs[0]))])
+			fig = plt.figure()
+			ax = fig.add_subplot(111)
+			counts, xedges, yedges, cax = ax.hist2d(x_vals, y_vals, weights=weights, bins=[len(x_params), len(y_params)], range=[(0,len(x_params)),(0,len(y_params))], cmap=cm.coolwarm, vmin=-1, vmax=1)
+			title_string = "Correlation Matrix: %s \\rightarrow %s"%(labeldict.get_nice_label(sample) ,labeldict.get_nice_label("channel_%s"%channel ))
+			title_string = title_string.replace("$", "")
+			title_string = "$"+title_string+"$"
+			ax.set_title(title_string)
+			ax.set_xticks([x+0.5 for x in range(len(x_params))])
+			ax.set_yticks([x+0.5 for x in range(len(y_params))])
+			ax.set_xticks([x for x in range(len(x_params))], minor = True)
+			ax.set_yticks([x for x in range(len(y_params))], minor = True)
+			ax.set_xticklabels([labeldict.get_nice_label(channel+"_"+x).replace(" / GeV", "").replace(" ", "\\,") for x in x_params], rotation = 50, size='x-large', va='center', ha='right', rotation_mode='anchor')
+			ax.set_yticklabels([labeldict.get_nice_label(channel+"_"+x).replace(" / GeV", "").replace(" ", "\\,") for x in y_params], rotation = 30, size='x-large', va='center', ha='right', rotation_mode='anchor')
+			for triples in zip(x_vals, y_vals, weights):
+				ax.annotate(s="%1.2f"%triples[2], xy=(triples[0],triples[1]), ha = "center", va = "center")
+			# Add colorbar, make sure to specify tick locations to match desired ticklabels
+			cbar = fig.colorbar(cax, ticks=[-1, 0, 1])
+			cbar.ax.set_yticklabels(['-1', '0', '1'])  # vertically oriented colorbar
+			ax.grid(True, which="minor", linewidth = 1.5)
+			ax.tick_params(axis='both', which='major', pad=5)
+			plt.tight_layout()
+			plt.savefig(os.path.join(dir_path,"%s-%i-%i_corM.png"%(outname,i,j)))
+
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Collect and Combine Correlation Information",
+									 parents=[logger.loggingParser])
+	parser.add_argument("-i", "--input-dir", required=True,
+						help="Input directory. Use directory of ooutput from correlation_SampleProducer.py")
+	parser.add_argument("-c", "--channels", nargs="*",
+						default=["tt", "mt", "et", "em", "mm", "ee"],
+						choices=["tt", "mt", "et", "em", "mm", "ee"],
+						help="Channels. [Default: %(default)s]")
+	parser.add_argument("-o", "--output-file",
+							default="combination",
+							help="Output file. [Default: %(default)s]")
+	parser.add_argument("-s", "--samples", nargs="+",
+						default=["ggh", "qqh", "vh", "ztt", "zll", "ttj", "vv", "wj", "data"],
+						choices=["ggh", "qqh", "vh", "ztt", "zll", "ttj", "vv", "wj", "data"],
+						help="Samples for correlation calculation and scatter plots. [Default: %(default)s]")
+	args = parser.parse_args()
+	logger.initLogger(args)
+
+	dir_path = os.path.expandvars(args.input_dir)
+	config_list = []
+	for sample in args.samples:
+		file_dir = os.path.join(dir_path, sample)
+		if os.path.exists(file_dir):
+			for channel in args.channels:
+				config_list.append(jsonTools.JsonDict(os.path.join(file_dir, "%s_corr-config.json"%channel)))
+				config = config_list[-1]
+				plot_correlations(copy.copy(config["parameters_list"]), config["correlations"], sample, channel, file_dir, channel)
+	overall_correlations = {}
+	for i,config in enumerate(config_list):
+		if i == 0:
+			overall_correlations = copy.copy(config["correlations"])
+		else:
+			for varxy in overall_correlations.iterkeys():
+				overall_correlations[varxy] += config["correlations"][varxy]
+	if not os.path.exists(os.path.join(dir_path, "combination")):
+		os.makedirs(os.path.join(dir_path, "combination"))
+
+	plot_correlations(config_list[0]["parameters_list"], overall_correlations, "Combination" ,config_list[0]["channel"], os.path.join(dir_path, "combination"), args.output_file)
+	#calculate variances and correlations -> moved to collector_script
--- scripts/correlation_SampleProducer.py
+++ scripts/correlation_SampleProducer.py
@@ -19,7 +19,7 @@ ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser(description="Train BDTs using TMVA interface.",
+	parser = argparse.ArgumentParser(description="Produce reduced samples and calculate correlations",
 									 parents=[logger.loggingParser])
 	parser.add_argument("-i", "--input-dir", required=True,
 						help="Input directory. Use directory of artusoutput, which contains output and merged folder")
@@ -120,9 +120,10 @@ if __name__ == "__main__":
 				c_tree_list[-1].Add(root_file_name)
 				c_tree_list[-1].SetName("list_tree")
 
-
 			store_file = ROOT.TFile(stored_files_list[-1], "RECREATE")
 			selection_string = cuts.replace("eventWeight*", "")
+			if config["request_nick"] in ["ztt", "zll"]:
+				selection_string = cuts.replace("eventWeight*", "").replace("*stitchWeight%s"%(config["request_nick"].upper()), "")
 			for index in range(len(c_tree_list)):
 				log.debug("Cut Tree %s for Sample %s "%(root_file_name_list[index], stored_files_list[-1]))
 				c_tree_list[index]=c_tree_list[index].CopyTree(selection_string)
@@ -163,71 +164,132 @@ if __name__ == "__main__":
 		root_histograms = {}
 		corr_vars = {}
 		binnings_dict = binnings.BinningsDict()
-		print config["storage_file"]
+		nick_path = os.path.join(dir_path, config["request_nick"])
+		if not os.path.exists(nick_path):
+			os.makedirs(nick_path)
 		root_inf = ROOT.TFile(config["storage_file"], "read")
 		root_inst = root_inf.Get(config["storage_ntuple"])
 		for variables in itertools.combinations(config["parameters_list"], 2):
 			#print variables
-			#print binnings_dict.get_binning("%s_"%config["channel"]+variables[0])
-			#print binnings_dict.get_binning("%s_"%config["channel"]+variables[1])
+			xbins_list = binnings_dict.get_binning("%s_"%config["channel"]+variables[0])
+			ybins_list = binnings_dict.get_binning("%s_"%config["channel"]+variables[1])
+			xbins = []
+			ybins = []
+			if "," in xbins_list:
+				xbins_list = map(float, xbins_list.split(","))
+				xbins += xbins_list
+				xbins[0] = int(xbins[0])
+			elif len(xbins_list.split(" ")) >= 2 and xbins_list.split(" ")[1] != "":
+				xbins_list = map(float, xbins_list.split(","))
+				xbins.append(len(xbins_list.split(" "))-1)
+				xbins.apped(a_bins_list)
+			else:
+				xbins = [100,0.,0.]
 
+			if "," in ybins_list:
+				ybins_list = map(float, ybins_list.split(","))
+				ybins += ybins_list
+				ybins[0] = int(ybins[0])
+			elif len(ybins_list.split(" ")) >= 2 and ybins_list.split(" ")[1] != "":
+				ybins_list = map(float, ybins_list.split(","))
+				ybins.append(len(ybins_list.split(" "))-1)
+				ybins.apped(a_bins_list)
+			else:
+				ybins = [100,0.,0.]
+
+			binnings = xbins + ybins
+			log.debug("Generate RootHistogram TH2F: %s"%("+-+".join(variables)))
+			log.debug(binnings)
 			root_histograms["+-+".join(variables)] = ROOT.TH2F("+-+".join(variables),
-													  "correlation: %s vs %s"%variables, 100,0,0,100,0,0)
+													  "correlation: %s vs %s"%variables, *binnings)
 			corr_vars["+-+".join(variables)] = 0
+			root_histograms["+-+".join(variables)].SetDirectory(0)
+			ROOT.SetOwnership (root_histograms["+-+".join(variables)], False)
 		for variable in config["parameters_list"]:
+			xbins_list = binnings_dict.get_binning("%s_"%config["channel"]+variables[0])
+			xbins = []
+			if "," in xbins_list:
+				xbins_list = map(float, xbins_list.split(","))
+				xbins += xbins_list
+				xbins[0] = int(xbins[0])
+			elif len(xbins_list.split(" ")) >= 2 and xbins_list.split(" ")[1] != "":
+				xbins_list = map(float, xbins_list.split(","))
+				xbins.append(len(xbins_list.split(" "))-1)
+				xbins.apped(a_bins_list)
+			else:
+				xbins = [100,0.,0.]
+
+			binnings = xbins + xbins
 			root_histograms["+-+".join([variable,variable])] = ROOT.TH2F("+-+".join([variable,variable]),
-													  "correlation: %s vs %s"%(variable,variable), 100,0,0,100,0,0)
+													  "correlation: %s vs %s"%(variable,variable), *binnings)
 			corr_vars["+-+".join([variable,variable])] = 0
 			corr_vars[variable] = 0
 			corr_vars["var_%s"%variable] = 0
+			root_histograms["+-+".join([variable,variable])].SetDirectory(0)
+			ROOT.SetOwnership (root_histograms["+-+".join([variable,variable])], False)
 		print "======================================================================"
 		print "Calculate correlations and make scatter plots for %i variable pairs."%len(root_histograms)
 		print "======================================================================"
 
 		i = 0.
+		n = 0
 		zero_vals = {}
 		for event in root_inst:
 			calced_means = []
+			w = event.__getattr__("eventWeight")
 			for varxy in root_histograms.iterkeys():
 				varx, vary = map(str, varxy.split("+-+"))
 				x, y = map(event.__getattr__, map(str, varxy.split("+-+")))
-				w = event.__getattr__("eventWeight")
 				root_histograms[varxy].Fill(x, y, w)
 
 				if varx not in zero_vals:
-					zero_vals[varx] = x
+					zero_vals[varx] = 0
 				if vary not in zero_vals:
-					zero_vals[vary] = y
+					zero_vals[vary] = 0
 
 				if varx not in calced_means:
 					#print "calculate mean for %s" %varx
-					corr_vars[varx] += x -zero_vals[varx]
-					corr_vars["var_%s"%varx] += (x -zero_vals[varx])**2
+					corr_vars[varx] += w*(x -zero_vals[varx])
+					corr_vars["var_%s"%varx] += w*(x -zero_vals[varx])**2
 					calced_means.append(varx)
 				if vary not in calced_means:
 					#print "calculate mean for %s" %vary
-					corr_vars[vary] += y - zero_vals[vary]
-					corr_vars["var_%s"%vary] += (y - zero_vals[vary])**2
+					corr_vars[vary] += w*(y - zero_vals[vary])
+					corr_vars["var_%s"%vary] += w*(y - zero_vals[vary])**2
 					calced_means.append(vary)
-				corr_vars[varxy] += (x -zero_vals[varx]) * (y - zero_vals[vary])
+				corr_vars[varxy] += w*(x -zero_vals[varx]) * (y - zero_vals[vary])
 			#sys.exit()
-			i += 1
-			if i%1000 == 0:
-				print "processed %i events" %i
-			if i%5000 == 0:
-				break
-				print "processed %i events" %i
-		calced_variances = []
+			i += w
+			n += 1
+			#if n == 1000:
+				#break
+
+		#root_inf.Close()
+		hist_file = ROOT.TFile(os.path.join(nick_path, "%s_histograms.root"%config["channel"]),"RECREATE")
 		for varxy in root_histograms.iterkeys():
-			varx, vary = map(str, varxy.split("+-+"))
-			if varx not in calced_variances:
-				corr_vars["var_%s"%varx] = (corr_vars["var_%s"%varx] - (corr_vars[varx]**2.)/i)/(i-1.)
-				calced_variances.append(varx)
-			if vary not in calced_variances:
-				corr_vars["var_%s"%vary] = (corr_vars["var_%s"%vary] - (corr_vars[vary]**2.)/i)/(i-1.)
-				calced_variances.append(vary)
-			try:
-				corr_vars[varxy] = (corr_vars[varxy]-corr_vars[varx]*corr_vars[vary]/i)/i/(corr_vars["var_%s"%varx])**0.5/(corr_vars["var_%s"%vary])**0.5
-			except ZeroDivisionError:
-				corr_vars[varxy] = None
-		jsonTools.JsonDict(corr_vars).save(os.path.join(dir_path,"%s_test.json"%config["channel"]),indent=4)
+			root_histograms[varxy].Write()
+		#calculate variances and correlations -> moved to collector_script
+		#calced_variances = []
+		#for varxy in root_histograms.iterkeys():
+			#varx, vary = map(str, varxy.split("+-+"))
+			#if varx not in calced_variances:
+				#corr_vars["var_%s"%varx] = (corr_vars["var_%s"%varx] - (corr_vars[varx]**2.)/i)/i
+				#calced_variances.append(varx)
+			#if vary not in calced_variances:
+				#corr_vars["var_%s"%vary] = (corr_vars["var_%s"%vary] - (corr_vars[vary]**2.)/i)/i
+				#calced_variances.append(vary)
+			#try:
+				#corr_vars[varxy] = (corr_vars[varxy]-corr_vars[varx]*corr_vars[vary]/i)/i/(corr_vars["var_%s"%varx])**0.5/(corr_vars["var_%s"%vary])**0.5
+			#except ZeroDivisionError:
+				#log.error("ZeroDivisonError: %s" %varxy)
+				#corr_vars[varxy] = None
+			#except ValueError:
+				#log.error("ValueError: %s" %varxy)
+				#corr_vars[varxy] = None
+		corr_vars["weight_sum"] = i
+		corr_vars["n"] = n
+		hist_file.Write()
+		hist_file.Close()
+		root_inf.Close()
+		config["correlations"] = corr_vars
+		jsonTools.JsonDict(config).save(os.path.join(nick_path,"%s_corr-config.json"%config["channel"]),indent=4)
