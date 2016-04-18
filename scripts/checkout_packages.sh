432b9c846f8727d345e6885b2ea610aef6063b53
Raphael Friese <raphael.friese@cern.ch>
2016-04-18 10:15:46 +0200

    Checkout recipe for CMSSW_8_0_X with gcc493

--- scripts/checkout_packages.sh
+++ scripts/checkout_packages.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e # exit on errors
 
-export SCRAM_ARCH=slc6_amd64_gcc481
+export SCRAM_ARCH=slc6_amd64_gcc493
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
 # set up CMSSW release area
-scramv1 project CMSSW CMSSW_7_1_5; cd CMSSW_7_1_5/src # slc6 # Combine requires this version
+scramv1 project CMSSW CMSSW_8_0_4; cd CMSSW_8_0_4/src # slc6 # Combine requires this version
 eval `scramv1 runtime -sh`
 
 # JEC
-git cms-addpkg CondFormats/JetMETObjects
+#git cms-addpkg CondFormats/JetMETObjects
 
 # From Kappa, only the DataFormats are needed
 # Mind that for certain skims, you need exactly the Kappa git tag that has been used for the production
@@ -33,7 +33,8 @@ git clone https://github.com/artus-analysis/Artus.wiki.git Artus/Core/doc/wiki
 # checkout KITHiggsToTauTau CMSSW analysis package
 git clone https://github.com/cms-analysis/HiggsAnalysis-KITHiggsToTauTau HiggsAnalysis/KITHiggsToTauTau -b Kappa_2_1
 git clone https://github.com/cms-analysis/HiggsAnalysis-KITHiggsToTauTau.wiki.git HiggsAnalysis/KITHiggsToTauTau/doc/wiki
-svn co https://ekptrac.physik.uni-karlsruhe.de/svn/KITHiggsToTauTau-auxiliaries/trunk HiggsAnalysis/KITHiggsToTauTau/auxiliaries
+# currently not needed
+#svn co https://ekptrac.physik.uni-karlsruhe.de/svn/KITHiggsToTauTau-auxiliaries/trunk HiggsAnalysis/KITHiggsToTauTau/auxiliaries
 
 # Svfit and HHKinFit
 git clone https://github.com/veelken/SVfit_standalone.git TauAnalysis/SVfitStandalone
@@ -56,22 +57,13 @@ git clone https://github.com/cms-analysis/CombineHarvester.git CombineHarvester
 #### --change to the recommendation of Combind Twiki 
 ##https://twiki.cern.ch/twiki/bin/viewauth/CMS/SWGuideHiggsAnalysisCombinedLimit#ROOT5_SLC6_release_CMSSW_7_1_X
 ##git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git -b slc6-root5.34.17 HiggsAnalysis/CombinedLimit
-git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
-cd HiggsAnalysis/CombinedLimit
-git fetch origin
-git checkout v5.0.3
-cd -
-
+git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit -b v6.1.1
 
 # needed for error propagation e.g. in the background estimations
 git clone https://github.com/lebigot/uncertainties.git -b 2.4.6.1 HiggsAnalysis/KITHiggsToTauTau/python/uncertainties
 
-# install TauSpinner is not needed anymore (since now it used TauSpinner from /cvmfs )
-## git clone https://github.com/rfriese/TauSpinnerSetup
-## python TauSpinnerSetup/checkoutPackagesForTauSpinner.py --tauolaversion=1.1.5
-
 # Grid-Control
-svn co https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control -r 1501
+svn co https://ekptrac.physik.uni-karlsruhe.de/svn/grid-control/trunk/grid-control -r 1701
 
 # source ini script, needs to be done in every new shell
 source HiggsAnalysis/KITHiggsToTauTau/scripts/ini_KITHiggsToTauTauAnalysis.sh
