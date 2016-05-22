0c6cfce8a3b582b8816933962c7c6af45e355300
Thomas Mueller <t.muller@cern.ch>
2016-05-22 21:25:13 +0200

    makePlots_datacardsCPStudies.py: cleanup of code

--- scripts/makePlots_datacardsCPStudies.py
+++ scripts/makePlots_datacardsCPStudies.py
@@ -27,7 +27,7 @@ def _call_command(command):
 
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser(description="Create ROOT inputs and datacards for SM HTT analysis.",
+	parser = argparse.ArgumentParser(description="Create ROOT inputs and datacards for CP studies.",
 	                                 parents=[logger.loggingParser])
 
 	parser.add_argument("-i", "--input-dir", required=True,
@@ -49,8 +49,6 @@ if __name__ == "__main__":
 	                    help="Add bin-by-bin uncertainties. [Default: %(default)s]")
 	parser.add_argument("--lumi", type=float, default=2.301,
 	                    help="Luminosity for the given data in fb^(-1). [Default: %(default)s]")
-	parser.add_argument("--for-dcsync", action="store_true", default=False,
-	                    help="Produces simplified datacards for the synchronization exercise. [Default: %(default)s]")
 	parser.add_argument("-w", "--weight", default="1.0",
 	                    help="Additional weight (cut) expression. [Default: %(default)s]")
 	parser.add_argument("--analysis-modules", default=[], nargs="+",
@@ -70,30 +68,30 @@ if __name__ == "__main__":
 	                    help="Delete/clear output directory before running this script. [Default: %(default)s]")
 	parser.add_argument("--scale-lumi", default=False,
                         help="Scale datacard to luminosity specified. [Default: %(default)s]")
-	
+
 	args = parser.parse_args()
 	logger.initLogger(args)
-	
+
 	args.output_dir = os.path.abspath(os.path.expandvars(args.output_dir))
 	if args.clear_output_dir and os.path.exists(args.output_dir):
 		logger.subprocessCall("rm -r " + args.output_dir, shell=True)
-		
+
 	# preparation of CP mixing angles alpha_tau/(pi/2)
 	cp_mixing_angles_over_pi_half = ["{mixing:03d}".format(mixing=int(mixing*100)) for mixing in args.cp_mixings]
 	cp_mixing_angles_over_pi_half_str=[str(mixing) for mixing in cp_mixing_angles_over_pi_half]
-	
+
 	# initialisations for plotting
 	sample_settings = samples.Samples()
 	binnings_settings = binnings.BinningsDict()
 	systematics_factory = systematics.SystematicsFactory()
-	
+
 	plot_configs = []
 	output_files = []
 	merged_output_files = []
 	hadd_commands = []
-	
+
 	datacards = cpstudiesdatacards.CPStudiesDatacards(cp_mixing_angles_over_pi_half_str)
-	
+
 	# initialise datacards
 	tmp_input_root_filename_template = "input/${ANALYSIS}_${CHANNEL}_${BIN}_${SYSTEMATIC}_${ERA}.root"
 	input_root_filename_template = "input/${ANALYSIS}_${CHANNEL}_${BIN}_${ERA}.root"
@@ -103,9 +101,7 @@ if __name__ == "__main__":
 	sig_syst_histogram_name_template = "${BIN}/${PROCESS}_${MASS}_${SYSTEMATIC}"
 	datacard_filename_templates = datacards.configs.htt_datacard_filename_templates
 	output_root_filename_template = "datacards/common/${ANALYSIS}.input_${ERA}.root"
-	if args.for_dcsync:
-		output_root_filename_template = "datacards/common/${ANALYSIS}.inputs-sm-${ERA}-mvis.root"
-	
+
 	# prepare channel settings based on args and datacards
 	if args.channel != parser.get_default("channel"):
 		args.channel = args.channel[1:]
@@ -113,39 +109,30 @@ if __name__ == "__main__":
 		args.channel = datacards.cb.channel_set()
 	else:
 		args.channel = list(set(args.channel).intersection(set(datacards.cb.channel_set())))
-	
+
 	# restrict CombineHarvester to configured channels:
 	datacards.cb.channel(args.channel)
-	
+
 	if args.categories != parser.get_default("categories"):
 		args.categories = args.categories[1:]
 	args.categories = (args.categories * len(args.channel))[:len(args.channel)]
 	for index, (channel, categories) in enumerate(zip(args.channel, args.categories)):
-		
+
 		# prepare category settings based on args and datacards
 		if (len(categories) == 1) and (categories[0] == "all"):
 			categories = datacards.cb.cp().channel([channel]).bin_set()
 		else:
 			categories = list(set(categories).intersection(set(datacards.cb.cp().channel([channel]).bin_set())))
 		args.categories[index] = categories
-		
+
 		# restrict CombineHarvester to configured categories:
 		datacards.cb.FilterAll(lambda obj : (obj.channel() == channel) and (obj.bin() not in categories))
-		
+
 		for category in categories:
 			datacards_per_channel_category = cpstudiesdatacards.CPStudiesDatacards(cb=datacards.cb.cp().channel([channel]).bin([category]))
 			
-			exclude_cuts = []
-			if args.for_dcsync:
-				if category[3:] == 'inclusive':
-					exclude_cuts=["mt", "pzeta"]
-				elif category[3:] == 'inclusivemt40':
-					exclude_cuts=["pzeta"]
-				
-				datacards_per_channel_category = cpstudiesdatacards.CPstudiesDatacardsForSync(cb=datacards.cb.cp().channel([channel]).bin([category]))
-			
 			higgs_masses = args.higgs_masses
-			
+
 			output_file = os.path.join(args.output_dir, input_root_filename_template.replace("$", "").format(
 					ANALYSIS="htt",
 					CHANNEL=channel,
@@ -154,47 +141,48 @@ if __name__ == "__main__":
 			))
 			output_files.append(output_file)
 			tmp_output_files = []
-			
+
 			for shape_systematic, list_of_samples in datacards_per_channel_category.get_samples_per_shape_systematic().iteritems():
 				nominal = (shape_systematic == "nominal")
 				list_of_samples = ([] if nominal else []) + [datacards.configs.process2sample(process) for process in list_of_samples]
 				for shift_up in ([True] if nominal else [True, False]):
 					systematic = "nominal" if nominal else (shape_systematic + ("Up" if shift_up else "Down"))
-					
+
 					log.debug("Create inputs for (samples, systematic) = ([\"{samples}\"], {systematic}), (channel, category) = ({channel}, {category}).".format(
 							samples="\", \"".join(list_of_samples),
 							channel=channel,
 							category=category,
 							systematic=systematic
 					))
-					
+
 					# prepare plotting configs for retrieving the input histograms
-					config={}					
+					config={}
 					config_bkg = sample_settings.get_config(
-							     	samples=[getattr(samples.Samples, sample) for sample in list_of_samples if sample == "ztt" or sample == "zll"],
-							     	channel=channel,
-								category="catHtt13TeV_"+category,					     	
-								weight=args.weight,
-							     	lumi = args.lumi * 1000						     	
-						)
-					histogram_name_template = bkg_histogram_name_template if nominal else bkg_syst_histogram_name_template					
+							samples=[getattr(samples.Samples, sample) for sample in list_of_samples if sample == "ztt" or sample == "zll"],
+							channel=channel,
+							category="catHtt13TeV_"+category,
+							weight=args.weight,
+							lumi=args.lumi * 1000
+					)
+
+					histogram_name_template = bkg_histogram_name_template if nominal else bkg_syst_histogram_name_template
 					config_bkg["labels"] = [histogram_name_template.replace("$", "").format(
 						PROCESS=datacards.configs.sample2process(sample),
 						BIN=category,
 						SYSTEMATIC=systematic
 					) for sample in config_bkg["labels"]]
-					config_bkg["x_expressions"] = [args.quantity]
-					config = samples.Samples.merge_configs(config, config_bkg)				
+					config = samples.Samples.merge_configs(config, config_bkg)
+					
 					for cp_mixing_angle_over_pi_half in cp_mixing_angles_over_pi_half_str:
 
 						config_sig = sample_settings.get_config(
-							   	samples=[getattr(samples.Samples, sample) for sample in list_of_samples if sample == "qqh" or sample == "ggh"],
-								channel=channel,							     	
+								samples=[getattr(samples.Samples, sample) for sample in list_of_samples if sample == "qqh" or sample == "ggh"],
+								channel=channel,
 								category="catHtt13TeV_"+category,
 								nick_suffix="_" + cp_mixing_angle_over_pi_half,
 								weight=args.weight+"*"+"tauSpinnerWeightInvSample"+"*tauSpinnerWeight"+cp_mixing_angle_over_pi_half,
-							     	lumi = args.lumi * 1000,
-								higgs_masses=higgs_masses 	
+								lumi = args.lumi * 1000,
+								higgs_masses=higgs_masses
 						)
 						histogram_name_template = sig_histogram_name_template if nominal else sig_syst_histogram_name_template
 						config_sig["labels"] = [histogram_name_template.replace("$", "").format(
@@ -203,28 +191,24 @@ if __name__ == "__main__":
 								MASS=cp_mixing_angle_over_pi_half,
 								SYSTEMATIC=systematic
 						) for sample in config_sig["labels"]]
-						config_sig["x_expressions"] = [args.quantity]						
-						binnings_key = "binningHtt13TeV_"+category+"_svfitMass"
-						if binnings_key in binnings_settings.binnings_dict:
-							config["x_bins"] = [binnings_key]
-						else:
-							config["x_bins"] = ["30,0.0,6.28"]						
 
 						config = samples.Samples.merge_configs(config, config_sig)
-						
-					
+
+
 					systematics_settings = systematics_factory.get(shape_systematic)(config)
 					# TODO: evaluate shift from datacards_per_channel_category.cb
 					config = systematics_settings.get_config(shift=(0.0 if nominal else (1.0 if shift_up else -1.0)))
-										
+
+					config["x_expressions"] = [args.quantity]
+					
 					binnings_key = "binningHtt13TeV_"+category+"_svfitMass"
 					if binnings_key in binnings_settings.binnings_dict:
 						config["x_bins"] = [binnings_key]
 					else:
 						config["x_bins"] = ["30,0.0,6.28"]
-					
+
 					config["directories"] = [args.input_dir]
-					
+
 					tmp_output_file = os.path.join(args.output_dir, tmp_input_root_filename_template.replace("$", "").format(
 							ANALYSIS="htt",
 							CHANNEL=channel,
@@ -235,44 +219,24 @@ if __name__ == "__main__":
 					tmp_output_files.append(tmp_output_file)
 					config["output_dir"] = os.path.dirname(tmp_output_file)
 					config["filename"] = os.path.splitext(os.path.basename(tmp_output_file))[0]
-				
+
 					config["plot_modules"] = ["ExportRoot"]
 					config["file_mode"] = "UPDATE"
-			
+
 					if "legend_markers" in config:
 						config.pop("legend_markers")
-					
-					if args.for_dcsync:
-						config["ttbar_from_mc"] = True
-						config["wjets_from_mc"] = True
-					
+
 					plot_configs.append(config)
-			
+
 			hadd_commands.append("hadd -f {DST} {SRC} && rm {SRC}".format(
 					DST=output_file,
 					SRC=" ".join(tmp_output_files)
 			))
-		if args.for_dcsync:
-			merged_input_root_filename_template = "input/${ANALYSIS}_${CHANNEL}.inputs-sm-${ERA}-mvis.root"
-			merged_output_file = os.path.join(args.output_dir, merged_input_root_filename_template.replace("$", "").format(
-					ANALYSIS="htt",
-					CHANNEL=channel,
-					ERA="13TeV"
-			))
-			hadd_commands.append("hadd -f {DST} {SRC} && rm {SRC}".format(
-					DST=merged_output_file,
-					SRC=" ".join(output_files)
-			))
-			output_files = []
-			merged_output_files.append(merged_output_file)
-	
-	if args.for_dcsync:
-		input_root_filename_template = "input/${ANALYSIS}_${CHANNEL}.inputs-sm-${ERA}-mvis.root"
-	
-	#if log.isEnabledFor(logging.DEBUG):
-	#	import pprint
-	#	pprint.pprint(plot_configs)
-	
+
+	if log.isEnabledFor(logging.DEBUG):
+		import pprint
+		pprint.pprint(plot_configs)
+
 	# delete existing output files
 	tmp_output_files = list(set([os.path.join(config["output_dir"], config["filename"]+".root") for config in plot_configs[:args.n_plots[0]]]))
 	for output_file in tmp_output_files:
@@ -280,17 +244,16 @@ if __name__ == "__main__":
 			os.remove(output_file)
 			log.debug("Removed file \""+output_file+"\" before it is recreated again.")
 	output_files = list(set(output_files))
-	import pprint	
-	pprint.pprint(plot_configs[0])
+	
 	# create input histograms with HarryPlotter
 	higgsplot.HiggsPlotter(list_of_config_dicts=plot_configs, list_of_args_strings=[args.args], n_processes=args.n_processes, n_plots=args.n_plots[0])
 	tools.parallelize(_call_command, hadd_commands, n_processes=args.n_processes)
-	
+
 	debug_plot_configs = []
-	for output_file in (output_files if not args.for_dcsync else merged_output_files):
+	for output_file in  merged_output_files:
 		debug_plot_configs.extend(plotconfigs.PlotConfigs().all_histograms(output_file, plot_config_template={"markers":["E"], "colors":["#FF0000"]}))
 	higgsplot.HiggsPlotter(list_of_config_dicts=debug_plot_configs, list_of_args_strings=[args.args], n_processes=args.n_processes, n_plots=args.n_plots[0])
-	
+
 	# update CombineHarvester with the yields and shapes
 	datacards.extract_shapes(
 			os.path.join(args.output_dir, input_root_filename_template.replace("$", "")),
@@ -305,10 +268,9 @@ if __name__ == "__main__":
 				add_threshold=0.1, merge_threshold=0.5, fix_norm=True
 		)
 	
-
 	# scale
 	if(args.scale_lumi):
-		datacards.scale_expectation( float(args.scale_lumi) / args.lumi)
+		datacards.scale_expectation(float(args.scale_lumi) / args.lumi)
 
 	# write datacards and call text2workspace
 	datacards_cbs = {}
@@ -318,7 +280,7 @@ if __name__ == "__main__":
 				output_root_filename_template.replace("{", "").replace("}", ""),
 				args.output_dir
 		))
-	
+
 	datacards_poi_ranges = {}
 	for datacard, cb in datacards_cbs.iteritems():
 		channels = cb.channel_set()
@@ -333,11 +295,11 @@ if __name__ == "__main__":
 				datacards_poi_ranges[datacard] = [-50.0, 50.0]
 			else:
 				datacards_poi_ranges[datacard] = [-25.0, 25.0]
-	
+
 	datacards_workspaces = datacards.text2workspace(datacards_cbs, n_processes=args.n_processes)
-	
+
 	annotation_replacements = {channel : index for index, channel in enumerate(["combined", "tt", "mt", "et", "em"])}
-	
+
 	# Max. likelihood fit and postfit plots
 	#stable_options = "--robustFit=1 --preFitValue=1. --X-rtd FITTER_NEW_CROSSING_ALGO --minimizerAlgoForMinos=Minuit2 --minimizerToleranceForMinos=0.1 --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND --minimizerAlgo=Minuit2 --minimizerStrategy=0 --minimizerTolerance=0.1 --cminFallbackAlgo \"Minuit2,0:1.\""
 	#datacards.combine(datacards_cbs, datacards_workspaces, datacards_poi_ranges, args.n_processes, "-M MaxLikelihoodFit {stable} -n \"\"".format(stable=stable_options))
@@ -361,11 +323,11 @@ if __name__ == "__main__":
 			#args.n_processes,
 			#"-t limit -b channel"
 	#)
-	
+
 	# Asymptotic limits
 	datacards.combine(datacards_cbs, datacards_workspaces, None, args.n_processes, "--expectSignal=1 -t -1 -M Asymptotic -n \"\"")
 	#datacards.combine(datacards_cbs, datacards_workspaces, None, args.n_processes, "--expectSignal=1 -t -1 --significance -n \"\"")
-	
+
 	"""
 	# cV-cF scan
 	cv_cf_datacards_workspaces = datacards.text2workspace(
