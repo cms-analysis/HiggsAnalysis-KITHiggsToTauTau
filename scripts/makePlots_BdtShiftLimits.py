a9b22fbac4a964ed5c27bab2b6568396b9a1f5f5
MarcusSchmitt <Marcus.Schmitt90@gmail.com>
2016-06-01 14:07:14 +0200

    MVALimit Plotting update

--- scripts/makePlots_BdtShiftLimits.py
+++ scripts/makePlots_BdtShiftLimits.py
@@ -41,7 +41,15 @@ def get_limit(root_file, file_type="asymptotic"):
 				up = line.r
 				break
 			counter += 1
-	#print down, mid, up
+	elif file_type == "profile":
+		sigs = []
+		for line in chain:
+			if line.limit != 0.0:
+				sigs.append(line.limit)
+		mid = np.percentile(sigs, 50)
+		down = np.percentile(sigs, 16)
+		up = np.percentile(sigs, 84)
+	print root_file, (down, mid, up)
 	return (down,mid,up)
 
 if __name__ == "__main__":
@@ -56,12 +64,14 @@ if __name__ == "__main__":
 						help="labels for the legend, has to be specified as often as --input-dirs. [Default%(default)s]")
 	parser.add_argument("-c", "--colors", nargs = "+", default=["blue"],
 						help="colors for the legend and curves, has to be specified as often as --input-dirs. [Default%(default)s]")
-	parser.add_argument("--limits-type", default="asymptotic", choices=["asymptotic", "multidim"],
+	parser.add_argument("--limits-type", default="asymptotic", choices=["asymptotic", "multidim", "profile", "maxlikelihood"],
 						help="use one of the following limit types: [Default:%(default)s, choices:%(choices)s]")
 	parser.add_argument("--base-line", default=None,
 						help="use the limit extracted from this workspace as baseline for the others: other-this")
 	parser.add_argument("--channel", default="mt",
 						help="channels. [Default%(default)s]")
+	parser.add_argument("--lumi-mode", action="store_true", default=False, help="do lumi projection")
+	parser.add_argument("--lumi-list", nargs="+", default=[], help="lumi values")
 	parser.add_argument("-m", "--mass", type=int, default=125, help="higgs mass")
 	parser.add_argument("-o", "--output_file", default="limit_collection", help="output file")
 
@@ -91,8 +101,15 @@ if __name__ == "__main__":
 	search_pattern = "{mass}/*Combine*Asymp*".format(mass=args.mass)
 	if args.limits_type == "multidim":
 		search_pattern = "{mass}/*Combine*MultiDim*".format(mass=args.mass)
+	elif args.limits_type == "profile":
+		search_pattern = "{mass}/*Combine*ProfileLikelihood*".format(mass=args.mass)
+	elif args.limits_type == "maxlikelihood":
+		search_pattern = "{mass}/*Combine*MaxLikelihoodFit*".format(mass=args.mass)
+
 	#print args.input_dirs, args.labels, args.colors, args.diff_dirs, args.unc_files
-	for (in_dirs, label, color, diff_dirs) in zip(args.input_dirs, args.labels, args.colors, args.diff_dirs):
+	for conf_i, (in_dirs, diff_dirs) in enumerate(zip(args.input_dirs, args.diff_dirs)):
+		label = args.labels[conf_i%len(args.labels)]
+		color = args.colors[conf_i%len(args.colors)]
 		values = {}
 		file_list = []
 		diff_list = []
@@ -107,7 +124,7 @@ if __name__ == "__main__":
 			if not name in x_names:
 				x_names.append(os.path.split(path)[1])
 			values[name]={"categories":{}}
-			values[name]["combined_diff"] = get_limit(diff_file, args.limits_type)
+			values[name]["combined_diff"] = get_limit(diff_file, args.limits_type) if calculate_diff else (0,0,0)
 			values[name]["combined"] = get_limit(in_file, args.limits_type)
 			values[name]["x_val"] = x_names.index(name)
 			d = os.path.join(path, "datacards/individual/")
@@ -128,24 +145,72 @@ if __name__ == "__main__":
 				values[name]["categories"][split_name]["value"] = get_limit(cat, args.limits_type)
 		values_list.append(values)
 		#print values
-		x_values = np.array([values[name]["x_val"] for name in x_names])
 		lower = np.array([values[name]["combined"][0]-values[name]["combined_diff"][0] for name in x_names])
 		upper = np.array([values[name]["combined"][2]-values[name]["combined_diff"][2] for name in x_names])
-		y_values = upper - lower
-		un_value = []
-		for i,cat in enumerate(categories_names):
-			if i == 0:
-				unc_value = np.array([values[name]["categories"][cat]["unc"] for name in x_names])**2
-			unc_value += np.array([values[name]["categories"][cat]["unc"] for name in x_names])**2
-		unc_value = unc_value**0.5
-		if args.limits_type == "asymptotic":
-			y_values = np.array([values[name]["combined"][1]-values[name]["combined_diff"][1] for name in x_names])
-		ax.errorbar(x_values,y_values, yerr= [y_values-lower, upper-y_values], marker="x", label=label, color = color, markersize = 12.5, ls = "None", markeredgewidth=2)
-
+		x_values = np.array([values[name]["x_val"] for name in x_names])
+		#y_values = upper - lower
+		y_values = np.array([values[name]["combined"][1]-values[name]["combined_diff"][1] for name in x_names])
+		if not args.lumi_mode:
+			if args.limits_type == "asymptotic":
+				ax.errorbar(x_values,y_values, yerr= [y_values-lower, upper-y_values], marker="x", label=label, color = color, markersize = 12.5, ls = "None", markeredgewidth=2)
+				ax.set_ylabel("asympt. limit")
+			elif args.limits_type == "profile":
+				y_values = np.array([values[name]["combined"][1]-values[name]["combined_diff"][1] for name in x_names])
+				ax.plot(x_values, y_values, marker="x", label=label, color = color, markersize = 12.5, ls = "None", markeredgewidth=2)
+				ax.set_ylabel("significance")
 		#if args.limits_type == "asymptotic":
 
 		#elif args.limits_type == "multidim":
-
+	if args.lumi_mode:
+		save_values = {}
+		y_dict = {}
+		y_up_dict = {}
+		y_down_dict = {}
+		x_values_list = []
+		full_y_values = [[values[name]["combined"][1]-values[name]["combined_diff"][1] for name in x_names] for values in values_list]
+		full_y_up = [[values[name]["combined"][2]-values[name]["combined_diff"][2] for name in x_names] for values in values_list]
+		full_y_down =[[values[name]["combined"][0]-values[name]["combined_diff"][0] for name in x_names] for values in values_list]
+		for i,lumi in enumerate(args.lumi_list):
+			if not lumi in y_dict.keys():
+				y_dict[lumi] = full_y_values[i]
+				y_up_dict[lumi] = full_y_up[i]
+				y_down_dict[lumi] = full_y_down[i]
+			else:
+				y_dict[lumi] += full_y_values[i]
+				y_up_dict[lumi] += full_y_up[i]
+				y_down_dict[lumi] += full_y_down[i]
+			if not lumi in x_values_list:
+				x_values_list.append(lumi)
+		y_values_amount = len(y_dict[args.lumi_list[0]])
+		y_values_list = [[] for i in range(y_values_amount)]
+		y_values_up = [[] for i in range(y_values_amount)]
+		y_values_down = [[] for i in range(y_values_amount)]
+		for lumi_key in x_values_list:
+			for i in range(y_values_amount):
+				y_values_list[i].append(y_dict[lumi_key][i])
+				y_values_up[i].append(y_up_dict[lumi_key][i])
+				y_values_down[i].append(y_down_dict[lumi_key][i])
+		x_values_list = [float(x) for x in x_values_list]
+		if args.limits_type == "asymptotic":
+			ax.errorbar(x_values,y_values, yerr= [y_values-lower, upper-y_values], marker="x", label=label, color = color, markersize = 12.5, ls = "None", markeredgewidth=2)
+			ax.set_ylabel("asympt. limit")
+		elif args.limits_type == "profile":
+			for counter,(y_values, label, color, downs, ups) in enumerate(zip(y_values_list, args.labels, args.colors, y_values_down, y_values_up)):
+				#y_values = np.array([values[name]["combined"][1]-values[name]["combined_diff"][1] for name in x_names])
+				save_values[label]={"x-lumi": x_values_list, "y-values":y_values, "upper":ups, "lower":downs, "shift_up":list(np.array(ups)-np.array(y_values)), "shift_down":list(np.array(downs)-np.array(y_values)), "label":label}
+				ax.errorbar(np.array(x_values_list)+counter*0.1, y_values, yerr=[np.array(y_values)-np.array(downs), np.array(ups)-np.array(y_values)], marker="x", label=label, color = color, markersize = 12.5, ls = "None", markeredgewidth=2)
+				#ax.plot(x_values_list, y_values, marker="x", label=label, color = color, markersize = 12.5, ls = "None", markeredgewidth=2)
+				#ax.plot(x_values_list, np.array(ups)-np.array(downs), marker="_", label="size 68% range", color = color, markersize = 12.5, ls = "None", markeredgewidth=2)
+			ax.set_ylabel("significance", size='x-large')
+			ax.set_xlabel("x times current lumi", size='x-large')
+		jsonTools.JsonDict(save_values).save("%s_Values.txt"%args.output_file, indent=4)
+	ax.set_xticks(x_values_list)
+	ax.set_xlim(0, 1.2*max(x_values_list))
+	ax.set_xticklabels([str("%1.1f"%x) for x in x_values_list], size='large')
+	lgd = ax.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0., numpoints=1)
+	plt.savefig("%s.png"%args.output_file, bbox_extra_artists=(lgd,), bbox_inches='tight')
+	plt.savefig("%s.pdf"%args.output_file, bbox_extra_artists=(lgd,), bbox_inches='tight')
+	log.info("create plot %s.png"%args.output_file)
 
 	#print values_list
 
@@ -197,14 +262,9 @@ if __name__ == "__main__":
 
 
 	##if not args.bdt_uncertainties:
-		##ax.set_xticks([i for i in range(0,len(x_names))])
-		##ax.set_xlim(-0.5, len(x_names)+0.2*len(x_names))
 		##x_names = [name.replace("ggh_xxh", "reg").replace("_all", "").replace("qqh", "vbf") for name in x_names]
-		##ax.set_xticklabels(x_names, rotation = 90, size='x-large', va='center', ha='right', rotation_mode='anchor')
-		##ax.set_ylabel("Limit")
 		##if calculate_diff:
 	#ax.set_ylabel("$\\Delta$Limits")
-	lgd = ax.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.)
 	##else:
 		##os.path.exists(args.bdt_uncertainties)
 		##unc_dict = jsonTools.JsonDict(args.bdt_uncertainties)
@@ -215,8 +275,6 @@ if __name__ == "__main__":
 
 
 	##plt.tight_layout()
-	plt.savefig("%s.png"%args.output_file, bbox_extra_artists=(lgd,), bbox_inches='tight')
-	log.info("create plot %s.png"%args.output_file)
 	##plt.savefig("%s.pdf"%args.output_file)
 	##log.info("create plot %s.pdf"%args.output_file)
 	##plt.savefig("%s.eps"%args.output_file)
