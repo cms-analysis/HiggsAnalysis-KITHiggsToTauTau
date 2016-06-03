10657d7a5836dce482b3faf7f3aafafad510e9ba
MarcusSchmitt <Marcus.Schmitt@cern.ch>
2016-06-03 10:19:45 +0200

    expand PoissonYield to up to TH3

--- python/tools.py
+++ python/tools.py
@@ -14,19 +14,32 @@ from Artus.Utility.tools import find_common_patterns
 class PoissonYield(object):
 	def __init__(self, root_histogram):
 		error = array.array("d", [0.0])
-		integral = root_histogram.IntegralAndError(0, root_histogram.GetNbinsX()+1, error)
+		dims = root_histogram.GetDimension()
+		integral_ranges=[]
+		for dim in range(dims):
+			if dim==0:
+				integral_ranges.append(0)
+				integral_ranges.append(root_histogram.GetNbinsX()+1)
+			elif dim==1:
+				integral_ranges.append(0)
+				integral_ranges.append(root_histogram.GetNbinsY()+1)
+			elif dim==2:
+				integral_ranges.append(0)
+				integral_ranges.append(root_histogram.GetNbinsZ()+1)
+		integral_ranges.append(error)
+		integral = root_histogram.IntegralAndError(*integral_ranges)
 		error = error[0]
 		#self.poisson_yield = uncertainties.ufloat(integral, error)
 		self.poisson_yield = uncertainties.ufloat(integral, math.sqrt(abs(integral)))
-	
+
 	def __call__(self):
 		return self.poisson_yield
-	
+
 def find_common_httpipename(s1, s2):
 	string = ""
 	for pattern in find_common_patterns(s1, s2)[0]:
 		if pattern[0] > 0:
-			string += pattern[1] if pattern[1][-2:] != "_z" else pattern[1][:-2]  
+			string += pattern[1] if pattern[1][-2:] != "_z" else pattern[1][:-2]
 		else:
 			string += "Nom" if pattern[1] in ["Up","Down","Nom"] else ""
 	return string
