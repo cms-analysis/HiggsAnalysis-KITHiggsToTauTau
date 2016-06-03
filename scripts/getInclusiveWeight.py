bea4caa1c26a37deae7b5670810f9030be8d3425
Raphael Friese <raphael.friese@cern.ch>
2016-06-03 11:16:04 +0200

    finish theory unc. script

--- scripts/getInclusiveWeight.py
+++ scripts/getInclusiveWeight.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from array import array
 import logging
 import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
 import argparse
 import os
-import re
-from datetime import datetime
+import sys
 import math
 import ROOT
+
 ROOT.gROOT.SetBatch(True)
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 ROOT.gErrorIgnoreLevel = ROOT.kError
@@ -43,8 +44,8 @@ def count(file_name, inclusive_weight, channel):
 	weights = {}
 	for leave in list_of_leaves:
 		if("NNPDF30" in leave.GetTitle() or "muR" in leave.GetTitle()):
-	#	if("muR" in leave.GetTitle()):
-			weight_names.append(str(leave.GetTitle()))
+			if(not "muR0p5_muF0p5" in leave.GetTitle() and not "muR2p0_muF2p0" in leave.GetTitle()):
+				weight_names.append(str(leave.GetTitle()))
 	for index in range(len(weight_names)):
 		eventTree.Draw("1", weight_names[index] + "*" +  inclusive_weight)
 		hist = ROOT.gPad.GetPrimitive("htemp") 
@@ -58,6 +59,38 @@ def get_channel_string(ichannel):
 		if ichannel in channel:
 			return channel
 
+def fill_histogram(unc, unctype):
+	new_unc = {}
+	from collections import OrderedDict
+	for key, value in unc.iteritems():
+		if "_inclusive" in key:
+			continue
+		if unctype in key:
+			new_unc[key] = value
+	new_histo = ROOT.TH1F(unctype, "", len(new_unc), 0, len(new_unc))
+	i = 1
+	for key, value in sorted(new_unc.iteritems()):
+		new_histo.SetBinContent(i, value)
+		new_histo.GetXaxis().SetBinLabel(i, key)
+		i=i+1
+	new_histo.Write()
+
+def acceptance_to_tree(A, method, channel, category="fullinclusive"):
+	name = "_".join([method, channel, category])
+	acceptance_tree = ROOT.TTree(name, name)
+	acceptance=array('d', [0])
+	acceptance_tree.Branch('acceptance', acceptance, 'acceptance/D')
+	for a in A.values():
+		acceptance[0] = float(a)
+		acceptance_tree.Fill()
+	acceptance_tree.Write()
+
+def save_as_histograms(unc, root_tree_file):
+	alphas = fill_histogram(unc, "alphas")
+	scale = fill_histogram(unc, "scale")
+	pdf = fill_histogram(unc, "pdf")
+	root_tree_file.Write()
+
 def main():
 	
 	ROOT.gSystem.Load(os.path.expandvars("$CMSSW_BASE/src/Kappa/lib/libKappa"))
@@ -68,13 +101,14 @@ def main():
 	parser.add_argument("--full-file", help="Input files.")
 	parser.add_argument("--inclusive-file", help="Input files.")
 	parser.add_argument("--channels", default=["et", "mt", "tt", "em"], nargs="+", help="Channels. [Default: %(default)s]")
-	parser.add_argument("--categories", default=["inclusive", "0jet_low", "0jet_high", "1jet_low", "1jet_high", "2jet_vbf"], nargs="+", help="Channels. [Default: %(default)s]")
+	parser.add_argument("--categories", default=["0jet_low", "0jet_high", "1jet_low", "1jet_high", "2jet_vbf"], nargs="+", help="Channels. [Default: %(default)s]")
+	parser.add_argument("--process", help="Process Name [Default: %(default)s]")
 
 	args = parser.parse_args()
 	logger.initLogger(args)
 	
 	full_dict = {}
-
+	args.categories = ["inclusive"] + args.categories
 
 	#inclusive
 	for channel in args.channels:
@@ -92,18 +126,17 @@ def main():
 		for category in args.categories:
 			lookup_string = "catHtt13TeV_"+channel+"_"+category
 			weight_string = "(" + ExpressionsDict.static_get_expression(lookup_string) + ")"
-			weight_string += "*(eventWeight)"
+			#weight_string += "*puWeight"
 			# add baseline selection string
 			cutstrings = CutStringsDict.baseline(channel, "sm")
 			del cutstrings["blind"]
 			for cutstring in cutstrings.values():
 				weight_string += "*" + cutstring
 			full_dict[channel][category] = count(args.inclusive_file, weight_string, get_channel_string(channel))
-			#print "channel:category:weight_string:count:" + channel + "," +category + "," + weight_string 
-			#print str(full_dict[channel][category] )
 
 
 	unc = {}
+	root_tree_file=ROOT.TFile("theory_uncertainties_" + args.process+".root","RECREATE")
 	######## calculate inclusive weights
 	# scale
 	for channel in args.channels:
@@ -111,14 +144,15 @@ def main():
 		for weight in [weight for weight in full_dict[channel]["full"] if "muR" in weight]:
 			A[weight] = full_dict[channel]["fullinclusive"][weight] / full_dict[channel]["full"][weight]
 		unc["scale_" + channel + "_fullinclusive"] = (max(A.values()) - min(A.values()))/2. / A["muR1p0_muF1p0_weight"]
+		acceptance_to_tree(A, "scale", channel)
 
 	# PDF Sets
 	for channel in args.channels:
 		A = {}
 		for weight in [weight for weight in full_dict[channel]["full"] if "NNPDF30_nlo_as_0118" in weight ]:
 			A[weight] = full_dict[channel]["fullinclusive"][weight] / full_dict[channel]["full"][weight]
-#		print "rms: " + str(rms(A.values())) + ", mse: " + str(mse(A.values()))
 		unc["pdf_" + channel + "_fullinclusive"] = mse(A.values()) / rms(A.values()) 
+		acceptance_to_tree(A, "pdf", channel)
 
 	#alpha_s
 	for channel in args.channels:
@@ -126,6 +160,7 @@ def main():
 		for weight in ['NNPDF30_nlo_as_0119_weight','NNPDF30_nlo_as_0118_00_weight','NNPDF30_nlo_as_0117_weight' ]:
 			A[weight] = full_dict[channel]["fullinclusive"][weight] / full_dict[channel]["full"][weight]
 		unc["alphas_" + channel + "_fullinclusive"] = (max(A.values()) - min(A.values()))/2. / A["NNPDF30_nlo_as_0118_00_weight"]
+		acceptance_to_tree(A, "alphas", channel)
 
 	####### calculate category uncertainties
 	# scale
@@ -135,13 +170,15 @@ def main():
 			for weight in [weight for weight in full_dict[channel][category] if "muR" in weight]:
 				A[weight] = full_dict[channel][category][weight] / full_dict[channel]["inclusive"][weight]
 			unc["scale_" + channel+"_"+category] = (max(A.values()) - min(A.values()))/2. / A["muR1p0_muF1p0_weight"]
+			acceptance_to_tree(A, "scale", channel, category)
 	# PDF Sets
 	for channel in args.channels:
 		for category in args.categories:
 			A = {}
 			for weight in [weight for weight in full_dict[channel][category] if "NNPDF30_nlo_as_0118" in weight ]:
 				A[weight] = full_dict[channel][category][weight] / full_dict[channel]["inclusive"][weight]
-				unc["pdf_" + channel+"_"+category] = mse(A.values()) / rms(A.values()) 
+			unc["pdf_" + channel+"_"+category] = mse(A.values()) / rms(A.values()) 
+			acceptance_to_tree(A, "pdf", channel, category)
 
 	#alpha_s
 	for channel in args.channels:
@@ -150,6 +187,11 @@ def main():
 			for weight in ['NNPDF30_nlo_as_0119_weight','NNPDF30_nlo_as_0118_00_weight','NNPDF30_nlo_as_0117_weight' ]:
 				A[weight] = full_dict[channel][category][weight] / full_dict[channel]["inclusive"][weight]
 			unc["alphas_" + channel+"_"+category] = (max(A.values()) - min(A.values()))/2. / A["NNPDF30_nlo_as_0118_00_weight"]
+			acceptance_to_tree(A, "alphas", channel, category)
+
+	#fulldict_to_tree(full_dict)
+	save_as_histograms(unc, root_tree_file)
+	root_tree_file.Close()
 	pprint.pprint(unc)
 
 
