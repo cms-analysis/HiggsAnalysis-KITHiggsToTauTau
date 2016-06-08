f165ce0c22ef47f7357775773c59194c94d36c67
Rene Caspart <rcaspart@cern.ch>
2016-06-08 16:35:50 +0200

    adapted crab configuration for svfit calculation to run all jobs in as few tasks as possible (up to 8000); also adapted merge script to these changes

--- scripts/submitCrabSvfitJobs.py
+++ scripts/submitCrabSvfitJobs.py
@@ -88,57 +88,57 @@ def submission(base_path):
 	from CRABClient.UserUtilities import config
 	config = config()
 	today=datetime.date.today().strftime("%Y-%m-%d")
-	allowed_paths = [f for f in glob(base_path+"/*") if not ("tar.gz" in f)]
-	for path in allowed_paths:
-		dataset_nick = path.split('/')[-1]
-		print "nick: " + dataset_nick
-		print "path: " +path
-		files =0
-		cache_files = [f for f in glob(path+"/*.root") if("SvfitCache" in f)]
-		if len(cache_files) == 0:
-			continue
-		jobfile_name = get_filename(today,dataset_nick)
-		jobfile = open(jobfile_name,"w+") 
-		jobfile.write("#!/bin/bash\n")
-		jobfile.write("declare -A arr\n")
-		jobfile.write(CRAB_PREFIX)
-		for index,file in enumerate(cache_files):
-			jobfile.write("arr[%s,0]=dcap://dcache-cms-dcap.desy.de/%s\n"%(index+1,file))
-		jobfile.write("if [ \"x$2\" != \"x\" ]; then\npushd %s\nSCRAM_ARCH=slc6_amd64_gcc493\nsource /cvmfs/cms.cern.ch/cmsset_default.sh\neval `scramv1 runtime -sh`\n"%(os.getcwd()))
-		jobfile.write("ComputeSvfit -i ${arr[$1,0]} -o SvfitCache.root\n")
-		jobfile.write("else\n./ComputeSvfit -i ${arr[$1,0]} -o SvfitCache.root\nfi\n")
-		jobfile.close()
-		
-		config.General.workArea = '/nfs/dust/cms/user/%s/%s/'%(getUsernameFromSiteDB(),today)
-		config.General.transferOutputs = True
-		config.General.transferLogs = True
-		jobname = "SvFit_"+today+"_"+dataset_nick.split("_MINIAOD")[0]
-		config.General.requestName = jobname
-		print jobname
-		config.Data.outputPrimaryDataset = 'Svfit'
-		config.Data.splitting = 'EventBased'
-		config.Data.unitsPerJob = 1
-		config.Data.totalUnits = len(cache_files)
-		config.Data.publication = False
-		config.Data.outputDatasetTag = config.General.requestName
-		config.Data.outLFNDirBase = '/store/user/%s/higgs-kit/Svfit/%s/%s/'%(getUsernameFromSiteDB(),today,dataset_nick)
-		print "outdir: " + config.Data.outLFNDirBase
-		config.Data.publication = False
-		
-		config.User.voGroup = 'dcms'
-		
-		config.JobType.pluginName = 'PrivateMC'
-		config.JobType.psetName = os.environ['CMSSW_BASE']+'/src/CombineHarvester/CombineTools/scripts/do_nothing_cfg.py'
-		config.JobType.inputFiles = ['Kappa/lib/libKappa.so', os.environ['CMSSW_BASE']+'/bin/'+os.environ['SCRAM_ARCH']+'/ComputeSvfit', jobfile_name]
-		config.JobType.allowUndistributedCMSSW = True
-		config.JobType.scriptExe = jobfile_name
-		config.JobType.outputFiles = ['SvfitCache.root']
+	files =0
+	cache_files = [f for f in glob(base_path+"/*/*.root") if("SvfitCache" in f)]
+	if len(cache_files) == 0:
+		continue
+	if len(cache_files > 8000):
+            cache_files = [cache_files[i:i+8000] for i in xrange(0,len(cache_files),8000)]
+        else:
+            cache_files = [cache_files]
+        for index,cache_file enumerate(cache_files):
+            jobfile_name = get_filename(today,index)
+            jobfile = open(jobfile_name,"w+")
+            jobfile.write("#!/bin/bash\n")
+            jobfile.write("declare -A arr\n")
+            jobfile.write(CRAB_PREFIX)
+            for index,file in enumerate(cache_file):
+                    jobfile.write("arr[%s,0]=dcap://dcache-cms-dcap.desy.de/%s\n"%(index+1,file))
+            jobfile.write("if [ \"x$2\" != \"x\" ]; then\npushd %s\nSCRAM_ARCH=slc6_amd64_gcc493\nsource /cvmfs/cms.cern.ch/cmsset_default.sh\neval `scramv1 runtime -sh`\n"%(os.getcwd()))
+            jobfile.write("ComputeSvfit -i ${arr[$1,0]} -o SvfitCache.root\n")
+            jobfile.write("else\n./ComputeSvfit -i ${arr[$1,0]} -o $(basename ${arr[$1,0]})\ntar -cf $(basename ${arr[$1,0]})\nfi\n")
+            jobfile.close()
 			
-		config.Site.storageSite = "T2_DE_DESY"
-			# config.Site.blacklist = ["T3_US_PuertoRico","T2_ES_CIEMAT","T2_DE_RWTH","T3_US_Colorado","T2_BR_UERJ","T2_ES_IFCA","T2_RU_JINR","T2_UA_KIPT","T2_EE_Estonia","T2_FR_GRIF_LLR","T2_CH_CERN","T2_FR_GRIF_LLR","T3_IT_Bologna","T2_US_Nebraska","T2_US_Nebraska","T3_TW_NTU_HEP","T2_US_Caltech","T3_US_Cornell","T2_IT_Legnaro","T2_HU_Budapest","T2_IT_Pisa","T2_US_Florida",'T2_IT_Bari',"T2_FR_GRIF_IRFU","T2_IT_Rome","T2_FR_GRIF_IRFU","T2_CH_CSCS","T3_TW_NCU"]
-		p = Process(target=submit, args=(config,))
-		p.start()
-		p.join()
+            config.General.workArea = '/nfs/dust/cms/user/%s/%s/'%(getUsernameFromSiteDB(),today)
+            config.General.transferOutputs = True
+            config.General.transferLogs = True
+            jobname = "SvFit_"+today
+            config.General.requestName = jobname
+            print jobname
+            config.Data.outputPrimaryDataset = 'Svfit'
+            config.Data.splitting = 'EventBased'
+            config.Data.unitsPerJob = 1
+            config.Data.totalUnits = len(cache_files)
+            config.Data.publication = False
+            config.Data.outputDatasetTag = config.General.requestName
+            config.Data.outLFNDirBase = '/store/user/%s/higgs-kit/Svfit/%s/'%(getUsernameFromSiteDB(),today)
+            print "outdir: " + config.Data.outLFNDirBase
+            config.Data.publication = False
+			
+            config.User.voGroup = 'dcms'
+			
+            config.JobType.pluginName = 'PrivateMC'
+            config.JobType.psetName = os.environ['CMSSW_BASE']+'/src/CombineHarvester/CombineTools/scripts/do_nothing_cfg.py'
+            config.JobType.inputFiles = ['Kappa/lib/libKappa.so', os.environ['CMSSW_BASE']+'/bin/'+os.environ['SCRAM_ARCH']+'/ComputeSvfit', jobfile_name]
+            config.JobType.allowUndistributedCMSSW = True
+            config.JobType.scriptExe = jobfile_name
+            config.JobType.outputFiles = ['SvfitCache.tar.gz']
+			
+            config.Site.storageSite = "T2_DE_DESY"
+                    # config.Site.blacklist = ["T3_US_PuertoRico","T2_ES_CIEMAT","T2_DE_RWTH","T3_US_Colorado","T2_BR_UERJ","T2_ES_IFCA","T2_RU_JINR","T2_UA_KIPT","T2_EE_Estonia","T2_FR_GRIF_LLR","T2_CH_CERN","T2_FR_GRIF_LLR","T3_IT_Bologna","T2_US_Nebraska","T2_US_Nebraska","T3_TW_NTU_HEP","T2_US_Caltech","T3_US_Cornell","T2_IT_Legnaro","T2_HU_Budapest","T2_IT_Pisa","T2_US_Florida",'T2_IT_Bari',"T2_FR_GRIF_IRFU","T2_IT_Rome","T2_FR_GRIF_IRFU","T2_CH_CSCS","T3_TW_NCU"]
+            p = Process(target=submit, args=(config,))
+            p.start()
+            p.join()
 
 if __name__ == "__main__":
 	if len(sys.argv) == 1: 
