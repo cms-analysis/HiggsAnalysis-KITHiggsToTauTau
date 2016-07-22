ee4f9600c8bab97bab09c30976edefd3a55520c4
Raphael Friese <raphael.friese@cern.ch>
2016-07-22 09:21:16 +0200

    gc config & plotting script

--- python/gc_configs/gc_makePlots_datacardsSMHtt.py
+++ python/gc_configs/gc_makePlots_datacardsSMHtt.py
@@ -3,61 +3,70 @@
 #   (<GCDIR>/packages needs to be in your PYTHONPATH - or grid-control was properly installed)
 
 import time
+import os
 print(time.time())
 active = ["0jet", "1jet", "2jet"]
 
 active = active[0]
+project_name = "2ndscan"
 print "doing stuff for " + active
 
 cfg = Settings()
-cfg.section('jobs').jobs = 2
 cfg.workflow.task = 'UserTask'
-cfg.workflow.backend = 'Host'
+cfg.workflow.backend = 'local'
 
-cfg.jobs.wall_time = '2:00'
+cfg.jobs.wall_time = '12:00:00'
+cfg.jobs.set("memory", "14000")
 
-cfg.usertask.executable = 'makePlots_datacardsSMHtt.py'
+cfg.usertask.executable = 'HiggsAnalysis/KITHiggsToTauTau/scripts/userjob_epilog.sh'
+cmssw_base = os.getenv("CMSSW_BASE") + "/src/"
+cfg.usertask.set("input files", [cmssw_base + "HiggsAnalysis/KITHiggsToTauTau/scripts/userjob_epilog.sh", cmssw_base + "HiggsAnalysis/KITHiggsToTauTau/scripts/makePlots_datacardsSMHtt.py"] )
 
+executable = 'makePlots_datacardsSMHtt.py'
 input_dataset = "-i /nfs/dust/cms/user/rfriese/htautau/artus/2016-05-31_11-32_FullAnalysis/merged/"
 variable = "-x m_sv"
 mass = "-m 125"
 output_dir = "-o ."
 channels= ["et", "mt", "tt", "em"]
-extra=" --n-plots 1000 0 --auto-rebin --qcd-subtract-shape -n 3 --remote"
+extra=" --n-plots 1000 0 --auto-rebin --qcd-subtract-shape -n 1 --remote"
 
 cfg.parameters.set("parameters", ["P1", "P2"])
 cfg.parameters.set("repeat", 1)
 if active == "2jet":
-	cfg.parameters.set("P1", ["200", "300", "400", "500", "600", "700", "800", "900", "1000"])
-	cfg.parameters.set("P2", ["1.0", "1.5", "2.0", "2.5", "3.0", "4.0","4.5", "5.0", "5.5", "6.0", "6.5"])
+	cfg.parameters.set("P1", ["0", "100", "200", "300", "400", "500", "600", "700", "800"])
+	cfg.parameters.set("P2", ["0.0", "0.5", "1.0", "1.5", "2.0", "2.5", "3.0", "4.0","4.5", "5.0", "5.5", "6.0"])
+elif active == "1jet":
+	cfg.parameters.set("P1", ["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100", "110", "120", "130", "140", "150"])
+	cfg.parameters.set("P2", ["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"])
 else:
-	cfg.parameters.set("P1", ["20", "30", "40", "50", "60", "70", "80", "90", "100", "110", "120", "130", "140", "150"])
-	cfg.parameters.set("P2", ["20", "30", "40", "50", "60", "70", "80", "90", "100"])
+	cfg.parameters.set("P1", ["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"])
+	cfg.parameters.set("P2", ["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"])
 
-arguments = input_dataset + " " + variable +" "+ mass +" "+ output_dir+ " " + extra
+arguments = executable + " " + variable +" "+ mass +" "+ output_dir+ " " + extra + " " + input_dataset
 for channel in channels:
-	arguments = arguments +" -c " + channel + " --categories"
+	arguments = arguments +" --channel " + channel + " --categories"
 	if active == "0jet":
 		arguments = arguments +" 0jet_@P1@_@P2@ i0jet_@P1@_@P2@ "
 	else:
-		arguments = arguments +" ZeroJet30 "
+		#arguments = arguments +" ZeroJet30 "
+		arguments = arguments +" 0jet_10_50 i0jet_10_50 "
 
 	if active == "1jet":
 		arguments = arguments +" 1jet_@P1@_@P2@ i1jet_@P1@_@P2@ "
 	else:
-		arguments = arguments +" OneJet30 "
+		#arguments = arguments +" OneJet30 "
+		arguments = arguments +" 1jet_120_40 i1jet_120_40 "
 
 	if active == "2jet":
-		arguments = arguments +" 2jet_@P1@_@P2@ i2jet_@P1@_@P2@ "
+		arguments = arguments +" vbf_@P1@_@P2@ ivbf_@P1@_@P2@ "
 	else:
-		arguments = arguments +" TwoJet30 "
-#arguments = arguments + " -c mt --categories OneJet30"
+		#arguments = arguments +" TwoJet30 "
+		arguments = arguments +" vbf_400_3.0 ivbf_400_3.0 "
 
-cfg.usertask.set('arguments', arguments)
-#cfg.usertask.set('output files', "jobresult.tar")
-cfg.storage.set('se path', "/nfs/dust/cms/user/rfriese/scans/" + active + "scan")
+cfg.usertask.set('arguments', "%s"%arguments)
+cfg.storage.set('se path', "/nfs/dust/cms/user/rfriese/" + project_name + "/" + active + "scan")
 cfg.storage.set('se output files', "jobresult.tar")
 cfg.storage.set('se output pattern', "@P1@_@P2@/@X@")
-getattr(cfg, 'global').set('workdir', "/nfs/dust/cms/user/rfriese/workdir/"+active)
+getattr(cfg, 'global').set('workdir', "/nfs/dust/cms/user/rfriese/workdir_" + project_name + "/" + active)
 print(cfg)
 print('=' * 20)
--- scripts/significance_2d.py
+++ scripts/significance_2d.py
@@ -1,63 +1,67 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import logging
-import Artus.Utility.logger as logger
+#import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
 import argparse
-import os
+import os, sys
 import glob
 import ROOT
 import matplotlib.pyplot as plt
 import numpy as np
-
+import pprint
 
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser(description="Plot significance from combine output.", parents=[logger.loggingParser])
+	parser = argparse.ArgumentParser(description="Plot significance from combine output.")
 
 	parser.add_argument("-i","--input-dir", help="Input ROOT file")
 	parser.add_argument("-m", "--mass", type=int, default=125, help="higgs mass")
 	parser.add_argument("-o", "--output-file", default="significance_collection", help="output file")
 	parser.add_argument("-c", "--channel", default="mt", help="Channel where categorization is varied")
+	parser.add_argument("--x-label", default = "$\Delta$")
+	parser.add_argument("--y-label", default = "jdeta")
+	parser.add_argument("--folder", default = "datacards/combined")
+	parser.add_argument("--x-type", default = "int")
+	parser.add_argument("--y-type", default = "int")
+	parser.add_argument("--x-range", default = "200,800,4")
+	parser.add_argument("--y-range", default = "2.0,5.0,4")
 	channels = ["et", "mt", "tt", "em"]
 	args = parser.parse_args()
-	logger.initLogger(args)
+	print args
+	#logger.initLogger(args)
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
 	plots = []
 	mass = args.mass
 
-	# pfade sammeln, signifikanzen auslesen ( gesamt odr nur vbf )
-	# in 2-dim array schreiben
-	mjjs = range(200,801,200)
-	mjj_width = (max(mjjs) - min(mjjs)) / (len(mjjs) -1)
-	print mjj_width
-	print mjjs
-	print max(mjjs)
-	print len(mjjs)
-	jdetas = np.linspace(2.0, 5.0, 4)
-	jdeta_width = (max(jdetas) - min(jdetas)) / (len(jdetas) -1)
-	#out_file = ROOT.TFile(args.output_file, "recreate")
-	#matrix = ROOT.TH2F("sig", "significances", len(mjjs), min(mjjs) - mjj_width/2, max(mjjs)+mjj_width/2, len(jdetas), min(jdetas)-jdeta_width/2, max(jdetas)+jdeta_width/2)
-	matrix = [[0 for x in range(len(mjjs))] for y in range(len(jdetas))]
-	for imjj, mjj in enumerate(mjjs, start=1):
-		for ijdeta, jdeta in enumerate(jdetas, start=1):
-			in_raw =os.path.join(args.input_dir, str(mjj)+"_"+str(jdeta), "datacards/combined", str(mass), "higgsCombineTest.ProfileLikelihood.mH125.*.root")
+	x_range = ([float(a) if "." in a else int(a) for a in args.x_range.split(",")])
+	print x_range
+	p1s = np.linspace(*x_range) if args.x_type == "float" else range(*x_range)
+	p1_width = (max(p1s) - min(p1s)) / (len(p1s) -1)
+	y_range = ([float(a) if "." in a else int(a) for a in args.y_range.split(",")])
+	p2s = np.linspace(*y_range) if args.y_type == "float" else range(*y_range)
+	p2_width = (max(p2s) - min(p2s)) / (len(p2s) -1)
+
+	matrix = [[0 for x in range(len(p1s))] for y in range(len(p2s))]
+	pprint.pprint(matrix)
+
+	for ip1, p1 in enumerate(p1s, start=1):
+		for ip2, p2 in enumerate(p2s, start=1):
+			in_raw =os.path.join(args.input_dir, str(p1)+"_"+str(p2), args.folder, str(mass), "higgsCombineTest.ProfileLikelihood.mH125.*.root")
 			print in_raw
 			in_file = glob.glob( in_raw)[0] if len(glob.glob(in_raw)) == 1 else "invalid"
 			if (not os.path.isfile(in_file)):
 				continue
-
-			current_chain = ROOT.TChain("chain_%i_%f"%(mjj, jdeta))
+			current_chain = ROOT.TChain("chain_%i_%f"%(p1, p2))
 			current_chain.Add(in_file+"/limit", -1)
 			for line in current_chain:
 					print "line: " + str(line)
 					y = line.limit
 					print y
-					#matrix.SetBinContent(imjj, ijdeta, y)
-					matrix[imjj-1][ijdeta-1] = y
-	print matrix
+					matrix[ip2-1][ip1-1] = y if y < 10 else 0
+	pprint.pprint( matrix)
 
 	# plotting
 	import matplotlib.pyplot as plt
@@ -65,11 +69,15 @@ if __name__ == "__main__":
 	npmatrix = np.matrix(matrix)
 	fig = plt.figure(figsize = (4,4))
 	ax = fig.add_subplot(1, 1, 1)
-	im = plt.imshow(matrix, origin = "lower", cmap=plt.cm.Reds, interpolation="None", extent=[jdetas[0]-jdeta_width/2, jdetas[-1]+jdeta_width/2,mjjs[0]-mjj_width/2, mjjs[-1]+mjj_width/2])
-	plt.yticks(mjjs)
-	plt.xticks( jdetas)
-	ax.set_aspect((jdetas[-1]-jdetas[0])/(mjjs[-1]-mjjs[0]))
+	min_value=np.min(npmatrix[np.nonzero(npmatrix)])
+	im = plt.imshow(matrix, origin = "lower", cmap=plt.cm.Reds, interpolation="None", extent=[p1s[0]-p1_width/2, p1s[-1]+p1_width/2,p2s[0]-p2_width/2, p2s[-1]+p2_width/2], clim = (min_value,npmatrix.max()))
+	plt.yticks(p2s)
+	plt.xticks( p1s, rotation=70)
+	
+	ax.set_aspect(float((p1s[-1]-p1s[0]))/float((p2s[-1]-p2s[0])))
 	fig.colorbar(im, ax=ax)
-	ax.set_ylabel("$m_{jj}$")
-	ax.set_xlabel("$\Delta \eta$")
+	labels = { "mjj" : "$m_{jj}$", "jdeta" : "$\Delta \eta_{jj}$", "hpt" : "$p_T^{H}$", "pt2" : "$p_T^2$"}
+	ax.set_xlabel(labels[args.x_label])
+	ax.set_ylabel(labels[args.y_label])
+	plt.tight_layout()
 	plt.savefig(args.output_file)
