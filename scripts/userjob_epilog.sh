b8c86833e6c42cd9a77bc17d144d55d95b94ddd7
Raphael Friese <raphael.friese@cern.ch>
2016-07-26 14:04:53 +0200

    not needed

--- scripts/userjob_epilog.sh
+++ scripts/userjob_epilog.sh
@@ -7,7 +7,7 @@ export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 eval `scramv1 runtime -sh`
 source $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/scripts/ini_KITHiggsToTauTauAnalysis.sh
-source $CMSSW_BASE/src/Artus/HarryPlotter/scripts/ini_harry.sh
+#source $CMSSW_BASE/src/Artus/HarryPlotter/scripts/ini_harry.sh
 popd
 pwd
 arguments="$@"
