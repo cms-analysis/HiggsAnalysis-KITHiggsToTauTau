f2c1aea061bbacb1f539046505c13865934fde1c
Raphael Friese <raphael.friese@cern.ch>
2016-08-18 15:05:09 +0200

    update to deleteEmptyFilelists to DCAP only

--- scripts/deleteEmptyFilelists.sh
+++ scripts/deleteEmptyFilelists.sh
@@ -3,8 +3,6 @@ function checkDelete() {
 	then
 		filename=$(basename $1)
 		dirname=$(dirname $1)
-		rm "$dirname/NAF_${filename:4}" -i
-		rm "$dirname/XROOTD_${filename:4}" -i
 		rm "$dirname/DCAP_${filename:4}" -i
 	fi
 }
@@ -18,17 +16,17 @@ function checkWarn() {
 
 
 # check if files in sample lists still point to an existing file
-for list in $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/Samples/NAF_sample*.txt;
+for list in $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/Samples/DCAP_sample*.txt;
 do
 	if ! [ -L $list ]
 	then
 		testfile=$(head -n 1 $list)
-		checkDelete $list $testfile
+		checkDelete $list ${testfile:31}
 	fi
 done 
 
 # check if all files in a collection are still present and warn if not
-for list in $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/Samples/NAF_collection*.txt;
+for list in $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/Samples/DCAP_collection*.txt;
 do
 	if ! [ -L $list ]
 	then
@@ -39,7 +37,7 @@ do
 	fi
 done 
 # check if symlinks still point to something
-for list in $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/Samples/NAF_sample*.txt;
+for list in $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/data/Samples/DCAP_sample*.txt;
 do
 	if [ -L $list ]
 	then
