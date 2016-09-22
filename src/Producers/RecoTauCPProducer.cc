63ed0f9c01536e786c0b1daa463bdd45a84669a3
Thomas Mueller <t.muller@cern.ch>
2016-09-22 16:33:17 +0200

    RecoTauCPProducer.cc: switch from flavour-ordered to charge-ordered leptons to avoid coding difficulties

--- src/Producers/RecoTauCPProducer.cc
+++ src/Producers/RecoTauCPProducer.cc
@@ -64,50 +64,21 @@ void RecoTauCPProducer::Produce(event_type const& event, product_type& product, 
 	assert(event.m_vertexSummary);
 	assert(product.m_flavourOrderedLeptons.size() >= 2);
 	
-	KTrack track1 = product.m_flavourOrderedLeptons[0]->track;
-	KTrack track2 = product.m_flavourOrderedLeptons[1]->track;
-	RMFLV momentum1 = product.m_flavourOrderedLeptons[0]->p4;
-	RMFLV momentum2 = product.m_flavourOrderedLeptons[1]->p4;
-
-	if ((product.m_decayChannel == HttEnumTypes::DecayChannel::TT) &&
-	    (static_cast<KTau*>(product.m_flavourOrderedLeptons[0])->chargedHadronCandidates.size() > 0))
-	{
-		momentum1 = static_cast<KTau*>(product.m_flavourOrderedLeptons[0])->chargedHadronCandidates.at(0).p4;
-	}
-	if (((product.m_decayChannel == HttEnumTypes::DecayChannel::ET) ||
-	     (product.m_decayChannel == HttEnumTypes::DecayChannel::MT) ||
-	     (product.m_decayChannel == HttEnumTypes::DecayChannel::TT)) &&
-	    (static_cast<KTau*>(product.m_flavourOrderedLeptons[1])->chargedHadronCandidates.size() > 0))
-	{
-		momentum2 = static_cast<KTau*>(product.m_flavourOrderedLeptons[1])->chargedHadronCandidates.at(0).p4;
-	}
+	KTrack& trackP = product.m_chargeOrderedLeptons[0]->track;
+	KTrack& trackM = product.m_chargeOrderedLeptons[1]->track;
+	RMFLV& momentumP = ((product.m_chargeOrderedLeptons[0]->flavour() == KLeptonFlavour::TAU) ? static_cast<KTau*>(product.m_flavourOrderedLeptons[0])->chargedHadronCandidates.at(0).p4 : product.m_chargeOrderedLeptons[0]->p4);
+	RMFLV& momentumM = ((product.m_chargeOrderedLeptons[1]->flavour() == KLeptonFlavour::TAU) ? static_cast<KTau*>(product.m_flavourOrderedLeptons[1])->chargedHadronCandidates.at(0).p4 : product.m_chargeOrderedLeptons[1]->p4);
 	
-
-	// define which is the chargePlus particle and which is the chargeMinus particle
-	KTrack* trackP = &track1;
-	KTrack* trackM = &track2;
-	RMFLV* momentumP = &momentum1;
-	RMFLV* momentumM = &momentum2;
-
-	if (product.m_flavourOrderedLeptons[0]->charge() < 0.0)
-	{
-		// reminder: track1 = product.m_flavourOrderedLeptons[0]->track
-		trackP = &track2;
-		trackM = &track1;
-		momentumP = &momentum2;
-		momentumM = &momentum1;
-	}
-
 	CPQuantities cpq;
-	product.m_recoPhiStarCP = cpq.CalculatePhiStarCP(event.m_vertexSummary->pv, *trackP, *trackM, *momentumP, *momentumM);
-	//product.m_recoPhiStarCPrPV = cpq.CalculatePhiStarCP(event.m_refitVertexSummary->pv, *trackP, *trackM, *momentumP, *momentumM);
-	//product.m_recoPhiStarCPrPVbs = cpq.CalculatePhiStarCP(event.m_refitVertexBSSummary->pv, *trackP, *trackM, *momentumP, *momentumM);
-
+	product.m_recoPhiStarCP = cpq.CalculatePhiStarCP(event.m_vertexSummary->pv, trackP, trackM, momentumP, momentumM);
+	//product.m_recoPhiStarCPrPV = cpq.CalculatePhiStarCP(event.m_refitVertexSummary->pv, trackP, trackM, momentumP, momentumM);
+	//product.m_recoPhiStarCPrPVbs = cpq.CalculatePhiStarCP(event.m_refitVertexBSSummary->pv, trackP, trackM, momentumP, momentumM);
+	
 	product.m_recoPhiStar = cpq.GetRecoPhiStar();
 	product.m_recoIP1 = cpq.GetRecoIP1();
 	product.m_recoIP2 = cpq.GetRecoIP2();
-	product.m_recoChargedHadronEnergies.first = cpq.CalculateChargedHadronEnergy(product.m_diTauSystem, momentum1);
-	product.m_recoChargedHadronEnergies.second = cpq.CalculateChargedHadronEnergy(product.m_diTauSystem, momentum2);
-	product.m_recoTrackRefError1 = cpq.CalculateTrackReferenceError(track1);
-	product.m_recoTrackRefError2 = cpq.CalculateTrackReferenceError(track2);
+	product.m_recoChargedHadronEnergies.first = cpq.CalculateChargedHadronEnergy(product.m_diTauSystem, momentumP);
+	product.m_recoChargedHadronEnergies.second = cpq.CalculateChargedHadronEnergy(product.m_diTauSystem, momentumM);
+	product.m_recoTrackRefError1 = cpq.CalculateTrackReferenceError(trackP);
+	product.m_recoTrackRefError2 = cpq.CalculateTrackReferenceError(trackM);
 }
