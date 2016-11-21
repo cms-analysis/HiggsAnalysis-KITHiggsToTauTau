502562feff895e477e42310ebf897b0830ecc5c8
rfriese <raphael.friese@cern.ch>
2016-11-21 17:49:22 +0100

    Revert "remove key, since not there to be used anyway" Still I think it should not be used and we need a different key-system. Keeping it to be backward compatible
    This reverts commit 4208b8b9f1864610a1a0abf17970a8dd84d554d1.

--- python/datacards/datacards.py
+++ python/datacards/datacards.py
@@ -67,6 +67,7 @@ class Datacards(object):
 			"lnN",
 			ch.SystMap("era")
 				(["7TeV", "8TeV"], 1.026)
+				(       ["13TeV"], 1.027) # CMS-PAS-LUM-15-001
 		]
 		self.electron_efficiency_syst_args = [
 			"CMS_eff_e",
