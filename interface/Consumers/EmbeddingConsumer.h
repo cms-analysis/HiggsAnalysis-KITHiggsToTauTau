7ef0e2c569a3121ef81dbb7cf50cc4a3ca7f692f
Artur Akhmetshin <artur.akhmetshin@cern.ch>
2016-11-22 19:28:02 +0100

    EmbeddingConsumer: adjustments to the new iso sum calculation

--- interface/Consumers/EmbeddingConsumer.h
+++ interface/Consumers/EmbeddingConsumer.h
@@ -3,6 +3,7 @@
 
 #include <TH1.h>
 #include "TROOT.h"
+#include "TRandom3.h"
 
 #include "Artus/Core/interface/ConsumerBase.h"
 #include "Artus/Utility/interface/RootFileHelper.h"
@@ -21,35 +22,26 @@ public:
 	virtual void ProcessFilteredEvent(event_type const& event, product_type const& product, setting_type const& settings) override;
 	virtual void Finish(setting_type const& settings) override;
 
-	virtual void FillPtFlowHistogram(TH1F* hist, KPFCandidates* pf_collection, KMuon* muon);
+	virtual void FillPtFlowHistogram(std::map<std::string, TH1F*> histmap, std::vector<const KPFCandidate*> pf_collection, KMuon* muon, std::string region);
 
 private:
+	TRandom3* randomnumbergenerator = new TRandom3(0);
 	unsigned int nDeltaRBins = 0;
 	float DeltaRMax = 0.0;
 	unsigned int nIsoPtSumBins = 0;
 	float IsoPtSumMax = 0.0;
 	float IsoPtSumOverPtMax = 0.0;
+	bool randomMuon = false;
 
 	std::vector<TH1F*> histograms;
-	std::vector<TH2F*> histograms2D;
 
 	std::map<std::string, KMuon*> Muon;
 
 	std::vector<TString> muonTypeVector = {"leading", "trailing", "positive", "negative"};
+	std::vector<TString> regionTypeVector = {"full", "peak", "sideband"};
 
-	std::map<std::string, TH1F*> Muon_ChargedNoPUPtFlow;
-	std::map<std::string, TH1F*> Muon_ChargedPUPtFlow;
-	std::map<std::string, TH1F*> Muon_NeutralNoPUPtFlow;
-	std::map<std::string, TH1F*> Muon_PhotonsNoPUPtFlow;
-
-	std::map<std::string,TH2F*> Muon_absIsoPhotonsOverChargedPU;
-	std::map<std::string, TH2F*> Muon_absIsoNeutralOverChargedPU;
-
-	std::map<std::string,TH2F*> Muon_relIsoPhotonsOverChargedPU;
-	std::map<std::string, TH2F*> Muon_relIsoNeutralOverChargedPU;
-
-	std::map<std::string, TH2F*> Muon_absIsoNeutandPhoOverChargedPU; 
-	std::map<std::string, TH2F*> Muon_relIsoNeutandPhoOverChargedPU;
-
-
+	std::map<std::string, std::map<std::string, TH1F*>> Muon_ChargedFromFirstPVPtFlow;
+	std::map<std::string, std::map<std::string, TH1F*>> Muon_ChargedNotFromFirstPVPtFlow;
+	std::map<std::string, std::map<std::string, TH1F*>> Muon_NeutralFromFirstPVPtFlow;
+	std::map<std::string, std::map<std::string, TH1F*>> Muon_PhotonsFromFirstPVPtFlow;
 };
