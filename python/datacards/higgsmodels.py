9bdb87e4a13ef9ddad4bd08f11d0c991e6cb6bf0
jordy <jordy.degens@rwth-aachen.de>
2016-11-23 09:36:37 +0100

    cp studies initial state scripts

---
+++ python/datacards/higgsmodels.py
@@ -0,0 +1,30 @@
+from HiggsAnalysis.CombinedLimit.PhysicsModel import PhysicsModel
+
+class HiggsCPInitialstate(PhysicsModel):
+	def __init__(self):
+		self.verbose = False
+
+	def setPhysicsOptions(self, physOptions):
+		for po in physOptions:
+			if po.startswith("verbose"):
+				self.verbose = True
+
+
+	def doParametersOfInterest(self):
+		self.modelBuilder.doVar("x[0,0,1]")
+		self.modelBuilder.factory_('expr::sm("(1-@0)", alpha)')
+		self.modelBuilder.factory_('expr::ps("@0", alpha)')
+
+
+	def getYieldScale(self, bin, process):
+		if self.DC.isSignal[process]:
+			if "sm" in process.lower() and "ggh" in process.lower():
+				return "sm"
+			elif "ps" in process.lower() and "ggh" in process.lower():
+				return "ps"
+
+
+HiggsCPI=HiggsCPInitialstate()			
+
+
+
--- python/datacards/initialstatecpstudiesdatacards.py
+++ python/datacards/initialstatecpstudiesdatacards.py
@@ -10,11 +10,12 @@ import HiggsAnalysis.KITHiggsToTauTau.datacards.smhttdatacards as smhttdatacards
 import CombineHarvester.CombineTools.ch as ch
 
 class InitialStateCPStudiesDatacards(smhttdatacards.SMHttDatacards):
-	def __init__(self, higgs_masses=["125"], useRateParam=False, year="", cb=None):
-		super(InitialStateCPStudiesDatacards, self).__init__(higgs_masses,useRateParam,year,cb)
+	def __init__(self, higgs_masses=["125"], useRateParam=False, year="", cb=None, signal_processes=[ "qqH", "WH", "ZH","ggHsm","ggHmm","ggHps"]):
+		super(InitialStateCPStudiesDatacards, self).__init__(higgs_masses,useRateParam,year,cb, signal_processes=["qqH", "WH", "ZH","ggHsm","ggHmm","ggHps"])
 		
 		self.cb.cp().signals().ForEachProc(lambda process: process.set_signal(False if process.process() in ["qqH", "WH", "ZH"] else True) )
 		self.cb.cp().process(["qqH", "WH", "ZH"]).ForEachSyst(lambda systematic: systematic.set_signal(False) )
 		self.cb.cp().backgrounds().ForEachProc(lambda process: process.set_mass("*"))
 		self.cb.cp().backgrounds().ForEachSyst(lambda systematic: systematic.set_mass("*"))
+		#self.cb.cp().signals().ForEachProc(lambda process: process.set_process("ggHps_alt","ggHps")
 		#self.cb.PrintAll()
