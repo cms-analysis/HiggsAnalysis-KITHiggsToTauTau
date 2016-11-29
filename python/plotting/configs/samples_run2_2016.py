762519667ebc064ba831d479a6510d30e1f73e2d
Olena Hlushchenko <greyxray@gmail.com>
2016-11-29 17:46:27 +0100

    annoying line break removed that breaks proper code folding

--- python/plotting/configs/samples_run2_2016.py
+++ python/plotting/configs/samples_run2_2016.py
@@ -1867,8 +1867,7 @@ class Samples(samples.SamplesBase):
 			Samples._add_plot(config, "bkg", "HIST", "F", "qcd", nick_suffix)
 		return config
 
-	def htt(self, config, channel, category, weight, nick_suffix, higgs_masses, normalise_signal_to_one_pb=False,
-	        lumi=default_lumi, exclude_cuts=None, additional_higgs_masses_for_shape=[], mssm=False, normalise_to_sm_xsec=False, **kwargs):
+	def htt(self, config, channel, category, weight, nick_suffix, higgs_masses, normalise_signal_to_one_pb=False, lumi=default_lumi, exclude_cuts=None, additional_higgs_masses_for_shape=[], mssm=False, normalise_to_sm_xsec=False, **kwargs):
 		
 		if exclude_cuts is None:
 			exclude_cuts = []
