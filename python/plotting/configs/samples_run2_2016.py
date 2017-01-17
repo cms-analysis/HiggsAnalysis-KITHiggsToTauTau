352a40fe2e050260adf711d6162faa64ba496797
anehrkor <alexander.nehrkorn@cern.ch>
2017-01-17 08:57:55 +0100

    Updated luminosity for golden json file; removed unnecessary json file

--- python/plotting/configs/samples_run2_2016.py
+++ python/plotting/configs/samples_run2_2016.py
@@ -11,7 +11,7 @@ import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.samples as samples
 from Kappa.Skimming.registerDatasetHelper import get_nick_list
 from Artus.Utility.tools import make_multiplication, split_multiplication, clean_multiplication
 energy = 13
-default_lumi = 36.46*1000.0
+default_lumi = 36.814*1000.0
 
 class Samples(samples.SamplesBase):
 
