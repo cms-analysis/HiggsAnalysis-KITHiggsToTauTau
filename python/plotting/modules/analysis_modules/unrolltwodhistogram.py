05b6cc1b2c128d26743940aff6bfda181a95e0d8
anehrkor <alexander.nehrkorn@cern.ch>
2017-01-18 09:35:24 +0100

    New analysis module for HP: adds ability to perform unrolling of 2d distributions as done in SM HTT analysis such that combine can be used to fit them

---
+++ python/plotting/modules/analysis_modules/unrolltwodhistogram.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import ROOT
+
+import Artus.HarryPlotter.analysisbase as analysisbase
+
+class UnrollTwoDHistogram(analysisbase.AnalysisBase):
+	"""Convert two dimensional histogram into concatenation of one dimensional histograms"""
+	
+	def __init__(self):
+		super(UnrollTwoDHistogram, self).__init__()
+	
+	def modify_argument_parser(self, parser, args):
+		super(UnrollTwoDHistogram, self).modify_argument_parser(parser, args)
+		
+		self.unroll_twod_histogram_options = parser.add_argument_group("UnrollTwoDHistogram options")
+		self.unroll_twod_histogram_options.add_argument("--two-d-input-nicks", nargs="+", default=[""],
+				help="Nick names of two dimensional input histograms. [Default: %(default)s]")
+		self.unroll_twod_histogram_options.add_argument("--unroll-along-y", action="store_true", default=False,
+				help="Unroll histogram along y instead of x axis. [Default: %(default)s]")
+		self.unroll_twod_histogram_options.add_argument("--res-hist-nicks", nargs="+", default=[""],
+				help="Nick names of resulting one dimensional histograms. [Default: %(default)s]")
+		
+	def prepare_args(self, parser, plotData):
+		super(UnrollTwoDHistogram, self).prepare_args(parser, plotData)
+		self._plotdict_keys = ["two_d_input_nicks", "res_hist_nicks"]
+		self.prepare_list_args(plotData, self._plotdict_keys)
+		
+		for two_d_input_nick, res_hist_nick in zip(*[plotData.plotdict[key] for key in self._plotdict_keys]):
+			if not res_hist_nick in plotData.plotdict["nicks"]:
+				plotData.plotdict["nicks"].insert(
+					plotData.plotdict["nicks"].index(two_d_input_nick),
+					res_hist_nick
+				)
+	
+	def run(self, plotData=None):
+		super(UnrollTwoDHistogram, self).run(plotData)
+		
+		for two_d_input_nick, res_hist_nick in zip(*[plotData.plotdict[key] for key in self._plotdict_keys]):
+			
+			input_number_bins_x = plotData.plotdict["root_objects"][two_d_input_nick].GetNbinsX()
+			input_number_bins_y = plotData.plotdict["root_objects"][two_d_input_nick].GetNbinsY()
+			result_number_of_bins = input_number_bins_x * input_number_bins_y
+			
+			unrolledHistogram = ROOT.TH1D("","",result_number_of_bins,0,result_number_of_bins)
+			
+			if plotData.plotdict["unroll_along_y"]:
+				bin_y = 1
+				for x in range(1,input_number_bins_x+1):
+					for y in range(1,input_number_bins_y+1):
+						unrolledHistogram.SetBinContent(bin_y,plotData.plotdict["root_objects"][two_d_input_nick].GetBinContent(x,y))
+						bin_y += 1
+			else:
+				bin_x = 1
+				for y in range(1,input_number_bins_y+1):
+					for x in range(1,input_number_bins_x+1):
+						unrolledHistogram.SetBinContent(bin_x,plotData.plotdict["root_objects"][two_d_input_nick].GetBinContent(x,y))
+						bin_x += 1
+			
+			plotData.plotdict.setdefault("root_objects", {})[res_hist_nick] = unrolledHistogram
+			plotData.plotdict["root_objects"][res_hist_nick].SetName(res_hist_nick)
+			plotData.plotdict["root_objects"][res_hist_nick].SetTitle("")
