36567519e6837ab90155fcea12d39226e05dab7e
Thomas Müller <thomas-mueller@users.noreply.github.com>
2017-02-21 12:18:14 +0100

    checkout_packages.sh: minor change

--- scripts/checkout_packages.sh
+++ scripts/checkout_packages.sh
@@ -35,7 +35,7 @@ git clone https://github.com/cms-analysis/HiggsAnalysis-KITHiggsToTauTau.wiki.gi
 
 # Svfit and HHKinFit
 git clone https://github.com/veelken/SVfit_standalone.git TauAnalysis/SVfitStandalone -b HIG-16-006
-echo '<use   name="rootrflx"/>' >> TauAnalysis/SVfitStandalone/BuildFile.xml
+# git clone https://github.com/CMSAachen3B/SVfit_standalone.git TauAnalysis/SVfitStandalone -b HIG-16-006
 git clone https://github.com/artus-analysis/HHKinFit2.git -b artus
 
 # Jet2Tau Fakes
