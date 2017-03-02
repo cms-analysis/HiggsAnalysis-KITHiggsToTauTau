539aef403c957b7fd5a8b771ac600b50dc1761f5
Thomas Mueller <t.muller@cern.ch>
2017-03-02 19:54:49 +0100

    MadGraphReweightingProducer: implemented functionality for calculation of matrix elements for different CP mixing angles

--- src/Producers/MadGraphReweightingProducer.cc
+++ src/Producers/MadGraphReweightingProducer.cc
@@ -87,111 +87,48 @@ void MadGraphReweightingProducer::Produce(event_type const& event, product_type&
 {
 	assert(event.m_eventInfo);
 	
-	float mixingAngleOverPiHalf = 0.0; // TODO
-	RMFLV gluon1LV; // TODO
-	RMFLV gluon2LV; // TODO
-	RMFLV higgsLV; // TODO
-	
-	PyObject* arguments = PyTuple_Pack(4,
-			PyFloat_FromDouble(mixingAngleOverPiHalf),
-			PyTuple_Pack(4,
-				PyFloat_FromDouble(gluon1LV.Px()),
-				PyFloat_FromDouble(gluon1LV.Py()),
-				PyFloat_FromDouble(gluon1LV.Pz()),
-				PyFloat_FromDouble(gluon1LV.E())
-			),
-			PyTuple_Pack(4,
-				PyFloat_FromDouble(gluon2LV.Px()),
-				PyFloat_FromDouble(gluon2LV.Py()),
-				PyFloat_FromDouble(gluon2LV.Pz()),
-				PyFloat_FromDouble(gluon2LV.E())
-			),
-			PyTuple_Pack(4,
-				PyFloat_FromDouble(higgsLV.Px()),
-				PyFloat_FromDouble(higgsLV.Py()),
-				PyFloat_FromDouble(higgsLV.Pz()),
-				PyFloat_FromDouble(higgsLV.E())
-			)
-	);
-	
-	PyObject* resultGGH = PyObject_CallObject(m_functionMadGraphWeightGGH, arguments);
-	if (resultGGH)
+	// TODO: should this be an assertion (including a filter to run before this producer)?
+	if ((product.m_genBosonParticle != nullptr) &&
+	    (product.m_genParticlesProducingBoson.size() > 1) &&
+	    (product.m_genParticlesProducingBoson.at(0)->pdgId == DefaultValues::pdgIdGluon) &&
+	    (product.m_genParticlesProducingBoson.at(1)->pdgId == DefaultValues::pdgIdGluon))
 	{
-		//int result = PyInt_AsLong(resultGGH);
-		//LOG(ERROR) << result;
-	}
-	else
-	{
-		PyErr_Print();
-	}
+		RMFLV gluon1LV = product.m_genParticlesProducingBoson.at(0)->p4;
+		RMFLV gluon2LV = product.m_genParticlesProducingBoson.at(1)->p4;
+		RMFLV higgsLV = product.m_genBosonParticle->p4;
 	
-	// clean up
-	Py_DECREF(arguments);
-	Py_DECREF(resultGGH);
-	
-	/*
-	// A generator level boson and its decay products must exist
-	// The boson is searched for by a GenBosonProducer
-	// and the decay tree is built by the GenTauDecayProducer
-	assert(product.m_genBosonTree.m_daughters.size() > 1);
-	
-	assert(! settings.GetBosonPdgIds().empty());
-
-	GenParticleDecayTree selectedTau1 = product.m_genBosonTree.m_daughters[0];
-	GenParticleDecayTree selectedTau2 = product.m_genBosonTree.m_daughters[1];
-	
-	//TauSpinner considers only Taus and Tau-Neutrinos as daughters of a Boson (Higgs, W etc.)
-	// otherwise the weights are set to 1
-	if ((std::abs(selectedTau1.m_genParticle->pdgId) == DefaultValues::pdgIdTau) && (std::abs(selectedTau2.m_genParticle->pdgId) == DefaultValues::pdgIdTau))
-	{
-		TauSpinner::SimpleParticle boson = GetSimpleParticle(product.m_genBosonLV, settings.GetBosonPdgIds()[0]);
-		TauSpinner::SimpleParticle tau1 = GetSimpleParticle(selectedTau1.m_genParticle->p4, selectedTau1.m_genParticle->pdgId);
-		TauSpinner::SimpleParticle tau2 = GetSimpleParticle(selectedTau2.m_genParticle->p4, selectedTau2.m_genParticle->pdgId);
-		std::vector<TauSpinner::SimpleParticle> tauFinalStates1;
-		GetFinalStates(selectedTau1, tauFinalStates1);
-		std::vector<TauSpinner::SimpleParticle> tauFinalStates2;
-		GetFinalStates(selectedTau2, tauFinalStates2);
-
-		//LOG_N_TIMES(20, DEBUG) << "The event contains the following particles: " << std::endl;
-		//LOG_N_TIMES(20, DEBUG) << "Boson " << std::to_string(boson) << std::endl;
-		//LOG_N_TIMES(20, DEBUG) << std::string("Tau1") << std::to_string(tau1);
-		//LOG_N_TIMES(20, DEBUG) << std::string("Tau2") << std::to_string(tau2);
-		//LOG_N_TIMES(20, DEBUG) << std::string("Tau1FinalState") << std::to_string(tauFinalStates1);
-		//LOG_N_TIMES(20, DEBUG) << std::string("Tau2FinalState") << std::to_string(tauFinalStates2);
-
-		if ((tauFinalStates1.size() == 0) || (tauFinalStates2.size() == 0))
-		{
-			LOG_N_TIMES(20, WARNING) << "MadGraphReweightingProducer could not find enogh genParticles for the TauSpinner Algorithm" << std::endl;
-			// product.m_madGraphWeight = DefaultValues::UndefinedDouble; // TODO
-			return;
-		}
+		PyObject* arguments = PyTuple_Pack(4,
+				PyFloat_FromDouble(settings.GetTauSpinnerMixingAnglesOverPiHalfSample()),
+				PyTuple_Pack(4,
+					PyFloat_FromDouble(gluon1LV.Px()),
+					PyFloat_FromDouble(gluon1LV.Py()),
+					PyFloat_FromDouble(gluon1LV.Pz()),
+					PyFloat_FromDouble(gluon1LV.E())
+				),
+				PyTuple_Pack(4,
+					PyFloat_FromDouble(gluon2LV.Px()),
+					PyFloat_FromDouble(gluon2LV.Py()),
+					PyFloat_FromDouble(gluon2LV.Pz()),
+					PyFloat_FromDouble(gluon2LV.E())
+				),
+				PyTuple_Pack(4,
+					PyFloat_FromDouble(higgsLV.Px()),
+					PyFloat_FromDouble(higgsLV.Py()),
+					PyFloat_FromDouble(higgsLV.Pz()),
+					PyFloat_FromDouble(higgsLV.E())
+				)
+		);
 		
-		//Decision for a certain weight calculation depending on BosonPdgId
-		if (Utility::Contains(settings.GetBosonPdgIds(), std::abs(DefaultValues::pdgIdH)) ||
-		    Utility::Contains(settings.GetBosonPdgIds(), std::abs(DefaultValues::pdgIdHCPOdd)) ||
-		    Utility::Contains(settings.GetBosonPdgIds(), std::abs(DefaultValues::pdgIdACPOdd)))
+		PyObject* matrixElement2GGHSample = PyObject_CallObject(m_functionMadGraphWeightGGH, arguments);
+		if (matrixElement2GGHSample != nullptr)
 		{
-			// set mixing angle
-			// http://arxiv.org/pdf/1406.1647.pdf, section A.1, page 15
-			float mixingAngleOverPiHalfSample = settings.GetTauSpinnerMixingAnglesOverPiHalfSample();
-			float twoTimesMixingAngleRadSample = M_PI * mixingAngleOverPiHalfSample;
-			TauSpinner::setHiggsParametersTR(-cos(twoTimesMixingAngleRadSample), cos(twoTimesMixingAngleRadSample),
-			                                 -sin(twoTimesMixingAngleRadSample), -sin(twoTimesMixingAngleRadSample));
-			
-			product.m_optionalWeights["madGraphWeight"] = calculateWeightFromParticlesH(boson, tau1, tau2, tauFinalStates1, tauFinalStates2);
-			product.m_tauSpinnerPolarisation = TauSpinner::getTauSpin(); // http://tauolapp.web.cern.ch/tauolapp/tau__reweight__lib_8cxx_source.html#l00020
+			product.m_optionalWeights["madGraphWeight"] = PyFloat_AsDouble(matrixElement2GGHSample);
 		}
-		else if (Utility::Contains(settings.GetBosonPdgIds(), std::abs(DefaultValues::pdgIdZ)))
+		else
 		{
-			// call same function as for Higgs: http://tauolapp.web.cern.ch/tauolapp/namespaceTauSpinner.html#a33de132eef40cedcf39222fee0449d79
-			product.m_optionalWeights["madGraphWeight"] = calculateWeightFromParticlesH(boson, tau1, tau2, tauFinalStates1, tauFinalStates2);
-			product.m_tauSpinnerPolarisation = TauSpinner::getTauSpin(); // http://tauolapp.web.cern.ch/tauolapp/tau__reweight__lib_8cxx_source.html#l00020
+			PyErr_Print();
 		}
-		else if (Utility::Contains(settings.GetBosonPdgIds(), std::abs(DefaultValues::pdgIdW)))
-		{
-			product.m_optionalWeights["madGraphWeight"] = calculateWeightFromParticlesWorHpn(boson, tau1, tau2, tauFinalStates1);
-			product.m_tauSpinnerPolarisation = TauSpinner::getTauSpin(); // http://tauolapp.web.cern.ch/tauolapp/tau__reweight__lib_8cxx_source.html#l00020
-		}
+		Py_DECREF(matrixElement2GGHSample); // clean up
 		
 		// calculate the weights for different mixing angles
 		for (std::vector<float>::const_iterator mixingAngleOverPiHalfIt = settings.GetTauSpinnerMixingAnglesOverPiHalf().begin();
@@ -199,27 +136,21 @@ void MadGraphReweightingProducer::Produce(event_type const& event, product_type&
 			 ++mixingAngleOverPiHalfIt)
 		{
 			float mixingAngleOverPiHalf = *mixingAngleOverPiHalfIt;
-
-			double madGraphWeight = 1.0;
-			if (Utility::Contains(settings.GetBosonPdgIds(), std::abs(DefaultValues::pdgIdH)) ||
-			    Utility::Contains(settings.GetBosonPdgIds(), std::abs(DefaultValues::pdgIdHCPOdd)) ||
-			    Utility::Contains(settings.GetBosonPdgIds(), std::abs(DefaultValues::pdgIdACPOdd)))
+			PyTuple_SetItem(arguments, 0, PyFloat_FromDouble(mixingAngleOverPiHalf));
+			PyObject* matrixElement2GGH = PyObject_CallObject(m_functionMadGraphWeightGGH, arguments);
+			if (matrixElement2GGH != nullptr)
 			{
-				// set mixing angle
-				// http://arxiv.org/pdf/1406.1647.pdf, section A.1, page 15
-				float twoTimesMixingAngleRad = M_PI * mixingAngleOverPiHalf;
-				TauSpinner::setHiggsParametersTR(-cos(twoTimesMixingAngleRad), cos(twoTimesMixingAngleRad),
-				                                 -sin(twoTimesMixingAngleRad), -sin(twoTimesMixingAngleRad));
-				
-				madGraphWeight = calculateWeightFromParticlesH(boson, tau1, tau2, tauFinalStates1, tauFinalStates2);
+				product.m_optionalWeights[GetLabelForWeightsMap(mixingAngleOverPiHalf)] = PyFloat_AsDouble(matrixElement2GGH);
 			}
-			else {
-				madGraphWeight = 0.0; // no Higgs boson;
+			else
+			{
+				PyErr_Print();
 			}
-		
-			product.m_optionalWeights[GetLabelForWeightsMap(mixingAngleOverPiHalf)] = madGraphWeight;
+			Py_DECREF(matrixElement2GGH); // clean up
 		}
-	}*/
+		
+		Py_DECREF(arguments); // clean up
+	}
 }
 
 std::string MadGraphReweightingProducer::GetLabelForWeightsMap(float mixingAngleOverPiHalf) const
