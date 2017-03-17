ae4eeba2403b2d6f37b0c8144ebca381ae160194
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2017-03-17 17:37:06 +0100

    added Tau Polarisation Discriminator

--- interface/HttProduct.h
+++ interface/HttProduct.h
@@ -253,6 +253,7 @@ public:
 
 	// MVA outputs
 	std::vector<double> m_antiTtbarDiscriminators;
+	std::vector<double> m_tauPolarisationDiscriminators;
 
 	//MVATestMethods
 	std::vector<double> m_MVATestMethodsDiscriminators;
