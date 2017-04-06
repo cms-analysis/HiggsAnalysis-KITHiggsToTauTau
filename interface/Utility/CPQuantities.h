39ee613cc558984e19f23409d9214b6e211a393f
Claudia Pistone <pistone@physik.rwth-aachen.de>
2017-04-06 13:38:44 +0200

    added phi*cp calculation function with new PV in the header file

--- interface/Utility/CPQuantities.h
+++ interface/Utility/CPQuantities.h
@@ -21,7 +21,7 @@ class CPQuantities
 public:
 	double CalculatePhiStarCP(RMFLV tau1, RMFLV tau2, RMFLV chargPart1, RMFLV chargPart2);
 	double CalculatePhiStarCP(KVertex pv , KTrack track1, KTrack track2, RMFLV chargPart1,RMFLV chargPart2);
-	double CalculatePhiStarCP(KRefitVertex* pv , KTrack track1, KTrack track2, RMFLV chargPart1,RMFLV chargPart2);
+	double CalculatePhiStarCP(KRefitVertex* pv , KTrack track1, KTrack track2, RMFLV chargPart1, RMFLV chargPart2);
 	double CalculateChargedHadronEnergy(RMFLV diTauMomentum, RMFLV chargHad);
 	double CalculatePhiCP(RMFLV boson, RMFLV tau1, RMFLV tau2, RMFLV pion1, RMFLV pion2);
 	double CalculateChargedProngEnergy(RMFLV tau, RMFLV chargedProng);
