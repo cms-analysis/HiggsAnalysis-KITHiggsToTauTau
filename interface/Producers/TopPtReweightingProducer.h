e5412c0cee2a6f937d79317a31c37ab3ccc704c7
Artur Akhmetshin <artur.akhmetshin@cern.ch>
2017-05-08 20:17:05 +0200

    TopPtReweightingProducer: allow to configure, whether Run1 or Run2 reweighting is used

--- interface/Producers/TopPtReweightingProducer.h
+++ interface/Producers/TopPtReweightingProducer.h
@@ -1,25 +1,36 @@
 
 #pragma once
 
-#include "Artus/KappaAnalysis/interface/KappaProducerBase.h"
+#include "Artus/Core/interface/ProducerBase.h"
+#include "HiggsAnalysis/KITHiggsToTauTau/interface/HttTypes.h"
 #include <boost/regex.hpp>
+#include <boost/algorithm/string.hpp>
 
 /**
    \brief TopPtReweightingProducer
+   Top Pt reweighting as suggested on:
+   https://twiki.cern.ch/twiki/bin/viewauth/CMS/TopPtReweighting
    Config tags:
-   - Fill me with something meaningful
+   - TopPtReweightingStrategy: possible values: Run1, Run2. If something else is defined, then Run2 is taken.
+     Both weigths are computed.
 
 */
 
-class TopPtReweightingProducer : public KappaProducerBase {
+class TopPtReweightingProducer: public ProducerBase<HttTypes> {
 public:
+
+	typedef typename HttTypes::event_type event_type;
+	typedef typename HttTypes::product_type product_type;
+	typedef typename HttTypes::setting_type setting_type;
 
 	std::string GetProducerId() const override;
 
 	virtual void Init(setting_type const& settings) override;
-	void Produce( KappaEvent const& event,
-			KappaProduct & product,
-			KappaSettings const& settings) const override;
+	void Produce( event_type const& event,
+			product_type & product,
+			setting_type const& settings) const override;
 private:
 	bool m_isTTbar;
+	bool m_oldStrategy = false; // old == true: Run1, new == false: Run2
+	float ComputeWeight(float top1Pt, float top2Pt, float parameter_a, float parameter_b) const;
 };
