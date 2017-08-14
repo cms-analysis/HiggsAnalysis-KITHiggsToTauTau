bc185db472f3ee0392cb9d123541205f577b4e35
lucas <lucas.wiens@rwth-aachen.de>
2017-08-14 19:21:29 +0200

    fixed an error

--- src/Producers/MadGraphReweightingProducer.cc
+++ src/Producers/MadGraphReweightingProducer.cc
@@ -116,7 +116,7 @@ void MadGraphReweightingProducer::Init(setting_type const& settings)
 	});
 	LambdaNtupleConsumer<HttTypes>::AddIntQuantity("lheParticleJetNumber", [](event_type const& event, product_type const& product)
 	{
-		return (product.m_lheParticlesSortedForMadGraph.size() - 3) >= 0 ? product.m_lheParticlesSortedForMadGraph.size() - 3 : DefaultValues::UndefinedInt;
+		return product.m_lheParticlesSortedForMadGraph.size() - 3;
 	});
 }
 
