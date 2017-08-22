fbd5d3cb6b59571f6014e66abc78b629a8dd33c6
Olena Hlushchenko <greyxray@gmail.com>
2017-08-22 15:51:31 +0200

    Kaon: clean up

--- src/Producers/KaonsProducer.cc
+++ src/Producers/KaonsProducer.cc
@@ -12,7 +12,7 @@ inline std::string KaonsProducer::GetProducerId() const { return "KaonsProducer"
 void KaonsProducer::Init(setting_type const& settings)
 {
 	ProducerBase<HttTypes>::Init(settings);
-	
+
 	UseVertex = settings.GetUseVertex();
 	VertexFitter = settings.GetVertexFitter();
 	UseRefTracks = settings.GetUseRefTracks();
@@ -69,7 +69,7 @@ bool KaonsProducer::TrackIsGood(KTrack tmpTrack, KBeamSpot theBeamSpot, RMPoint 
 	double ipsigXY = std::abs(tmpTrack.getDxy(&theBeamSpot) / tmpTrack.errDxy());
 	if (UseVertex) ipsigXY = std::abs(tmpTrack.getDxy(&referenceVtx)/tmpTrack.errDxy());
 	double ipsigZ  = std::abs(tmpTrack.getDz(&theBeamSpot) / tmpTrack.errDz());
-	if (verbose) 
+	if (verbose)
 	{
 		dout("\t\tipsigXY =", ipsigXY, "= tmpTrack.getDxy(&theBeamSpot) / tmpTrack.errDxy() =", tmpTrack.getDxy(&theBeamSpot), "/", tmpTrack.errDxy());
 		dout("\n\t",
@@ -84,17 +84,17 @@ bool KaonsProducer::TrackIsGood(KTrack tmpTrack, KBeamSpot theBeamSpot, RMPoint 
 	{
 		if (verbose) dout("TrackIsGood::Didn't pass TkChi2Cut");
 		return false;
-	} 
+	}
 	else if (!((int)tmpTrack.nValidHits() >= (int)TkNHitsCut))
 	{
 		if (verbose) dout("TrackIsGood::Didn't pass TkNHitsCut");
 		return false;
-	} 
+	}
 	else if(!(tmpTrack.p4.Pt() > TkPtCut))
 	{
 		if (verbose) dout("TrackIsGood::Didn't pass TkPtCut");
 		return false;
-	} 
+	}
 	else if(!(ipsigXY > TkIPSigXYCut))
 	{
 		if (verbose) dout("TrackIsGood::Didn't pass TkIPSigXYCut");
@@ -110,7 +110,7 @@ bool KaonsProducer::TrackIsGood(KTrack tmpTrack, KBeamSpot theBeamSpot, RMPoint 
 }
 
 void KaonsProducer::Produce(event_type const& event, product_type& product,
-                                     setting_type const& settings) const
+									 setting_type const& settings) const
 {
 	dout();
 	using std::vector;
@@ -118,7 +118,7 @@ void KaonsProducer::Produce(event_type const& event, product_type& product,
 
 	assert(event.m_taus);
 	assert(event.m_beamSpot);
-	//
+
 	KBeamSpot theBeamSpot = (*event.m_beamSpot);
 	RMPoint referencePos(theBeamSpot.position);
 	KVertex referenceVtx;
@@ -152,9 +152,25 @@ void KaonsProducer::Produce(event_type const& event, product_type& product,
 			}
 		}
 
-		KKaonCandidates kshortCandidatesOfSingleJet = tau->kshortCandidates; // vector of all the Kaons 
+		KKaonCandidates kshortCandidatesOfSingleJet = tau->kshortCandidates; // vector of all the Kaons
 		for (auto singleKaonCand = kshortCandidatesOfSingleJet.begin(); singleKaonCand != kshortCandidatesOfSingleJet.end(); ++singleKaonCand)
 		{
+			if (UseVertex)
+			{
+				if (pow(singleKaonCand->referencePosPV.x() - referencePos.x(), 2) + pow(singleKaonCand->referencePosPV.y() - referencePos.y(), 2) + pow(singleKaonCand->referencePosPV.z() - referencePos.z(), 2) > 0 )
+				{
+					dout("wrong PV is taken");
+					exit(1);
+				}
+			}
+			else
+			{
+				if (pow(singleKaonCand->referencePosBS.x() - referencePos.x(), 2) + pow(singleKaonCand->referencePosBS.y() - referencePos.y(), 2) + pow(singleKaonCand->referencePosBS.z() - referencePos.z(), 2) > 0 )
+				{
+					dout("wrong BS is taken:", singleKaonCand->referencePosBS.x() - referencePos.x(), singleKaonCand->referencePosBS.y() - referencePos.y(), singleKaonCand->referencePosBS.z() - referencePos.z());
+					exit(1);
+				}
+			}
 
 			// Track validity check  // TODO: check that the tracks correspond accross collections
 			{
@@ -163,82 +179,80 @@ void KaonsProducer::Produce(event_type const& event, product_type& product,
 
 				KTrack tmpTrack_1 = singleKaonCand->firstTransPFCand.bestTrack;//= theKPFCandidatesPionsCollection[trackIndex_1].bestTrack;
 				KTrack tmpTrack_2 = singleKaonCand->secondTransPFCand.bestTrack;//= theKPFCandidatesPionsCollection[trackIndex_2].bestTrack;
-		
-				if (!TrackIsGood(tmpTrack_1, theBeamSpot, referencePos, referenceVtx, false) || 
+
+				if (!TrackIsGood(tmpTrack_1, theBeamSpot, referencePos, referenceVtx, false) ||
 					!TrackIsGood(tmpTrack_2, theBeamSpot, referencePos, referenceVtx, false)) continue;
 			}
 
 			// measure distance between tracks at their closest approach
 			if (singleKaonCand->distanceOfClosestApproach > TkDCACut) continue;
 
-			// the POCA should at least be in the sensitive volume TODO::KAPPA
-				// GlobalPoint cxPt = cApp.crossingPoint(); // TODO::KAPPA
-				if (sqrt(singleKaonCand->POCA.x() * singleKaonCand->POCA.x() + singleKaonCand->POCA.y() * singleKaonCand->POCA.y()) > 120. || std::abs(singleKaonCand->POCA.z()) > 300.) continue; 
+			// the POCA should at least be in the sensitive volume
+				if (sqrt(pow(singleKaonCand->POCA.x(), 2) + pow(singleKaonCand->POCA.y(),2)) > 120 || std::abs(singleKaonCand->POCA.z()) > 300.) continue;  // 120 - outer radius of strip detector; 300cm -- with EndCup
 
 			// calculate mPiPi
 				if (singleKaonCand->initialFirstTSCP.mass() > MPiPiCut) continue;
-			if ( singleKaonCand->secondaryVertex.nDOF == 0 || singleKaonCand->secondaryVertex.chi2 / singleKaonCand->secondaryVertex.nDOF > VtxChi2Cut) continue; 
-			
+			if ( singleKaonCand->secondaryVertex.nDOF == 0 || singleKaonCand->secondaryVertex.chi2 / singleKaonCand->secondaryVertex.nDOF > VtxChi2Cut) continue;
 
+			float sigmaDistMagXY, sigmaDistMagXYZ, distMagXY, distMagXYZ, pointingAngle2D, pointingAngle3D;
 			if (UseVertex)
 			{
-				if (singleKaonCand->distMagXYPV / singleKaonCand->sigmaDistMagXYPV < VtxDecaySigXYCut)
-				{
-					dout("didn't pass 2D signif PV");
-					continue;
-				}
-				if (singleKaonCand->distMagXYZPV / singleKaonCand->sigmaDistMagXYZPV < VtxDecaySigXYZCut)
-				{
-					dout("didn't pass 3D signif BS:", singleKaonCand->distMagXYZPV, "/", singleKaonCand->sigmaDistMagXYZPV, "<", VtxDecaySigXYZCut);
-					//continue;
-				}
+				sigmaDistMagXY = singleKaonCand->sigmaDistMagXYPV;
+				sigmaDistMagXYZ = singleKaonCand->sigmaDistMagXYZPV;
+				distMagXY = singleKaonCand->distMagXYPV;
+				distMagXYZ = singleKaonCand->distMagXYZPV;
+				pointingAngle2D = singleKaonCand->angleXYPV;
+				pointingAngle3D = singleKaonCand->angleXYZPV;
 			}
 			else
 			{
-				if (singleKaonCand->distMagXYBS / singleKaonCand->sigmaDistMagXYBS < VtxDecaySigXYCut)
-				{
-					dout("didn't pass 2D signif PV");
-					continue;
-				}
-				if (singleKaonCand->distMagXYZBS / singleKaonCand->sigmaDistMagXYZBS < VtxDecaySigXYZCut)
-				{
-					dout("didn't pass 3D signif BS:", singleKaonCand->distMagXYZBS, "/", singleKaonCand->sigmaDistMagXYZBS, "<", VtxDecaySigXYZCut);
-					//continue;
-				}
+				sigmaDistMagXY = singleKaonCand->sigmaDistMagXYBS;
+				sigmaDistMagXYZ = singleKaonCand->sigmaDistMagXYZBS;
+				distMagXY = singleKaonCand->distMagXYBS;
+				distMagXYZ = singleKaonCand->distMagXYZBS;
+				pointingAngle2D = singleKaonCand->angleXYBS;
+				pointingAngle3D = singleKaonCand->angleXYZPS;
+			}
+
+			//!
+			if (distMagXY / sigmaDistMagXY < VtxDecaySigXYCut)
+			{
+				dout("didn't pass 2D significance");
+				continue;
+			}
+			if (distMagXYZ / sigmaDistMagXYZ < VtxDecaySigXYZCut)
+			{
+				dout("didn't pass 3D significance:", singleKaonCand->distMagXYZPV, "/", singleKaonCand->sigmaDistMagXYZPV, "<", VtxDecaySigXYZCut);
+				continue;
 			}
 
 			// make sure the vertex radius is within the inner track hit radius - NOT ON MINIAOD LEVEL?
-			/* TODO : use ref point
-				if (InnerHitPosCut > 0. && positiveTrackRef->innerOk())  // TODO::KAPPA
-				{
-					reco::Vertex::Point posTkHitPos = positiveTrackRef->innerPosition(); // TODO::KAPPA
-					double posTkHitPosD2 =  pow(posTkHitPos.x() - referencePos.x(), 2) + pow(posTkHitPos.y() - referencePos.y(), 2);
-
-					if (sqrt(posTkHitPosD2) < (distMagXY - sigmaDistMagXY * innerHitPosCut_)) continue;
-				}
-				if (InnerHitPosCut > 0. && negativeTrackRef->innerOk()) 
-				{
-					reco::Vertex::Point negTkHitPos = negativeTrackRef->innerPosition();
-					double negTkHitPosD2 = pow(negTkHitPos.x() - referencePos.x(), 2) + pow(negTkHitPos.y()-referencePos.y(), 2);
-
-					if (sqrt(negTkHitPosD2) < (distMagXY - sigmaDistMagXY * innerHitPosCut_)) continue;
-				}
-			*/
-
-			if (UseVertex)
+			// check if either track has a hit radially inside the vertex position minus this number times the sigma of the vertex fit
+			// note: Set this to -1 to disable this cut, which MUST be done if you want to run on the AOD track collection!
+			if (InnerHitPosCut > 0)
 			{
-				// 2D pointing angle
-				if (singleKaonCand->angleXYPV < CosThetaXYCut) continue;
-				// 3D pointing angle
-				if (singleKaonCand->angleXYZPV < CosThetaXYZCut) continue;
+				RMPoint posTkHitPos = singleKaonCand->firstTransPFCand.bestTrack.ref;
+				RMPoint negTkHitPos = singleKaonCand->secondTransPFCand.bestTrack.ref;
+				double posTkHitPosDist =  sqrt(pow(posTkHitPos.x() - referencePos.x(), 2) + pow(posTkHitPos.y() - referencePos.y(), 2));
+				double negTkHitPosDist = sqrt(pow(negTkHitPos.x() - referencePos.x(), 2) + pow(negTkHitPos.y() - referencePos.y(), 2));
+
+				if (posTkHitPosDist < (distMagXY - sigmaDistMagXY * InnerHitPosCut))
+				{
+					dout("failed posTkHitPosDist cut:", posTkHitPosDist, "<", (distMagXY - sigmaDistMagXY * InnerHitPosCut));
+					continue;
+				}
+				if (negTkHitPosDist < (distMagXY - sigmaDistMagXY * InnerHitPosCut))
+				{
+					dout("failed negTkHitPosDist cut:", negTkHitPosDist, "<", (distMagXY - sigmaDistMagXY * InnerHitPosCut));
+					continue;
+				}
 			}
-			else
-			{
-				// 2D pointing angle
-				if (singleKaonCand->angleXYBS < CosThetaXYCut) continue;
-				// 3D pointing angle
-				if (singleKaonCand->angleXYZPS < CosThetaXYZCut) continue;
-			}			
+
+			// cos(angleXY) between x and p of K candidate
+			// cos(angleXYZ) between x and p of K candidate
+			if (pointingAngle2D < CosThetaXYCut) continue;// 2D pointing angle
+			if (pointingAngle3D < CosThetaXYZCut) continue;// 3D pointing angle
+
 			dout("PASSED");
 
 			product.kaonMass.push_back(singleKaonCand->kMass);
