f0632791d462e991237e086235b8cf3d11e8d5a4
Thomas Mueller <t.muller@cern.ch>
2017-09-18 14:29:28 +0200

    makePlots_datacardsCPInitalState.py: fix creation of asimov datasets in case of multiple signal processes

--- scripts/makePlots_datacardsCPInitalState.py
+++ scripts/makePlots_datacardsCPInitalState.py
@@ -320,48 +320,19 @@ if __name__ == "__main__":
 	#datacards.cb.cp().signals().ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (0.207)))
 	# use asimov dataset for s+b
 	if args.use_asimov_dataset:
-		if "ggh" in args.production_mode:
-			gghsm_signals = datacards.cb.cp().signals()
-			gghsm_signals.FilterAll(lambda obj : ("ggHsm" not in obj.process()))
-			gghsm_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (1.)))
+		signal_null_hypothesis = datacards.cb.cp().signals()
+		signal_null_hypothesis.FilterAll(lambda obj : ("ALT" in obj.process()))
+		signal_null_hypothesis.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (1.)))
 
-			gghps_signals = datacards.cb.cp().signals()
-			gghps_signals.FilterAll(lambda obj : ("ggHps_ALT" not in obj.process()))
-			gghps_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (0.000000001)))
-			#gghps_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (0.000000000451)))
-
-			gghmm_signals = datacards.cb.cp().signals()
-			gghmm_signals.FilterAll(lambda obj : ("ggHmm_ALT" not in obj.process()))
-			gghmm_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (0.000000001)))
-			#gghmm_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (0.00000000071875)))
-			
-			datacards.replace_observation_by_asimov_dataset("125")
-			
-			gghsm_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (1.0)))
-			gghps_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (0.000000001)))
-			gghmm_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (0.000000001)))
-		if "qqh" in args.production_mode:
-			qqhsm_signals = datacards.cb.cp().signals()
-			qqhsm_signals.FilterAll(lambda obj : ("qqHsm" not in obj.process()))
-			qqhsm_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (1.)))
-
-			qqhps_signals = datacards.cb.cp().signals()
-			qqhps_signals.FilterAll(lambda obj : ("qqHps_ALT" not in obj.process()))
-			qqhps_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (0.000000001)))
-			#qqhps_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (0.000000000451)))
-
-			qqhmm_signals = datacards.cb.cp().signals()
-			qqhmm_signals.FilterAll(lambda obj : ("qqHmm_ALT" not in obj.process()))
-			qqhmm_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (0.000000001)))
-			#qqhmm_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (0.00000000071875)))
-
-			datacards.replace_observation_by_asimov_dataset("125")
-
-			qqhsm_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (1.0)))
-			qqhps_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (0.000000001)))
-			qqhmm_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (0.000000001)))
-
-
+		signal_alt_hypothesis = datacards.cb.cp().signals()
+		signal_alt_hypothesis.FilterAll(lambda obj : ("ALT" not in obj.process()))
+		signal_alt_hypothesis.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (0.000000001)))
+		#signal_alt_hypothesis.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (0.000000000451)))
+		
+		datacards.replace_observation_by_asimov_dataset("125")
+		
+		signal_null_hypothesis.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (1.0)))
+		signal_alt_hypothesis.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (0.000000001)))
 
 	if args.auto_rebin:
 		datacards.auto_rebin(bin_threshold = 1.0, rebin_mode = 0)
