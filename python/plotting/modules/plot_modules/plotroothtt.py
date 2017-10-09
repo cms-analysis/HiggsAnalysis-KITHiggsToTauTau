2cd8e9a69393e84720c504da5f5026f09ae047a8
dominikwolfschläger <dominik.wolfschlaeger@googlemail.com>
2017-10-09 15:26:35 +0200

    CP plot functionality moved to plotroothtt

--- python/plotting/modules/plot_modules/plotroothtt.py
+++ python/plotting/modules/plot_modules/plotroothtt.py
@@ -4,6 +4,7 @@
 """
 
 import logging
+import math
 import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
@@ -18,8 +19,30 @@ class PlotRootHtt(plotroot.PlotRoot):
 		
 		self.nice_labels = labels.LabelsDict(latex_version="root")
 	
+	def modify_argument_parser(self, parser, args):
+		super(PlotRootHtt, self).modify_argument_parser(parser, args)
+		
+		self.axis_options.add_argument("--angular-plot", action="store_true", default=False,
+									   help="Use proper x tick labels for angular plots. [Default: %(default)s]")
+	
 	def prepare_args(self, parser, plotData):
 		if self.predefined_colors is None:
 			self.predefined_colors = colors.ColorsDict(color_scheme=plotData.plotdict["color_scheme"])
 		
 		super(PlotRootHtt, self).prepare_args(parser, plotData)
+		
+	def make_plots(self, plotData):
+		super(PlotRootHtt, self).make_plots(plotData)
+	
+		if plotData.plotdict["angular_plot"]:		
+			self.axes_histogram.GetXaxis().Set(1000,0,2*math.pi)
+			self.axes_histogram.GetXaxis().SetBinLabel(1, "0")
+			self.axes_histogram.GetXaxis().SetBinLabel(250, "#pi/2")
+			self.axes_histogram.GetXaxis().SetBinLabel(500, "#pi")
+			self.axes_histogram.GetXaxis().SetBinLabel(750, "3#pi/2")		
+			self.axes_histogram.GetXaxis().SetBinLabel(1000, "2#pi")
+			#TODO: Figure out why this adjustment is necessary to obtain equally sized labels on both axes.
+			self.axes_histogram.GetXaxis().SetLabelSize(1.35*self.axes_histogram.GetYaxis().GetLabelSize())
+			self.axes_histogram.LabelsOption("h", "X")
+	
+	
