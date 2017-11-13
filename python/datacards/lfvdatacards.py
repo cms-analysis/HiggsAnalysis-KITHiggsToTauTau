ab02f10a28a05b59b1c72cae1ff85c43ad46df02
DaveBrun94 <david.brunner@rwth-aachen.de>
2017-11-13 16:28:34 +0100

    Changed structure, dictionary for systematics created and add for a loop into Combine Harvester

--- python/datacards/lfvdatacards.py
+++ python/datacards/lfvdatacards.py
@@ -4,100 +4,86 @@ import Artus.Utility.logger as logger
 import Artus.Utility.tools as tools
 log = logging.getLogger(__name__)
 
-import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.categories as Categories
 import HiggsAnalysis.KITHiggsToTauTau.datacards.datacards as datacards
 import CombineHarvester.CombineTools.ch as ch
 
 
 
 class LFVDatacards(datacards.Datacards):
-	def __init__(self, higgs_masses=["125"], useRateParam=False, year="", cb=None, signal_processes= [], categories = []):
+	def __init__(self, channel_list, signal_list, category_list, cb=None, lnN_syst_enable = False, shape_syst_enable = False):
 		super(LFVDatacards, self).__init__(cb)
 		
-		print Categories.CategoriesDict().getCategories(["em"])["em"]
 		if cb is None:
-			background_processes_mt = ["ZTT", "ZL", "ZJ", "EWKZ", "TTT", "TTJJ", "VV", "W", "QCD"]
-			background_processes_et = ["ZTT", "ZL", "ZJ", "EWKZ", "TTT", "TTJJ", "VV", "W", "QCD"]
-			background_processes_em = ["ZTT", "ZLL", "EWKZ", "TT", "VV", "hww_gg125", "hww_qq125", "W", "QCD"]
+			
+			###Define all background processes for each channel
 
+			backgrounds = {
+					"em": ["ZTT", "ZLL", "EWKZ", "TT", "VV", "hww_gg125", "hww_qq125", "W", "QCD"],
+					"et": ["ZTT", "ZL", "ZJ", "EWKZ", "TTT", "TTJJ", "VV", "W", "QCD"],
+			}
+			
 			all_mc_bkgs = ["ZTT", "ZL", "ZJ", "ZLL", "EWKZ", "TT", "TTT", "TTJJ", "VV", "VVT", "VVJ", "W", "hww_gg125", "hww_qq125"]
 			all_mc_bkgs_no_W = ["ZTT", "ZL", "ZJ", "ZLL", "EWKZ", "TT", "TTT", "TTJJ", "VV", "VVT", "VVJ", "hww_gg125", "hww_qq125"]
 			all_mc_bkgs_no_TTJ = ["ZTT", "ZL", "ZJ", "ZLL", "EWKZ", "TT", "TTT", "VV", "VVT", "VVJ", "W", "hww_gg125", "hww_qq125"]
 
-			# ===========================Channels===========================================
-			# MT channel
-			self.add_processes(
-					channel="mt",
-					categories=categories,
-					bkg_processes=background_processes_mt,
-					sig_processes=signal_processes,
-					analysis=["LFV"],
-					era=["13TeV"],
-					mass=higgs_masses
-					
-					
-			)
 
-			# ET channel
-			self.add_processes(
-					channel="et",
-					categories=categories,
-					bkg_processes=background_processes_et,
-					sig_processes=signal_processes,
-					analysis=["LFV"],
-					era=["13TeV"],
-					mass=higgs_masses
-					
-			)
-
-			# EM channel
-			self.add_processes(
-					channel="em",
-					categories=categories,
-					bkg_processes=background_processes_em,
-					sig_processes=signal_processes,
-					analysis=["LFV"],
-					era=["13TeV"],
-					mass=higgs_masses
-					
-			)
-
-
-			# ttbar "channel" to extract normalization of ttbar process
-			self.add_processes(
-					channel="ttbar",
-					categories=categories,
-					bkg_processes=background_processes_ttbar,
-					sig_processes=signal_processes,
-					analysis=["LFV"],
-					era=["13TeV"],
-					mass=higgs_masses
-			)
-			
-			# =============================Uncertanties=========================================
-			# Efficiencies
-			
-			self.cb.cp().channel(["em"]).process(signal_processes+all_mc_bkgs).AddSyst(self.cb, *self.trigger_efficiency2016_em_syst_args)
-			self.cb.cp().channel(["em"]).process(signal_processes+all_mc_bkgs).AddSyst(self.cb, *self.electron_efficiency2016_syst_args)
-			self.cb.cp().channel(["em"]).process(signal_processes+all_mc_bkgs).AddSyst(self.cb, *self.muon_efficiency2016_syst_args)
-
-			self.cb.cp().channel(["et"]).process(signal_processes+all_mc_bkgs_no_W).AddSyst(self.cb, *self.trigger_efficiency2016_syst_args)
-			self.cb.cp().channel(["et"]).process(signal_processes+all_mc_bkgs_no_W).AddSyst(self.cb, *self.electron_efficiency2016_syst_args)
-			self.cb.cp().channel(["et"]).process(signal_processes+all_mc_bkgs).AddSyst(self.cb, *self.tau_efficiency2016_syst_args)
-
-			self.cb.cp().channel(["mt"]).process(signal_processes+all_mc_bkgs_no_W).AddSyst(self.cb, *self.trigger_efficiency2016_syst_args)
-			self.cb.cp().channel(["mt"]).process(signal_processes+all_mc_bkgs_no_W).AddSyst(self.cb, *self.muon_efficiency2016_syst_args)
-			self.cb.cp().channel(["mt"]).process(signal_processes+all_mc_bkgs).AddSyst(self.cb, *self.tau_efficiency2016_syst_args)
-
-
-			#fake ES
-
+			###Define directory for lnN and shape uncertanty, in which each channel has saved the systematic + process + category. If systematic is not specific for one category, empty string is given. 
+			##The values of each systematic are saved in datacard.py
+	
+			lnN_syst = {
+					"em": [
+						[self.trigger_efficiency2016_em_syst_args, 	signal_list+all_mc_bkgs,		""],
+						[self.electron_efficiency2016_syst_args, 	signal_list+all_mc_bkgs,		""],
+						[self.muon_efficiency2016_syst_args,		signal_list+all_mc_bkgs, 		""],
+						[self.btag_efficiency2016_syst_args,		["TT", "VV"], 				["LFVZeroJet", "LFVJet"]],
+						[self.lumi2016_syst_args, 			["ZL", "ZJ", "ZLL"], 			""],
+					],
+	
+					"et": [
+						[self.trigger_efficiency2016_em_syst_args, 	signal_list+all_mc_bkgs_no_W,		""],
+						[self.electron_efficiency2016_syst_args, 	signal_list+all_mc_bkgs_no_W,		""],
+						[self.tau_efficiency2016_syst_args,		signal_list+all_mc_bkgs, 		""],
+						[self.btag_efficiency2016_syst_args,		["TT", "VV"], 				["LFVZeroJet", "LFVJet"]],
+						[self.lumi2016_syst_args, 			["ZL", "ZJ", "ZLL", "ZTT"], 		""],
+						[self.tau_efficiency2016_corr_syst_args, 	signal_list+all_mc_bkgs, 		""]
+					]
+			}	
+						
+						 
+			shape_syst = {}			
 			
 
+			###Fill Combine Harvester object with relevant information
 			
+			for channel in channel_list:
+			
+				###Add channels as process in Combine Harvester			
+	
+				self.add_processes(
+					channel=channel,
+					categories=category_list[0],
+					bkg_processes=backgrounds[channel],  
+					sig_processes=signal_list,
+					analysis=["LFV"],
+					era=["13TeV"],
+					mass=["125"]
+					)					
 
-			if log.isEnabledFor(logging.DEBUG):
-				self.cb.PrintAll()
+				###Add lnN/shape uncertanty for each channel, process and category
 
+				if lnN_syst_enable:
+					for (systematic, process, category) in lnN_syst[channel]:
+						if category == "":
+							self.cb.cp().channel([channel]).process(process).AddSyst(self.cb, *systematic)
+						else:
+							self.cb.cp().channel([channel]).process(process).bin(category).AddSyst(self.cb, *systematic)
+			
+				if shape_syst_enable:
+					for (systematic, process) in shape_syst[channel]:
+						if category == "":
+							self.cb.cp().channel([channel]).process(process).AddSyst(self.cb, *systematic)
+						else:
+							self.cb.cp().channel([channel]).process(process).bin(category).AddSyst(self.cb, *systematic)
+			
 
 	
