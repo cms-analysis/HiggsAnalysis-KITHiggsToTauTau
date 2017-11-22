b56a852d575a224eeeb3cc85ac9f727ff84fd01e
DaveBrun94 <david.brunner@rwth-aachen.de>
2017-11-22 17:23:05 +0100

    Changes of structure due to the new python/plotting/configs/systematics_libary.py

--- python/datacards/lfvdatacards.py
+++ python/datacards/lfvdatacards.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import HiggsAnalysis.KITHiggsToTauTau.datacards.datacards as datacards
 import CombineHarvester.CombineTools.ch as ch
-import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.categories as Categories
 
+import HiggsAnalysis.KITHiggsToTauTau.datacards.datacards as datacards
+import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.categories as Categories
+import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.systematics_libary as SystLib
 
 
 class LFVDatacards(datacards.Datacards):
@@ -22,78 +23,13 @@ class LFVDatacards(datacards.Datacards):
 			
 			all_mc_bkgs = ["ZTT", "ZL", "ZJ", "ZLL", "EWKZ", "TT", "TTT", "TTJJ", "VV", "VVT", "VVJ", "W", "hww_gg125", "hww_qq125"]
 			all_mc_bkgs_no_W = ["ZTT", "ZL", "ZJ", "ZLL", "EWKZ", "TT", "TTT", "TTJJ", "VV", "VVT", "VVJ", "hww_gg125", "hww_qq125"]
-			all_mc_bkgs_no_TTJ = ["ZTT", "ZL", "ZJ", "ZLL", "EWKZ", "TT", "TTT", "VV", "VVT", "VVJ", "W", "hww_gg125", "hww_qq125"]
+			all_mc_bkgs_no_TTJ = ["ZTT", "ZL", "ZJ", "ZLL", "EWKZ", "TT", "TTT", "VV", "VVT", "VVJ", "W", "hww_gg125", "hww_qq125"]	
 
 
-			###Define directory for lnN and shape uncertanty, in which each channel has saved the systematic + process + category. If systematic is not specific for one category, empty string is given. 
-			##The values of each systematic are saved in datacard.py
-	
-			lnN_syst = {
-					"em": [
-						[self.trigger_efficiency2016_em_syst_args, 	signal_list+all_mc_bkgs,		""],
-						[self.electron_efficiency2016_syst_args, 	signal_list+all_mc_bkgs,		""],
-						[self.muon_efficiency2016_syst_args,		signal_list+all_mc_bkgs, 		""],
-						[self.lumi2016_syst_args, 			["ZL", "ZJ", "ZLL"], 			""],
-						[self.htt_jetFakeLep_syst_args, 		["W"], 					""],
-						[self.htt_QCD_0jet_syst_args,			["QCD"],				"em_LFVZeroJet"],
-						[self.htt_QCD_boosted_syst_args,		["QCD"],				"em_LFVJet"],
-						[self.htt_zmm_norm_extrap_0jet,			["ZTT", "ZLL", "EWKZ"],			"em_LFVZeroJet"],
-						[self.ttj_cross_section_syst_args,		["TT", "TTT", "TTJJ"],			""],				
-						[self.vv_cross_section2016_syst_args, 		["VV", "VVT", "VVJ"], 			""]
-					],
-	
-					"et": [
-						[self.trigger_efficiency2016_syst_args, 	signal_list+all_mc_bkgs_no_W,		""],
-						[self.electron_efficiency2016_syst_args, 	signal_list+all_mc_bkgs_no_W,		""],
-						[self.tau_efficiency2016_syst_args,		signal_list+all_mc_bkgs, 		""],
-						[self.lumi2016_syst_args, 			["ZL", "ZJ", "ZLL", "ZTT"], 		""],
-						[self.tau_efficiency2016_corr_syst_args, 	signal_list+all_mc_bkgs, 		""],
-						[self.QCD_Extrap_Iso_nonIso_syst_args,		["QCD"],				""],
-						[self.WHighMTtoLowMT_0jet_syst_args,		["W"],					"et_LFVZeroJet"],
-						[self.WHighMTtoLowMT_boosted_syst_args,		["W"],					"et_LFVJet"],
-						[self.htt_zmm_norm_extrap_0jet,			["ZTT", "ZL", "ZJ", "EWKZ"],		"et_LFVZeroJet"],
-						[self.ttj_cross_section_syst_args,		["TT", "TTT", "TTJJ"],			""],
-						[self.vv_cross_section2016_syst_args, 		["VV", "VVT", "VVJ"], 			""]
-					],
-					
-					"mt": [
-						[self.trigger_efficiency2016_syst_args, 	signal_list+all_mc_bkgs_no_W,		""],
-						[self.muon_efficiency2016_syst_args, 		signal_list+all_mc_bkgs_no_W,		""],
-						[self.tau_efficiency2016_syst_args,		signal_list+all_mc_bkgs, 		""],
-						[self.lumi2016_syst_args, 			["ZL", "ZJ", "ZLL", "ZTT"], 		""],
-						[self.tau_efficiency2016_corr_syst_args, 	signal_list+all_mc_bkgs, 		""],
-						[self.QCD_Extrap_Iso_nonIso_syst_args,		["QCD"],				""],
-						[self.WHighMTtoLowMT_0jet_syst_args,		["W"],					"mt_LVFZeroJet"],
-						[self.WHighMTtoLowMT_boosted_syst_args,		["W"],					"mt_LFVJet"],
-						[self.htt_zmm_norm_extrap_0jet,			["ZTT", "ZL", "ZJ", "EWKZ"],		"mt_LFVZeroJet"],
-						[self.ttj_cross_section_syst_args,		["TT", "TTT", "TTJJ"],			""],
-						[self.vv_cross_section2016_syst_args, 		["VV", "VVT", "VVJ"], 			""]	
-					]
-						
-			}	
-						
-						 
-						
-						 
-			shape_syst = {
-					"em" : [],
-					
-					"et": [
-						[self.zl_shape_1prong_syst_args,		["ZL"],					""],
-						[self.zl_shape_1prong1pizero_syst_args,		["ZL"],					""],
-					],
-					
-					"mt": [	
-						[self.zl_shape_1prong_syst_args,		["ZL"],					""],
-						[self.zl_shape_1prong1pizero_syst_args,		["ZL"],					""],
-						[self.mFakeTau_1prong_syst_args,		["ZL"], 				""],
-						[self.scale_t_1prong_syst_args,			signal_list+all_mc_bkgs,		""],
-						[self.scale_t_3prong_syst_args,			signal_list+all_mc_bkgs, 		""],
-						[self.scale_t_1prong1pizero_syst_args,		signal_list+all_mc_bkgs,		""]
-					]
-			}			
+			##Generate instance of systematic libary, in which the relevant information about the systematics are safed
+
+			systematics_list = SystLib.SystematicLibary()		
 			
-
 			###Fill Combine Harvester object with relevant information
 			
 			for channel in channel_list:
@@ -113,14 +49,14 @@ class LFVDatacards(datacards.Datacards):
 				###Add lnN/shape uncertanty for each channel, process and category
 
 				if lnN_syst_enable:
-					for (systematic, process, category) in lnN_syst[channel]:
+					for (systematic, process, category) in systematics_list.get_LFV_systs(channel, lnN = lnN_syst_enable):
 						if category == "":
 							self.cb.cp().channel([channel]).process(process).AddSyst(self.cb, *systematic)
 						else:
 							self.cb.cp().channel([channel]).process(process).bin([category]).AddSyst(self.cb, *systematic)
 			
 				if shape_syst_enable:
-					for (systematic, process, category) in shape_syst[channel]:
+					for (systematic, process, category) in systematics_list.get_LFV_systs(channel, shape = shape_syst_enable):
 						if category == "":
 							self.cb.cp().channel([channel]).process(process).AddSyst(self.cb, *systematic)
 						else:
