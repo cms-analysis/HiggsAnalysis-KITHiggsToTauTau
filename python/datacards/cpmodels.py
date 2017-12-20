b5ce90358b194c3ebbd484c17bb7aa484b5c1593
Thomas Mueller <t.muller@cern.ch>
2017-12-20 11:58:45 +0100

    cpmodels.py: added options for different inclusive signal scalings

--- python/datacards/cpmodels.py
+++ python/datacards/cpmodels.py
@@ -12,8 +12,9 @@ cp_xsec = PhysicsModel()
 ###   - possibly modifies the systematical uncertainties (does nothing by default)
 
 class CPMixing(PhysicsModel):
-	def __init__(self, use_mixing_angle=True):
+	def __init__(self, use_mixing_angle=True, inclusive_scaling="muV_muF"):
 		self.use_mixing_angle = use_mixing_angle
+		self.inclusive_scaling = inclusive_scaling
 		self.verbose = False
 
 	def setPhysicsOptions(self, physOptions):
@@ -25,9 +26,17 @@ class CPMixing(PhysicsModel):
 		"""Create POI and other parameters, and define the POI set."""
 		# --- POI and other parameters ----
 		
+		self.modelBuilder.doVar("cpmixing[0.0,0.0,1.0]") # CP mixing angle in units of pi/2
+		
 		self.modelBuilder.doVar("muF[1.0,0.0,5.0]")
 		self.modelBuilder.doVar("muV[1.0,0.0,5.0]")
-		self.modelBuilder.doVar("cpmixing[0.0,0.0,1.0]") # CP mixing angle in units of pi/2
+		
+		self.modelBuilder.doVar("kappa[1.0,-5.0,5.0]")
+		
+		self.modelBuilder.doVar("kappa_ggh[1.0,0.0,5.0]")
+		self.modelBuilder.doVar("kappa_qqh[1.0,0.0,5.0]")
+		self.modelBuilder.doVar("kappa_vh[1.0,0.0,5.0]")
+		self.modelBuilder.doVar("kappa_tth[1.0,0.0,5.0]")
 		
 		if self.use_mixing_angle:
 			maxmix = {
@@ -48,7 +57,7 @@ class CPMixing(PhysicsModel):
 			self.modelBuilder.factory_('expr::ps_scaling("@0-sqrt((1-@0)*@0)", cpmixing)')
 			self.modelBuilder.factory_('expr::mm_scaling("sqrt((1-@0)*@0)", cpmixing)')
 		
-		for production in ["muF", "muV"]:
+		for production in ["muF", "muV", "kappa", "kappa_ggh", "kappa_qqh", "kappa_vh", "kappa_tth"]:
 			for decay in ["muF"]:
 				self.modelBuilder.factory_('expr::{production}_{decay}("@0*@1", {production}, {decay})'.format(
 						production=production, decay=decay)
@@ -64,18 +73,33 @@ class CPMixing(PhysicsModel):
 		if self.DC.isSignal[process]:
 			production_decay_cp = []
 			
-			if "ggh" in process.lower():
-				production_decay_cp.append("muF")
-			elif "qqh" in process.lower():
-				production_decay_cp.append("muV")
-			elif "wh" in process.lower():
-				production_decay_cp.append("muV")
-			elif "zh" in process.lower():
-				production_decay_cp.append("muV")
-			elif "tth" in process.lower():
+			if self.inclusive_scaling == "muV_muF":
+				if "ggh" in process.lower():
+					production_decay_cp.append("muF")
+				elif "qqh" in process.lower():
+					production_decay_cp.append("muV")
+				elif "wh" in process.lower():
+					production_decay_cp.append("muV")
+				elif "zh" in process.lower():
+					production_decay_cp.append("muV")
+				elif "tth" in process.lower():
+					production_decay_cp.append("muF")
 				production_decay_cp.append("muF")
 			
-			production_decay_cp.append("muF")
+			elif self.inclusive_scaling == "kappa":
+				production_decay_cp.append("kappa")
+			
+			elif self.inclusive_scaling == "free":
+				if "ggh" in process.lower():
+					production_decay_cp.append("kappa_ggh")
+				elif "qqh" in process.lower():
+					production_decay_cp.append("kappa_ggh")
+				elif "wh" in process.lower():
+					production_decay_cp.append("kappa_vh")
+				elif "zh" in process.lower():
+					production_decay_cp.append("kappa_vh")
+				elif "tth" in process.lower():
+					production_decay_cp.append("kappa_tth")
 			
 			if "sm" in process.lower():
 				production_decay_cp.append("sm_scaling")
@@ -89,6 +113,15 @@ class CPMixing(PhysicsModel):
 		else:
 			return 1
 
-cp_mixing_angle = CPMixing(use_mixing_angle=True)
-cp_fa3 = CPMixing(use_mixing_angle=False)
+cp_mixing_angle = CPMixing(use_mixing_angle=True, inclusive_scaling="kappa")
+cp_fa3 = CPMixing(use_mixing_angle=False, inclusive_scaling="kappa")
+
+cp_mixing_angle_muV_muF = CPMixing(use_mixing_angle=True, inclusive_scaling="muV_muF")
+cp_fa3_muV_muF = CPMixing(use_mixing_angle=False, inclusive_scaling="muV_muF")
+
+cp_mixing_angle_kappa = CPMixing(use_mixing_angle=True, inclusive_scaling="kappa")
+cp_fa3_kappa = CPMixing(use_mixing_angle=False, inclusive_scaling="kappa")
+
+cp_mixing_angle_free = CPMixing(use_mixing_angle=True, inclusive_scaling="free")
+cp_fa3_free = CPMixing(use_mixing_angle=False, inclusive_scaling="free")
 
