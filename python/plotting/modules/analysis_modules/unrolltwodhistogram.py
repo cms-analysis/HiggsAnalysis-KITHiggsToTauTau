3a184cd4cc8f2a0020e9ecbb79d2bad2e08ff390
Thomas Mueller <t.muller@cern.ch>
2018-01-16 22:46:03 +0100

    unrolltwodhistogram.py: avoid having twice the same nick where it should be only one

--- python/plotting/modules/analysis_modules/unrolltwodhistogram.py
+++ python/plotting/modules/analysis_modules/unrolltwodhistogram.py
@@ -32,7 +32,7 @@ class UnrollTwoDHistogram(analysisbase.AnalysisBase):
 		self.prepare_list_args(plotData, self._plotdict_keys)
 		
 		for two_d_input_nick, unrolled_hist_nick in zip(*[plotData.plotdict[key] for key in self._plotdict_keys]):
-			if not unrolled_hist_nick in plotData.plotdict["nicks"]:
+			if (two_d_input_nick != unrolled_hist_nick) and (not unrolled_hist_nick in plotData.plotdict["nicks"]):
 				plotData.plotdict["nicks"].insert(
 					plotData.plotdict["nicks"].index(two_d_input_nick),
 					unrolled_hist_nick
