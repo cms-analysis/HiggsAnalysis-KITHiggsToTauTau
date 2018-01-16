2cba23d3306d612ddf2d34cd70603ab0d57946e6
Thomas Mueller <t.muller@cern.ch>
2018-01-16 22:49:55 +0100

    makePlots_datacardsSMHtt.py: added options -b/--batch and --no-shape-uncs
    Conflicts:
    	scripts/makePlots_datacardsSMHtt.py

--- scripts/makePlots_datacardsSMHtt.py
+++ scripts/makePlots_datacardsSMHtt.py
@@ -59,10 +59,12 @@ if __name__ == "__main__":
 	                    help="Add ratio subplot. [Default: %(default)s]")
 	parser.add_argument("-a", "--args", default="",
 	                    help="Additional Arguments for HarryPlotter. [Default: %(default)s]")
-	parser.add_argument("-b", "--background-method", default="new",
+	parser.add_argument("--background-method", default="new",
 	                    help="Background estimation method to be used. [Default: %(default)s]")
 	parser.add_argument("-n", "--n-processes", type=int, default=1,
 	                    help="Number of (parallel) processes. [Default: %(default)s]")
+	parser.add_argument("-b", "--batch", default=None, const="rwthcondor", nargs="?",
+	                    help="Run with grid-control. Optionally select backend. [Default: %(default)s]")
 	parser.add_argument("-f", "--n-plots", type=int, nargs=2, default=[None, None],
 	                    help="Number of plots for datacard inputs (1st arg) and for postfit plots (2nd arg). [Default: all]")
 	parser.add_argument("-o", "--output-dir",
@@ -98,6 +100,8 @@ if __name__ == "__main__":
 	                    help="Do not split JEC uncertainties into the 27 different sources but use the envelope instead. [Default: %(default)s]")
 	parser.add_argument("--new-tau-id", default=False, action="store_true",
 	                    help="Use rerun tau Id instead of nominal one. [Default: %(default)s]")
+	parser.add_argument("--no-shape-uncs", default=False, action="store_true",
+	                    help="Do not include shape-uncertainties. [Default: %(default)s]")
 	
 	args = parser.parse_args()
 	logger.initLogger(args)
@@ -284,6 +288,10 @@ if __name__ == "__main__":
 	
 	do_not_normalize_by_bin_width = args.do_not_normalize_by_bin_width
 
+	#restriction to requested systematics
+	if args.no_shape_uncs:
+		datacards.cb.FilterSysts(lambda systematic : systematic.type() == "shape")
+	
 	#restriction to CH
 	datacards.cb.channel(args.channel)
 
@@ -417,9 +425,7 @@ if __name__ == "__main__":
 						elif args.x_bins != None:
 							config["x_bins"] = [args.x_bins]
 						else:
-							log.fatal("binnings key " + binnings_key + " not found in binnings_dict! Available binnings are (see HiggsAnalysis/KITHiggsToTauTau/python/plotting/configs/binnings.py):")
-							for key in binnings_settings.binnings_dict:
-								print key
+							log.fatal("binnings key " + binnings_key + " not found in binnings_dict!")
 							sys.exit()
 					
 					# define quantities and binning for control regions
@@ -523,7 +529,7 @@ if __name__ == "__main__":
 	output_files = list(set(output_files))
 	
 	# create input histograms with HarryPlotter
-	higgsplot.HiggsPlotter(list_of_config_dicts=plot_configs, list_of_args_strings=[args.args], n_processes=args.n_processes, n_plots=args.n_plots[0])
+	higgsplot.HiggsPlotter(list_of_config_dicts=plot_configs, list_of_args_strings=[args.args], n_processes=args.n_processes, n_plots=args.n_plots[0], batch=args.batch)
 	if args.n_plots[0] != 0:
 		tools.parallelize(_call_command, hadd_commands, n_processes=args.n_processes)
 	if args.debug_plots:
@@ -546,7 +552,7 @@ if __name__ == "__main__":
 				processes=datacards.cb.cp().backgrounds().process_set(),
 				add_threshold=0.05, merge_threshold=0.8, fix_norm=False
 		)
-
+	
 	# scale
 	if(args.scale_lumi):
 		datacards.scale_expectation( float(args.scale_lumi) / args.lumi)
