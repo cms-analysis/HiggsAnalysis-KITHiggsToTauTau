2e8420ac2b9607d48f7a0c07081da4091f45b99d
DaveBrun94 <david.brunner@rwth-aachen.de>
2018-01-29 17:22:51 +0100

    Modification due to change in LepFlaVio

--- python/lfv/ConfigMaster.py
+++ python/lfv/ConfigMaster.py
@@ -9,16 +9,16 @@ class Plotmodule():
 	efficiency_plot = 2
 	shape_plot = 3
 	datacard = 4
-	bkg_reduction_plot = 5
-	cutflow_plot = 6
-	ratio = 7
-	limit = 8
+	cutflow_plot = 5
+	ratio = 6
+	limit = 7
+
 
 ### Class to create harry plotter configs for your special desire. All configs are safed as dictionaries and can be combined to the final config which is needed	
 
 class ConfigMaster(object):
 
-	##Constructor
+	##Constructor for loading sample information from samples_run2_2016.py
 
 	def __init__(self, base_values, sample_values = None):
 
@@ -27,9 +27,9 @@ class ConfigMaster(object):
 		###Config which is initiliazed with information for each sample from samples_run2_201X.py if wished
 		if sample_values != None:
 			sample_settings = samples.Samples()	
-			sample_list, channel, category, no_plot, nick_suffix, weight, estimationMethod, cut_type = sample_values
+			sample_list, channel, category, estimationMethod, cut_type, nick_suffix, no_plot, weight= sample_values
 
-			self._config = sample_settings.get_config(samples=[getattr(samples.Samples, sample) for sample in sample_list], channel = channel, category = category, no_plot = no_plot, nick_suffix = nick_suffix, weight = weight, estimationMethod = estimationMethod, cut_type = cut_type)
+			self._config = sample_settings.get_config(samples=[getattr(samples.Samples, sample) for sample in sample_list], channel = channel, category = category, no_plot = no_plot, nick_suffix = nick_suffix, estimationMethod = estimationMethod, cut_type = cut_type, weight = weight)			
 
 		###Fill config with basic information
 
@@ -45,25 +45,31 @@ class ConfigMaster(object):
 					Plotmodule.efficiency_plot:		self.__efficiencyplot__,
 					Plotmodule.shape_plot:			self.__shapeplot__,
 					Plotmodule.datacard: 			self.__datacard__,
-					Plotmodule.bkg_reduction_plot:		self.__bkgreductionplot__,
 					Plotmodule.cutflow_plot:		self.__cutflowplot__,
 					Plotmodule.ratio:			self.__ratio__,
 					Plotmodule.limit:			self.__limitplot__
 		}
 
+
+	##Pythonic way of overloaded constructor not using sample information and doing any other plot you wish
+	@classmethod
+	def init(self):
+		return "Work in progress"
+
+	
 	###Dictionaries for information for each plotting/analysis modules
 
-	def __base__(self, input_dir, output_dir, output_file, formats, www, www_nodate, x_expressions, x_bins):
+	def __base__(self, input_dir, output_dir, formats, www_nodate, www, x_expressions, x_bins, output_file):
 	
 		self._base = {
 					"directories":			input_dir,
 					"output_dir":			output_dir,
-					"filename":			output_file,
 					"formats":			formats,
-					"www":				www,
 					"www_nodate":			www_nodate,
+					"www":				www,
 					"x_expressions":		x_expressions,
 					"x_bins":			x_bins,
+					"filename":			output_file,
 		}
 
 		return self._base
@@ -94,33 +100,41 @@ class ConfigMaster(object):
 
 		return self._sumofhists
 
-	def __efficiencyplot__(self, analysis_mod, bkg_nicks, sig_nicks, cut_modes, cut_nicks, whitelist, markers,  y_label, lower_cut, plot_modules, output_dir):
+
+	def __efficiencyplot__(self, analysis_mod, bkg_nicks, markers, y_label, cut_modes, sig_nicks, cut_nicks, whitelist, lower_cut, plot_modules):
 		
 		self._efficiencyplot = {
 					"analysis_modules":		analysis_mod,
-					"output_dir":			output_dir,
 					"cut_efficiency_bkg_nicks":	bkg_nicks,
 					"cut_efficiency_sig_nicks":	sig_nicks,
 					"cut_efficiency_nicks":		cut_nicks,
 					"cut_efficiency_modes":		cut_modes,
 					"nicks_whitelist":		whitelist,
 					"markers":			markers,
+					"x_label":			x_label,
 					"y_label":			y_label,
+					"www":				www,
 					"select_lower_values":		lower_cut,
 					"plot_modules":			plot_modules
 		}
 
 		return self._efficiencyplot
 
-	def __shapeplot__(self, y_label, analysis_mod):
+	def __shapeplot__(self, x_label, title, legend, lumis, energies, year, www, y_label, analysis_mod):
 		
 		self._shapeplot = {
+					"x_label":			x_label,
+					"legend":			legend,
+					"lumis"	:			lumis,
+					"energies":			energies,
+					"title"	:			title,
+					"year":				year,
+					"www":				www,
 					"y_label":			y_label,
 					"analysis_modules":		analysis_mod					
 		}
 
 		return self._shapeplot
-
 
 	def __datacard__(self, labels, plot_modules, file_mode):
 
@@ -132,29 +146,12 @@ class ConfigMaster(object):
 
 		return self._datacard
 
-	def __bkgreductionplot__(self, numerator_nicks, denominator_nicks, result_nicks, no_errors, analysis_modules, y_lims, markers, colors, labels, legend, legend_markers, y_label):
 
-		self._bkgreductionplot = {
-					"divide_numerator_nicks":	numerator_nicks,
-					"divide_denominator_nicks":	denominator_nicks,
-					"divide_result_nicks":		result_nicks,
-					"divide_denominator_no_errors":	no_errors,
-					"analysis_modules":		analysis_modules,
-					"y_lims":			y_lims,
-					"markers":			markers,
-					"colors":			colors, 
-					"labels":			labels, 
-					"legend":			legend, 
-					"legend_markers":		legend_markers,
-					"y_label":			y_label
-		}	
-	
-		return self._bkgreductionplot
-
-	def __cutflowplot__(self, files, markers, nicks, x_tick_labels, stacks): 
+	def __cutflowplot__(self, subplot_nicks, y_subplot_label, files, markers, nicks, x_tick_labels, stacks): 
 
 		self._cutflowplot = {
-				
+					"subplot_nicks":		subplot_nicks,
+					"y_subplot_label":		y_subplot_label,
 					"files":			files,
 					"markers":			markers, 
 					"nicks":			nicks, 
@@ -178,7 +175,7 @@ class ConfigMaster(object):
 		return self._ratio
 
 
-	def __limitplot__(self, y_label, files, folders, y_expressions, markers, colors, fill_styles, marker_styles, line_widths, tree_draw_options, y_tick_labels, nicks, x_lims, y_lims):
+	def __limitplot__(self, y_label, files, folders, y_expressions, markers, colors, fill_styles, marker_styles, line_widths, tree_draw_options, y_tick_labels, nicks, y_lims, x_lims):
 
 		self._limitplot = {
 					"y_label":			y_label,
@@ -193,12 +190,13 @@ class ConfigMaster(object):
 					"tree_draw_options":		tree_draw_options,
 					"y_tick_labels":		y_tick_labels,
 					"nicks":			nicks,
+					"y_lims":			y_lims,
 					"x_lims":			x_lims,
-					"y_lims":			y_lims
 					
 		}
 
 		return self._limitplot
+
 
 	###Function for adding information to the config
 
@@ -207,17 +205,14 @@ class ConfigMaster(object):
 		###Fill config will all module information checking if values are already avaible as list or string
 		for (key, value) in self._modules_dict[module](*module_values).iteritems():
 			try:
-				if type(value) == list:
-					self._config[key].extend(value)
-				
-				else:
-					self._config[key].append(value)
+				self._config[key].append(value)
 
 			except AttributeError:
 				self._config[key] += value
 
 			except KeyError:
 				self._config[key] = value
+
 
 	##Change specific values or add new key by hand
 
@@ -263,6 +258,16 @@ class ConfigMaster(object):
 			self._config.pop(config_keys)
 		
 
+	##Function for recplacing value of a key
+	
+	def replace(self, config_keys, config_values):
+		if type(config_keys) == list:
+			for key, value in zip(config_keys, config_values):
+				self._config[key] = value
+
+		else:
+			self._config[config_keys] = config_values
+		
 	##Merges merge config into the config of the configmaster by keys you give the function
 
 	def merge_by_keys(self, merge_config, keys):
@@ -273,8 +278,15 @@ class ConfigMaster(object):
 			else: 
 				self._config[key].append(merge_config[key])
 
-	##Using pretty print to print actual config
+	##Return a copy of the configmaster object
 
+	def copy(self):
+		from copy import deepcopy
+		copied_config = deepcopy(self)
+
+		return copied_config
+
+	##Using pretty print to print actual config
 	def print_config(self):
 		import pprint
 		pprint.pprint(self._config)
