74d98fccc26a58cb68aa78086b96935849edad5f
DaveBrun94 <david.brunner@rwth-aachen.de>
2018-03-05 17:39:07 +0100

    Producer correction jet distribution in LFV samples of 2016

---
+++ interface/Producers/LFVJetCorrection2016Producer.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include "HiggsAnalysis/KITHiggsToTauTau/interface/HttTypes.h"
+
+class LFVJetCorrection2016Producer: public ProducerBase<HttTypes> {
+public:
+
+	virtual void Init(setting_type const& settings, metadata_type& metadata) override;
+
+	virtual void Produce(event_type const& event, product_type& product,
+	                     setting_type const& settings, metadata_type const& metadata) const override;
+};
+
