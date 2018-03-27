17e5ea31a0f16b86769da2c813d8c71d27e90f87
Wolfschläger Dominik <dominik.wolfschlaeger@googlemail.com>
2018-03-27 14:42:48 +0200

    Adjust binning.

--- python/plotting/configs/binnings.py
+++ python/plotting/configs/binnings.py
@@ -667,7 +667,7 @@ class BinningsDict(binnings.BinningsDict):
 			self.binnings_dict["binningHtt13TeV_"+channel+"_dijet2D_boosted_melaDiscriminatorD0MinusGGH"] = "4,0.0,1.0"
 			self.binnings_dict["binningHtt13TeV_"+channel+"_dijet2D_lowboost_jdphi"] = "12,-3.2,3.2"
 			
-			self.binnings_dict["binningHtt13TeV_"+channel+"_dijet2D_lowboost_dcp_star"] = "8,-1.0,1.0"
+			self.binnings_dict["binningHtt13TeV_"+channel+"_dijet2D_lowboost_dcp_star"] = "4,-1.0,1.0"
 			self.binnings_dict["binningHtt13TeV_"+channel+"_dijet2D_lowboost_melaDiscriminatorDCPGGH"] = "2,-1.0,1.0"
 			self.binnings_dict["binningHtt13TeV_"+channel+"_dijet2D_lowboost_melaDiscriminatorD0MinusGGH"] = "4,0.0,1.0"	
 					
