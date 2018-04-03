90c8694373565f6892fea2f989ea0f57591a2e18
jordy <jordy.degens@rwth-aachen.de>
2018-04-03 16:04:19 +0200

    added the singletauquantities

---
+++ python/ArtusConfigs/Includes/SingleTauQuantities.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#this are functions that return python list, the lists are the same as in the json quantities files. Idea is that one can add the list to the quantities to be read in.
+
+
+def SingleTauQuantities():
+	return ["run",
+		"lumi",
+		"evt",
+		"npv",
+		"npu",
+		"rho",
+		"nLooseElectrons",
+		"nLooseMuons",
+		"nElectrons",
+		"nMuons",
+		"nTaus",
+		"leadingElePt",
+		"leadingEleEta",
+		"leadingEleIso",
+		"leadingEleIsoOverPt",
+		"leadingMuonPt",
+		"leadingMuonEta",
+		"leadingMuonIso",
+		"leadingMuonIsoOverPt",
+		"leadingTauPt",
+		"leadingTauEta",
+		"leadingTauIso",
+		"leadingTauIsoOverPt",
+		"trailingTauPt",
+		"trailingTauEta",
+		"trailingTauIso",
+		"trailingTauIsoOverPt",
+		"diTauPt",
+		"diTauEta",
+		"diTauPhi",
+		"diTauMass",
+		"diTauSystemReconstructed",
+		"weight",
+		"collinearMass"
+	]
+
