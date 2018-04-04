386490978e49de47472d1f13e2222e0fb603635f
hale sert <sert@physik.rwth-aachen.de>
2018-04-04 18:07:08 +0200

    samples_2017.py is updated for Fall17 samples

--- python/plotting/configs/samples_run2_2017.py
+++ python/plotting/configs/samples_run2_2017.py
@@ -16,45 +16,49 @@ from Kappa.Skimming.registerDatasetHelper import get_nick_list
 
 from Artus.Utility.tools import make_multiplication, split_multiplication, clean_multiplication
 energy = 13
-default_lumi =  42.0*1000.0
+default_lumi =  41.37*1000.0
 
 class Samples(samples.Samples):
 
 
 	# constants for all plots
 	data_format = "MINIAOD"
-	mc_campaign = "RunIISummer17MiniAOD"
-
+	mc_campaign = "RunIIFall17MiniAOD"
 	#NOTES: zPtReweightWeight is used for 2016 values currently.
 
 	# For 2017 v1 MC samples, remove the npartons=4 part since we don't have the sample yet, and also remove the highmass part in your return value if you have don't use the high mass sample!
 	def ztt_stitchingweight(self):
 		highmass = "((genbosonmass >= 150.0 && (npartons == 0 || npartons >= 5))*1.25469132e-3) + ((genbosonmass >= 150.0 && npartons == 1)*1.17290378e-3) + ((genbosonmass >= 150.0 && npartons == 2)*1.17845742e-3) + ((genbosonmass >= 150.0 && npartons == 3)*1.18139540e-3)+((genbosonmass >= 150.0 && npartons == 4)*1.15891212e-3)+"
-		mediummass = "((genbosonmass >= 50.0 && genbosonmass < 150.0 && (npartons == 0 || npartons >= 5))*5.75970078e-5) + ((genbosonmass >= 50.0 && genbosonmass < 150.0 && npartons == 1)*1.36277241e-5) + ((genbosonmass >= 50.0 && genbosonmass < 150.0 && npartons == 2)*7.42888435e-6) + ((genbosonmass >= 50.0 && genbosonmass < 150.0 && npartons == 3)*1.62808443e-5)+ ((genbosonmass >= 50.0 && genbosonmass < 150.0 && npartons == 4)*5.75970078e-5)+"
-		#mediummass = "((genbosonmass >= 50.0 && genbosonmass < 150.0 && (npartons == 0 || npartons >= 5))*5.75970078e-5) + ((genbosonmass >= 50.0 && genbosonmass < 150.0 && npartons == 1)*1.36277241e-5) + ((genbosonmass >= 50.0 && genbosonmass < 150.0 && npartons == 2)*7.42888435e-6) + ((genbosonmass >= 50.0 && genbosonmass < 150.0 && npartons == 3)*1.62808443e-5)+"
+		mediummass = "((genbosonmass >= 50.0 && genbosonmass < 150.0 && (npartons == 0 || npartons >= 5))*5.94801691e-5) + ((genbosonmass >= 50.0 && genbosonmass < 150.0 && npartons == 1)*1.35840903e-5) + ((genbosonmass >= 50.0 && genbosonmass < 150.0 && npartons == 2)*1.39144195e-5) + ((genbosonmass >= 50.0 && genbosonmass < 150.0 && npartons == 3)*1.74929571e-5)+ ((genbosonmass >= 50.0 && genbosonmass < 150.0 && npartons == 4)*1.17813305e-5)"
 		lowmass = "((genbosonmass < 50.0)*numberGeneratedEventsWeight*crossSectionPerEventWeight)"
 		normalization = "/(numberGeneratedEventsWeight*crossSectionPerEventWeight*sampleStitchingWeight)"
-		return "("+highmass+mediummass+lowmass+")"+normalization
+		#return "("+highmass+mediummass+lowmass+")"+normalization
 		#return "("+mediummass+lowmass+")"+normalization
-
+		return "("+mediummass+")"+normalization
 
 	# DYJetsToLLM_150 sample currently only contains Z->tautau decays
 	def zll_stitchingweight(self):
-		mediummass = "((genbosonmass >= 50.0 && (npartons == 0 || npartons >= 5))*5.75970078e-5) + ((genbosonmass >= 50.0 && npartons == 1)*1.36277241e-5) + ((genbosonmass >= 50.0 && npartons == 2)*7.42888435e-6) + ((genbosonmass >= 50.0 && npartons == 3)*1.62808443e-5) + ((genbosonmass >= 50.0 && npartons == 4)*5.75970078e-5)+"
+		mediummass = "((genbosonmass >= 50.0 && (npartons == 0 || npartons >= 5))*5.94801691e-5) + ((genbosonmass >= 50.0 && npartons == 1)*1.35840903e-5) + ((genbosonmass >= 50.0 && npartons == 2)*1.39144195e-5) + ((genbosonmass >= 50.0 && npartons == 3)*1.74929571e-5) + ((genbosonmass >= 50.0 && npartons == 4)*1.17813305e-5)"
 		lowmass = "((genbosonmass < 50.0)*numberGeneratedEventsWeight*crossSectionPerEventWeight)"
 		normalization = "/(numberGeneratedEventsWeight*crossSectionPerEventWeight*sampleStitchingWeight)"
-		return "("+mediummass+lowmass+")"+normalization
-
+		#return "("+mediummass+lowmass+")"+normalization
+		return "("+mediummass+")"+normalization
+		
 	def wj_stitchingweight(self):
-		return "(((npartons == 0 || npartons >= 5)*2.36006270e-3) + ((npartons == 1)*2.34817764e-4) + ((npartons == 2)*1.31144867e-4) + ((npartons == 3)*1.39177532e-4) + ((npartons == 4)*6.46064804e-5))/(numberGeneratedEventsWeight*crossSectionPerEventWeight*sampleStitchingWeight)"
+		return "(((npartons == 0 || npartons >= 5)*2.64738024e-3) + ((npartons == 1)*3.46943126e-4) + ((npartons == 2)*1.82919464e-4) + ((npartons == 3)*5.80550830e-5) + ((npartons == 4)*9.11576490e-5))/(numberGeneratedEventsWeight*crossSectionPerEventWeight*sampleStitchingWeight)"
 
+	def vv_stitchingweight(self):
+		return "(1.22671436926e-6)/(numberGeneratedEventsWeight*crossSectionPerEventWeight)"
+
+	def ttbar_stitchingweight(self):
+		return "(1)/(numberGeneratedEventsWeight*crossSectionPerEventWeight)"
 
 	def files_data(self, channel):
 		query_rereco = {}
 		query_promptreco = {}
 		query_reminiaod = {}
-		expect_n_results_rereco = 2 # adjust in if-statements if different depending on channel
-		expect_n_results_promptreco = 3
+		expect_n_results_rereco = 5 # adjust in if-statements if different depending on channel
+		expect_n_results_promptreco = 1
 		#expect_n_results_reminiaod = 8
 		expect_n_results_reminiaod = 0
 		if channel == "mt":
@@ -85,19 +89,19 @@ class Samples(samples.Samples):
 			log.error("Sample config (Data) currently not implemented for channel \"%s\"!" % channel)
 
 		query_rereco["data"] = True
-		query_rereco["campaign"] = "Run2017(B|C)"
-		query_rereco["scenario"] = "12Sep2017v(1|3)"
+		query_rereco["campaign"] = "Run2017(B|C|D|E|F)"
+		query_rereco["scenario"] = "17Nov2017v1"
 		query_promptreco["data"] = True
-		query_promptreco["campaign"] = "Run2017(D|E|F)"
+		query_promptreco["campaign"] = "Run2017(E)"
 		query_promptreco["scenario"] = "PromptRecov(1|2)"
 		query_reminiaod["data"] = True
-		query_reminiaod["campaign"] = "Run2017(B|C|D|E|F|G|H)"
+		query_reminiaod["campaign"] = "Run2017(B|C|D|E|F)"
 		query_reminiaod["scenario"] = "12Sep2017*"
 
 		rereco_files = self.artus_file_names(query_rereco, expect_n_results_rereco)
 		promptreco_files = self.artus_file_names(query_promptreco, expect_n_results_promptreco)
 		reminiaod_files = self.artus_file_names(query_reminiaod, expect_n_results_reminiaod)
-		return rereco_files + " " + promptreco_files
+		return rereco_files 
 		#return reminiaod_files
 
 	def data(self, config, channel, category, weight, nick_suffix, exclude_cuts=None, cut_type="baseline", **kwargs):
@@ -126,10 +130,12 @@ class Samples(samples.Samples):
 
 
 	def files_ztt(self, channel):
-		return self.artus_file_names({"process" : "(DYJetsToLLM50|DYJetsToLLM50v1|DY1JetsToLLM50|DY2JetsToLLM50|DY3JetsToLLM50)", "data": False, "campaign" : self.mc_campaign, "generator" : "madgraph\-pythia8"}, 5)
+		artus_files = self.artus_file_names({"process" : "(DYJetsToLLM50|DY1JetsToLLM50|DY2JetsToLLM50|DY3JetsToLLM50|DY4JetsToLLM50)", "data": False, "campaign" : self.mc_campaign, "generator" : "madgraph\-pythia8"}, 8)
+		return artus_files
 
 	def files_zll(self, channel):
-		return self.artus_file_names({"process" : "(DYJetsToLLM50|DYJetsToLLM50v1|DY1JetsToLLM50|DY2JetsToLLM50|DY3JetsToLLM50)", "data": False, "campaign" : self.mc_campaign, "generator" : "madgraph\-pythia8"},5)
+		artus_files = self.artus_file_names({"process" : "(DYJetsToLLM50|DY1JetsToLLM50|DY2JetsToLLM50|DY3JetsToLLM50|DY4JetsToLLM50)", "data": False, "campaign" : self.mc_campaign, "generator" : "madgraph\-pythia8"}, 8)
+		return artus_files
 
 	def files_ewkwm(self, channel):
 		return None
@@ -145,9 +151,10 @@ class Samples(samples.Samples):
 
 	def files_ttj(self, channel):
 		if self.ttbar_retuned:
-			return self.artus_file_names({"process" : "TTTo.*", "data": False, "campaign" : self.mc_campaign}, 2)
+			return self.artus_file_names({"process" : "TTTo.*", "data": False, "campaign" : self.mc_campaign}, 3)
 		else:
-			return self.artus_file_names({"process" : "TTv1_|TTv2_", "data": False, "campaign" : self.mc_campaign}, 2)
+			return self.artus_file_names({"process" : "TTTo.*", "data": False, "campaign" : self.mc_campaign}, 3)
+
 
 	def files_diboson(self, channel):
 
@@ -157,7 +164,7 @@ class Samples(samples.Samples):
 		                                    + "ZZ"
 		                                    +  ")",
 		                      "data" : False, "campaign" : self.mc_campaign, "generator" : "pythia8"}, 3)
-		artus_files = artus_files + " " + self.artus_file_names({ "process" : "(STtWantitop5finclusiveDecays|STtWtop5finclusiveDecays)",
+		artus_files = artus_files + " " + self.artus_file_names({ "process" : "(STtWantitop5finclusiveDecaysTuneCP5|STtWtop5finclusiveDecaysTuneCP5)",
 		                      "data" : False, "campaign" : self.mc_campaign}, 2)
 		return artus_files
 
