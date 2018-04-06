4c8caea4d01ebf554e6a0b5cf3235a02f315903c
Wolfschläger Dominik <dominik.wolfschlaeger@googlemail.com>
2018-04-06 11:50:49 +0200

    Make cuts used more obvious/ readable

--- python/plotting/configs/cutstrings.py
+++ python/plotting/configs/cutstrings.py
@@ -55,7 +55,8 @@ class CutStringsDict:
 				cuts["trg"] = "(trg_singlemuon == 1)"
 			elif "smhtt2016" or "cp2016" in cut_type:
 				# trigger weights are saved as optional weights, and thus need to be applied here
-				cuts["trg"] = "((trg_mutaucross == 1)*(triggerWeight_muTauCross_1)*(triggerWeight_muTauCross_2)*(pt_1 > 20)*(pt_1 <= 23)+(trg_singlemuon == 1)*(triggerWeight_singleMu_1)*(pt_1 > 23))"
+				cuts["trg"] = "((trg_mutaucross == 1)*(triggerWeight_muTauCross_1)*(triggerWeight_muTauCross_2)*(pt_1 <= 23)+(trg_singlemuon == 1)*(triggerWeight_singleMu_1)*(pt_1 > 23))"
+				cuts["pt_1"] = "(pt_1 > 20.0)"
 				cuts["pt_2"] = "(pt_2 > 30.0)"
 			cuts["mt"] = "(mt_1<40.0)" if "mssm2016" in cut_type else "(mt_1<30.0)" if "mssm" in cut_type else "(mt_1<50.0)" if "2016" in cut_type else "(mt_1<40.0)"
 			cuts["anti_e_tau_discriminators"] = "(againstElectronVLooseMVA6_2 > 0.5)"
@@ -69,6 +70,7 @@ class CutStringsDict:
 				cuts["trg"] = "(trg_singleelectron == 1)"
 			elif "smhtt2016" or "cp2016" in cut_type:
 				cuts["pt_2"] = "(pt_2 > 30.0)"
+				cuts["pt_1"] = "(pt_2 > 25.0)"
 			cuts["mt"] = "(mt_1<50.0)" if "2016" in cut_type else "(mt_1<40.0)"
 			cuts["anti_e_tau_discriminators"] = "(againstElectronTightMVA6_2 > 0.5)"
 			cuts["anti_mu_tau_discriminators"] = "(againstMuonLoose3_2 > 0.5)"
@@ -81,6 +83,7 @@ class CutStringsDict:
 				cuts["trg"] = "(trg_doubletau == 1)"
 			elif "smhtt2016" or "cp2016" in cut_type:
 				cuts["pt_1"] = "(pt_1 > 50.0)"
+				cuts["pt_2"] = "(pt_1 > 40.0)"
 			cuts["extra_lepton_veto"] = "(extraelec_veto < 0.5)*(extramuon_veto < 0.5)"
 			cuts["anti_e_tau_discriminators"] = "(againstElectronVLooseMVA6_1 > 0.5)*(againstElectronVLooseMVA6_2 > 0.5)"
 			cuts["anti_mu_tau_discriminators"] = "(againstMuonLoose3_1 > 0.5)*(againstMuonLoose3_2 > 0.5)"
