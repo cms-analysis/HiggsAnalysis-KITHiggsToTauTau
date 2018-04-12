dbf722d8f3831d842c54d1d1bfa86aee80c7b5b4
jordy <jordy.degens@rwth-aachen.de>
2018-04-12 17:04:59 +0200

    added weightquantities from here

--- python/ArtusConfigs/Includes/SingleTauQuantities.py
+++ python/ArtusConfigs/Includes/SingleTauQuantities.py
@@ -39,3 +39,39 @@ def SingleTauQuantities():
 		"collinearMass"
 	]
 
+def weightQuantities():
+	return [
+		"hltWeight",
+		"triggerWeight_1",
+		"triggerWeight_2",
+		"identificationWeight_1",
+		"identificationWeight_2",
+		"puWeight",
+		"tauEnergyScaleWeight",
+		"generatorWeight",
+		"crossSectionPerEventWeight",
+		"numberGeneratedEventsWeight",
+		"embeddingWeight",
+		"eventWeight",
+		"sampleStitchingWeight",
+		"antiEVLooseSFWeight_1",
+		"antiELooseSFWeight_1",
+		"antiEMediumSFWeight_1",
+		"antiETightSFWeight_1",
+		"antiEVTightSFWeight_1",
+		"antiEVLooseSFWeight_2",
+		"antiELooseSFWeight_2",
+		"antiEMediumSFWeight_2",
+		"antiETightSFWeight_2",
+		"antiEVTightSFWeight_2",
+		"emuQcdWeightUp",
+		"emuQcdWeightNom",
+		"emuQcdWeightDown",
+		"topPtReweightWeight",
+		"topPtReweightWeightRun1",
+		"topPtReweightWeightRun2",
+		"zPtReweightWeight",
+		"eleTauFakeRateWeight",
+		"muTauFakeRateWeight"
+	]
+
