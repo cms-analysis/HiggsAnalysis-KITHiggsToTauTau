2f48b256cd9eb5163e9dc57f009ac8580c153d46
DaveBrun94 <david.brunner@rwth-aachen.de>
2018-04-19 16:46:08 +0200

    Function for writing splitted input trees, for training and for the application step are now implented

--- python/lfv/MVAFlavio.py
+++ python/lfv/MVAFlavio.py
@@ -1,69 +1,321 @@
-import ROOT
-import sys
-import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.samples_run2_2016 as samples
-from pprint import pprint
-import glob
-import subprocess
+#! /usr/bin/env python
 
+import ROOT
+
+from multiprocessing import Pool
+import argparse
+import array
+from math import cos, sin, acos, fabs
 
 ##Define constants
 sample_dir = "/net/scratch_cms3b/brunner/artus/AllSamples/merged/"
 tree_name = "{channel}_nominal/ntuple"
-output_root = "$CMSSW_BASE/src/plots/FlavioOutput/{channel}_MVA.root"
+input_file = "$CMSSW_BASE/src/plots/FlavioOutput/MVA_trees_{sample_type}{cut_label}_{channel}.root"
+output_root = "$CMSSW_BASE/src/plots/FlavioOutput/output{cut_label}_{channel}.root"
 
 sig_list = ["zem"]
 bkg_list = ["ztt", "zll", "ttj", "vv", "wj"]
-parameter = ["pt_1", "pt_2", "d0_refitPV_1", "d0_refitPV_2", "diLepLV.Pt()", "met", "pZetaMissVis", "phi_1 - phi_2", "m_vis"]
+parameter = ["deltaPhi_ll:= acos(cos(phi_1)*cos(phi_2) + sin(phi_1)*sin(phi_2))", "pt_1", "pt_2", "ptvis", "met", "pZetaMissVis", "m_vis", "mt_1", "mt_2"]
+user_def_parameter = {
+			0:	[lambda phi_1, phi_2: acos(cos(phi_1)*cos(phi_2) + sin(phi_1)*sin(phi_2)), ["phi_1", "phi_2"]],
+}
+	
 
+##Argument parser
+def parser():
+	parser = argparse.ArgumentParser(description = "Script for using TMVA method in LFV analysis", formatter_class=argparse.RawTextHelpFormatter)
 
-def get_input_files(channel):
+	parser.add_argument("--create-input-trees", action = "store_true", default = False, help = "Do the splitting of background and signal samples into even and odd event number sample")
+	parser.add_argument("--training", action = "store_true", default = False, help = "Do the training on the background/signal samples")
+	parser.add_argument("--application", action = "store_true", default = False, help = "Use the trained BDT to apply on background/data and append it in the ROOT files")
+	parser.add_argument("--file-attach", action = "store", help = "Attach BDT score in ROOT File. Calling --application call this option for all files and submit it to the batch system.")
+	parser.add_argument("--results", action = "store", help = "Give .xml or .root output from TMVA to show results with the TMVA GUI")
+
+	return parser.parse_args()
+	
+
+##Create input trees for BDT method, splitting signal and background events for each channel in even and odd event numbered
+def create_input_trees():
+	import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.samples_run2_2016 as samples
+	import glob
+	
 	sample_settings = samples.Samples()
 	mc_list = []
 
+	##Get file names using samples_run2_2016
 	for sample_list in [sig_list, bkg_list]:
 		mc_set = set()
-		config = sample_settings.get_config(samples=[getattr(samples.Samples, sample) for sample in sample_list], channel = channel, category = None, cut_type = "lfv")
+		config = sample_settings.get_config(samples=[getattr(samples.Samples, sample) for sample in sample_list], channel = "em", category = None, cut_type = "lfv")
 		mc_set.update([glob.glob(sample_dir + mc_name)[0] for mc_name in [file_name for file_list in [config_string.split(" ") for config_string in config["files"]] for file_name in file_list]])
 		mc_list.append(list(mc_set))
 
-	return mc_list
+	for channel in ["em", "et", "mt"]:
+		##Create signal/background chain and read in all files
+		sig_chain = ROOT.TChain(tree_name.format(channel = channel))
+		bkg_chain = ROOT.TChain(tree_name.format(channel = channel))
 
-def MVA(channel, mc_list):
-	output = ROOT.TFile(output_root.format(channel=channel), "RECREATE")
-	factory = ROOT.TMVA.Factory(channel, output)
+		for sig_file in mc_list[0]:
+			sig_chain.Add(sig_file)
 
-	sig_trees = ROOT.TChain(tree_name.format(channel = channel))
-	bkg_trees = ROOT.TChain(tree_name.format(channel = channel))
+		for bkg_file in mc_list[1]:
+			bkg_chain.Add(bkg_file)
 
-	for sig_file in mc_list[0]:
-		sig_trees.Add(sig_file)
-
-	for bkg_file in mc_list[1]:
-		bkg_trees.Add(bkg_file)			
+		##Deactivate branches, which are not avaiable in all files
+		invalid_branch = ["genBosonLV", "genBosonLep1LV", "genBosonLep2LV", "genBosonTau1LV", "genBosonTau2LV", "genBosonTau1VisibleLV", "genBosonTau2VisibleLV"]
 	
+		for branch in invalid_branch:
+			sig_chain.SetBranchStatus(branch, 0)
+			bkg_chain.SetBranchStatus(branch, 0)
+	
+		##Create input root files in paralell
+		pool = Pool(4)
+		pool.map(write_root_file, [[sig_chain, "event%2==0", "sig", channel], [sig_chain, "event%2==1", "sig", channel], [bkg_chain, "event%2==0", "bkg", channel], [bkg_chain, "event%2==1", "bkg", channel]])
+
+
+##Function writing root files in paralell
+def write_root_file(args):
+	chain, cut, sample_type, channel = args
+
+	new_file = ROOT.TFile(input_file.format(sample_type = sample_type, cut_label = "A" if cut == "event%2==0" else "B", channel=channel), "RECREATE")
+	splitted_tree = chain.CopyTree(cut)
+	splitted_tree.SetName(channel)
+	splitted_tree.Write()
+	new_file.Close()
+
+	print "The input file " + input_file.format(sample_type = sample_type, cut_label = "A" if cut == "event%2==0" else "B", channel=channel) + " is written."
+
+
+##Do the training/evaluaton for the splitted samples for each channel
+def training():
+	##Config arguments for do_training function
+	pool = Pool(4)
+		
+	for channel in ["em", "et", "mt"]:
+		config_A = [channel, "A"]
+		config_B = [channel, "B"]
+		sub_config = []
+
+		for sample_type, cut_label in zip(["sig", "sig", "bkg", "bkg"], ["A", "B", "A", "B"]):
+			sub_config.append(input_file.format(sample_type = sample_type, cut_label = cut_label, channel=channel))
+			
+		pool.apply_async(do_training, args = (config_A + sub_config,))
+
+		for i, j in [[0,1], [2, 3]]:
+			sub_config[i], sub_config[j] = sub_config[j], sub_config[i] 
+		
+		pool.apply_async(do_training, args = (config_B + sub_config,))
+
+	pool.close()
+	pool.join()
+					
+
+##Function for doing the training in parelell
+def do_training(args):
+	##Retrieve arguments
+	channel, cut_label, sig_train, sig_test, bkg_train, bkg_test = args
+
+	##Get the input trees
+	file_sig_train = ROOT.TFile(sig_train, "READ")
+	tree_sig_train = ROOT.TTree()
+	file_sig_train.GetObject(channel, tree_sig_train)
+
+	file_sig_test = ROOT.TFile(sig_test, "READ")
+	tree_sig_test = ROOT.TTree()
+	file_sig_test.GetObject(channel, tree_sig_test)
+
+	file_bkg_train = ROOT.TFile(bkg_train, "READ")
+	tree_bkg_train = ROOT.TTree()
+	file_bkg_train.GetObject(channel, tree_bkg_train)
+
+	file_bkg_test = ROOT.TFile(bkg_test, "READ")
+	tree_bkg_test = ROOT.TTree()
+	file_bkg_test.GetObject(channel, tree_bkg_test)
+
+	##Create output file
+	output = ROOT.TFile(output_root.format(channel=channel, cut_label = cut_label), "RECREATE")
+	factory = ROOT.TMVA.Factory("BDT_" + channel + "_" + cut_label, output)
+
+	##Add signal/background trees
+	factory.AddSignalTree(tree_sig_train, 1., "Training")
+	factory.AddSignalTree(tree_sig_test, 1., "Testing")
+	factory.AddBackgroundTree(tree_bkg_train, 1., "Training")
+	factory.AddBackgroundTree(tree_bkg_test, 1., "Testing")
+
+	##Set signal/background event weights
+	factory.SetSignalWeightExpression("jetCorrectionWeight")
+	factory.SetBackgroundWeightExpression( "" )
+
+	##Add parameter for training
 	for param in parameter:
 		factory.AddVariable(param)
 
-	
-	factory.AddSignalTree(sig_trees)
-	factory.AddBackgroundTree(bkg_trees)
+	##Configure everything
+	sig_cut = ROOT.TCut("")
+	bkg_cut = ROOT.TCut("")
+	n_option = ROOT.TString("nTrain_Signal=4500:nTest_Signal=4500:nTrain_Background=4500:nTest_Background=4500:SplitMode=Random",) #ROOT.TString("")
 
-	n_option = ROOT.TString("nTrain_Signal=4500:nTest_Signal=4500:nTrain_Background=4500:nTest_Background=4500:SplitMode=Random",)
 
-	factory.PrepareTrainingAndTestTree(ROOT.TCut("d0_refitPV_2 > -900 && d0_refitPV_1 > -900"), ROOT.TCut("d0_refitPV_2 > -900 && d0_refitPV_1 > -900"), n_option)
-	factory.BookMethod(ROOT.TMVA.Types.kBDT, "BDT", "!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20")
+	factory.PrepareTrainingAndTestTree(sig_cut, bkg_cut, n_option)
+	factory.BookMethod(ROOT.TMVA.Types.kBDT, "BDT", "!H:!V:NTrees=500:MinNodeSize=2.5%:MaxDepth=10:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20")
+	#factory.BookMethod(ROOT.TMVA.Types.kKNN, "KNN", "H:nkNN=20:ScaleFrac=0.8:SigmaFact=1.0:Kernel=Gaus:UseKernel=F:UseWeight=T:!Trim")
+	#factory.BookMethod(ROOT.TMVA.Types.kMLP, "MLP", "H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:!UseRegulator")
 
+	##Run the training/evulation
 	factory.TrainAllMethods()
 	factory.TestAllMethods()
 	factory.EvaluateAllMethods()
 	output.Close()
 
 
-def main():
+##Function for creating grid control for the application of the BDT score
+def application():
+	import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.samples_run2_2016 as samples
+	import glob
+	import ConfigParser
+	import os
+	from random import uniform
+	from pprint import pprint
 
-	mc_list = get_input_files("et")	
-	MVA("et", mc_list)
-	ROOT.TMVA.TMVAGui("$CMSSW_BASE/src/plots/FlavioOutput/{channel}_MVA.root".format(channel="et"))
-	raw_input("Press Enter to exit")
+	sample_settings = samples.Samples()
+	mc_list = []
+	mc_set = set()
+
+	##Get file names using samples_run2_2016
+	for sample_list in [["data"], bkg_list, sig_list]:
+		for channel in ["em", "et", "mt"]:
+			config = sample_settings.get_config(samples=[getattr(samples.Samples, sample) for sample in sample_list], channel = channel, category = None, cut_type = "lfv")
+			mc_set.update([glob.glob(sample_dir + mc_name)[0] for mc_name in [file_name for file_list in [config_string.split(" ") for config_string in config["files"]] for file_name in file_list]])
+			
+	for file_name in list(mc_set):			
+		mc_list.append(file_name)
+
+	##Write bash script for grid control
+
+	##Write config for grid control
+	seed = str(int(uniform(1, 10000)))
+
+	Config = ConfigParser.ConfigParser()
+	Config.optionxform = str
+	config_name = "config" + seed + ".cfg"
+	config = open(config_name, "w")
+
+	sections = ["global", "jobs", "UserTask", "parameters"]
+	entries = [
+			[("task", "UserTask"), ("backend", "condor"), ("cmdargs", "-c -G"), ("workdir create", True), ("workdir", "/tmp/Flavio_work" + seed), ("dataset", "dataset.file")],
+			[("wall time", "1:00"), ("max retry", 5)],
+			[("executable", "kappa.sh")],
+			[("parameters", "FILE_NAME"), ("FILE_NAME", " ".join(mc_list[0]))]
+	]
+
+	for section in sections: 	
+		Config.add_section(section)
+
+	for section, entry in zip(sections, entries):
+		for key, value in entry:
+			Config.set(section, key, value)
+
+	Config.write(config)
+	config.close()
+
+	#Send jobs to the batch system
+	os.system("go.py {config_name}".format(config_name = config_name))
+	os.system("rm config*.cfg")
+	
+
+##Function for attaching BDT score on root file
+def attach(file_name):
+	print "File which is processed: " + file_name
+
+	##Prepare input variables and BDT_score variable
+	variables = [array.array("f", [0]) for param in parameter]
+	BDT_score = array.array("f", [0])
+
+	root_file = ROOT.TFile(file_name, "UPDATE")
+	list_of_keys = [key.GetName() for key in root_file.GetListOfKeys()]
+	root_file.Close()
+
+	
+	##Set up TMVA reader method
+	reader = ROOT.TMVA.Reader()		
+	for param, variable in zip(parameter, variables):
+		reader.AddVariable(param, variable)
+
+	for channel in ["em", "et", "mt"]:
+		method_A = "BDT_A_{channel}".format(channel=channel)
+		method_B = "BDT_B_{channel}".format(channel=channel)
 		
-main()
+		reader.BookMVA(method_A, "/.automount/home/home__home2/institut_3b/brunner/analysis/master/CMSSW_8_1_0/src/weights/BDT_{channel}_A_BDT.weights.xml".format(channel = channel))
+		reader.BookMVA(method_B, "/.automount/home/home__home2/institut_3b/brunner/analysis/master/CMSSW_8_1_0/src/weights/BDT_{channel}_B_BDT.weights.xml".format(channel = channel))
+		
+		for key in list_of_keys:
+			if channel in key[:2]:
+				root_file = ROOT.TFile(file_name, "UPDATE")
+				tree = root_file.Get(key + "/ntuple")
+				print "Channel which is evaluated: {key}".format(key = key)
+	
+	
+				##Delete branch with BDT_score if already there
+				if tree.GetLeaf("BDT_score"):	
+					tree.GetListOfLeaves().Remove(tree.GetLeaf("BDT_score"))
+					tree.GetListOfBranches().Remove(tree.GetBranch("BDT_score"))
+
+				##Create (new) branch for BDT_score and fill it
+				branch = tree.Branch("BDT_score", BDT_score, "BDT_score/F")
+
+				for index, event in enumerate(tree):
+					for index2, (param, variable) in enumerate(zip(parameter, variables)):
+						##Try to get value for variable from tree, if it is user defined, the function in the constant section will be used
+						try:
+							variable[0] = getattr(event, param)
+		
+						except AttributeError:
+							user_func, params = user_def_parameter[index2] 
+							variable[0] = user_func(*[getattr(event, param_string) for param_string in params]) 
+
+					##Get score for event/odd event and fill the branch
+					if event.event % 2 == 0:
+						BDT_score[0] = reader.EvaluateMVA(method_B)
+	
+					if event.event % 2 == 1:
+						BDT_score[0] = reader.EvaluateMVA(method_A)
+
+					if index % 10000 == 0:
+						print "Number of events evaluated: {index}".format(index = index)
+ 
+					branch.Fill()
+	
+				##Overwrite old tree and close file
+				root_file.cd(key)
+				tree.Write("", ROOT.TObject.kOverwrite)
+				root_file.Close()
+				print "BDT score was sucessfully applied in tree: " + key + "/ntuple"
+
+
+
+##Use TMVA Gui to show trainings results
+def show_results(file_name):
+	ROOT.TMVA.TMVAGui(file_name)
+	raw_input("Press Enter to exit")
+			
+
+def main():
+	args = parser()
+
+	if(args.create_input_trees):
+		create_input_trees()
+
+	if(args.training):	
+		training()
+
+	if(args.application):
+		application()
+
+	if(args.file_attach):
+		attach(args.file_attach)
+
+	if(args.results):
+		show_results(args.results)
+
+
+if __name__ == "__main__":		
+	main()
