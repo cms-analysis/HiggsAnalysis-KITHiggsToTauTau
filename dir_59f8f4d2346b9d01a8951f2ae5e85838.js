20edbd002f1aae6aca99c32d532f27f3df356a27
Documentation Builder <peter.fackeldey@rwth-aachen.de>
2018-04-24 16:55:41 +0000

    Automated documentation build for changeset 263fb36d9aa1a688d7a5065face5edc448c46224.

--- dir_59f8f4d2346b9d01a8951f2ae5e85838.js
+++ dir_59f8f4d2346b9d01a8951f2ae5e85838.js
@@ -1,16 +1,13 @@
 var dir_59f8f4d2346b9d01a8951f2ae5e85838 =
 [
-    [ "calculatewosssfactor.py", "calculatewosssfactor_8py.html", "calculatewosssfactor_8py" ],
     [ "cphypotheses.py", "cphypotheses_8py.html", "cphypotheses_8py" ],
     [ "estimatebase.py", "estimatebase_8py.html", "estimatebase_8py" ],
     [ "estimateff.py", "estimateff_8py.html", "estimateff_8py" ],
     [ "estimateqcd.py", "estimateqcd_8py.html", "estimateqcd_8py" ],
-    [ "estimateqcdprefit.py", "estimateqcdprefit_8py.html", "estimateqcdprefit_8py" ],
     [ "estimateqcdtauhadtauhad.py", "estimateqcdtauhadtauhad_8py.html", "estimateqcdtauhadtauhad_8py" ],
     [ "estimatettbar.py", "estimatettbar_8py.html", "estimatettbar_8py" ],
     [ "estimatewjets.py", "estimatewjets_8py.html", "estimatewjets_8py" ],
     [ "estimatewjetsandqcd.py", "estimatewjetsandqcd_8py.html", "estimatewjetsandqcd_8py" ],
-    [ "estimatewjetsandqcdsameequationmethod.py", "estimatewjetsandqcdsameequationmethod_8py.html", "estimatewjetsandqcdsameequationmethod_8py" ],
     [ "estimateztt.py", "estimateztt_8py.html", "estimateztt_8py" ],
     [ "normalizeforpolarisation.py", "normalizeforpolarisation_8py.html", "normalizeforpolarisation_8py" ],
     [ "pullvalues.py", "pullvalues_8py.html", "pullvalues_8py" ],
