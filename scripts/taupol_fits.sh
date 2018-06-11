6cf9622063af71cc4ad96db9d59055b3e2d35cdc
Thomas Mueller <t.muller@cern.ch>
2018-06-11 10:47:56 +0200

    taupol_fits.sh, taupol_scans.sh: minor changes

--- scripts/taupol_fits.sh
+++ scripts/taupol_fits.sh
@@ -23,20 +23,20 @@ for COMBINE_OUTPUT in $1/*/datacards/{individual/*/*,category/*,channel/*,combin
 	echo higgsplot.py -j ${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/plots/configs/combine/best_fit_pol_tot_stat_unc.json \
 		-d `echo ${COMBINE_OUTPUT} | sed -e "s@higgsCombine.pol.tot_unc.MultiDimFit.mH0.root@@g"` \
 		-x 2 --x-lims 0 3 --x-ticks 2 --x-tick-labels \" \" --x-label \"\" --y-lims -1.0 1.0 --title \"r floating\" --y-grid \
-		--www  $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol.tot_unc.MultiDimFit.mH0.root@@g"` --filename best_fit_pol_tot_stat_unc --formats pdf png \
+		--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol.tot_unc.MultiDimFit.mH0.root@@g"` --filename best_fit_pol_tot_stat_unc --formats pdf png \
 		--no-cache
 	
 	# zoomed
 	echo higgsplot.py -j ${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/plots/configs/combine/best_fit_pol_tot_stat_unc.json \
 		-d `echo ${COMBINE_OUTPUT} | sed -e "s@higgsCombine.pol.tot_unc.MultiDimFit.mH0.root@@g"` \
 		-x 2 --x-lims 0 3 --x-ticks 2 --x-tick-labels \" \" --x-label \"\" --y-lims -0.3 0.0 --title \"r floating\" --y-grid \
-		--www  $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol.tot_unc.MultiDimFit.mH0.root@@g"` --filename best_fit_pol_tot_stat_unc_zoom --formats pdf png \
+		--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol.tot_unc.MultiDimFit.mH0.root@@g"` --filename best_fit_pol_tot_stat_unc_zoom --formats pdf png \
 		--no-cache
 
 done | runParallel.py -n 8
 
 
-## ===== r=1 fixed =================================================================================
+# ===== r=1 fixed =================================================================================
 
 # combine
 
@@ -54,14 +54,14 @@ for COMBINE_OUTPUT in $1/*/datacards/{individual/*/*,category/*,channel/*,combin
 	echo higgsplot.py -j ${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/plots/configs/combine/best_fit_pol_r1_tot_stat_unc.json \
 		-d `echo ${COMBINE_OUTPUT} | sed -e "s@higgsCombine.pol_r1.tot_unc.MultiDimFit.mH0.root@@g"` \
 		-x 2 --x-lims 0 3 --x-ticks 2 --x-tick-labels \" \" --x-label \"\" --y-lims -1.0 1.0 --title \"r=1 fixed\" --y-grid \
-		--www  $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol_r1.tot_unc.MultiDimFit.mH0.root@@g"` --filename best_fit_pol_r1_tot_stat_unc --formats pdf png \
+		--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol_r1.tot_unc.MultiDimFit.mH0.root@@g"` --filename best_fit_pol_r1_tot_stat_unc --formats pdf png \
 		--no-cache
 	
 	# zoomed
 	echo higgsplot.py -j ${CMSSW_BASE}/src/HiggsAnalysis/KITHiggsToTauTau/data/plots/configs/combine/best_fit_pol_r1_tot_stat_unc.json \
 		-d `echo ${COMBINE_OUTPUT} | sed -e "s@higgsCombine.pol_r1.tot_unc.MultiDimFit.mH0.root@@g"` \
 		-x 2 --x-lims 0 3 --x-ticks 2 --x-tick-labels \" \" --x-label \"\" --y-lims -0.3 0.0 --title \"r=1 fixed\" --y-grid \
-		--www  $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol_r1.tot_unc.MultiDimFit.mH0.root@@g"` --filename best_fit_pol_r1_tot_stat_unc_zoom --formats pdf png \
+		--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol_r1.tot_unc.MultiDimFit.mH0.root@@g"` --filename best_fit_pol_r1_tot_stat_unc_zoom --formats pdf png \
 		--no-cache
 
 done | runParallel.py -n 8
--- scripts/taupol_scans.sh
+++ scripts/taupol_scans.sh
@@ -24,18 +24,21 @@ for COMBINE_OUTPUT in $1/*/datacards/{individual/*/*,category/*,channel/*,combin
 		-f limit -x pol -y \"2*deltaNLL\" --tree-draw-options TGraph --analysis-modules LikelihoodScan --x-lims -1 1 --y-lims 0 100 \
 		--x-label "\"Average Polarisation #LTP_{#tau}#GT\"" --y-label "\"Likelihood -2#Deltaln L\"" --title "\"r floating\"" -m LP L L L LP L L L --marker-sizes 0.5 --line-styles 1 1 2 3 1 1 2 3 -C 1 1 1 1 2 2 2 2 \
 		--labels \"Stat. + Syst.\" \"\" \"\" \"\" \"Stat.\" \"\" \"\" \"\" --legend-markers LP --legend 0.32 0.72 0.7 0.9 \
-		--www  $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol.tot_unc.scan.MultiDimFit.mH0.root@@g"` --filename higgsCombine.pol.scan.MultiDimFit.mH0 --formats pdf png
+		--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol.tot_unc.scan.MultiDimFit.mH0.root@@g"` --filename higgsCombine.pol.scan.MultiDimFit.mH0 --formats pdf png \
+		--no-cache
 	
 	# zoomed
 	echo higgsplot.py -i ${COMBINE_OUTPUT} `echo ${COMBINE_OUTPUT} | sed -e "s@higgsCombine.pol.tot_unc.scan.MultiDimFit.mH0.root@higgsCombine.pol.stat_unc.scan.MultiDimFit.mH0.root@g"` \
 		-f limit -x pol -y \"2*deltaNLL\" --tree-draw-options TGraph --analysis-modules LikelihoodScan --x-lims -0.3 0 --y-lims 0 10 \
 		--x-label "\"Average Polarisation #LTP_{#tau}#GT\"" --y-label "\"Likelihood -2#Deltaln L\"" --title "\"r floating\"" -m LP L L L LP L L L --marker-sizes 0.5 --line-styles 1 1 2 3 1 1 2 3 -C 1 1 1 1 2 2 2 2 \
 		--labels "\"Stat. + Syst.\"" \"\" \"\" \"\" "\"Stat.\"" \"\" \"\" \"\" --legend-markers LP --legend 0.32 0.72 0.7 0.9 \
-		--www  $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol.tot_unc.scan.MultiDimFit.mH0.root@@g"` --filename higgsCombine.pol.scan.MultiDimFit.mH0_zoom --formats pdf png
+		--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol.tot_unc.scan.MultiDimFit.mH0.root@@g"` --filename higgsCombine.pol.scan.MultiDimFit.mH0_zoom --formats pdf png \
+		--no-cache
+
 done | runParallel.py -n 8
 
 
-## ===== r=1 fixed =================================================================================
+# ===== r=1 fixed =================================================================================
 
 # combine
 
@@ -54,13 +57,16 @@ for COMBINE_OUTPUT in $1/*/datacards/{individual/*/*,category/*,channel/*,combin
 		-f limit -x pol -y \"2*deltaNLL\" --tree-draw-options TGraph --analysis-modules LikelihoodScan --x-lims -1 1 --y-lims 0 100 \
 		--x-label "\"Average Polarisation #LTP_{#tau}#GT\"" --y-label "\"Likelihood -2#Deltaln L\"" --title "\"r=1 fixed\"" -m LP L L L LP L L L --marker-sizes 0.5 --line-styles 1 1 2 3 1 1 2 3 -C 1 1 1 1 2 2 2 2 \
 		--labels \"Stat. + Syst.\" \"\" \"\" \"\" \"Stat.\" \"\" \"\" \"\" --legend-markers LP --legend 0.32 0.72 0.7 0.9 \
-		--www  $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol_r1.tot_unc.scan.MultiDimFit.mH0.root@@g"` --filename higgsCombine.pol_r1.scan.MultiDimFit.mH0 --formats pdf png
+		--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol_r1.tot_unc.scan.MultiDimFit.mH0.root@@g"` --filename higgsCombine.pol_r1.scan.MultiDimFit.mH0 --formats pdf png \
+		--no-cache
 	
 	# zoomed
 	echo higgsplot.py -i ${COMBINE_OUTPUT} `echo ${COMBINE_OUTPUT} | sed -e "s@higgsCombine.pol_r1.tot_unc.scan.MultiDimFit.mH0.root@higgsCombine.pol_r1.stat_unc.scan.MultiDimFit.mH0.root@g"` \
 		-f limit -x pol -y \"2*deltaNLL\" --tree-draw-options TGraph --analysis-modules LikelihoodScan --x-lims -0.3 0 --y-lims 0 10 \
 		--x-label "\"Average Polarisation #LTP_{#tau}#GT\"" --y-label "\"Likelihood -2#Deltaln L\"" --title "\"r=1 fixed\"" -m LP L L L LP L L L --marker-sizes 0.5 --line-styles 1 1 2 3 1 1 2 3 -C 1 1 1 1 2 2 2 2 \
 		--labels "\"Stat. + Syst.\"" \"\" \"\" \"\" "\"Stat.\"" \"\" \"\" \"\" --legend-markers LP --legend 0.32 0.72 0.7 0.9 \
-		--www  $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol_r1.tot_unc.scan.MultiDimFit.mH0.root@@g"` --filename higgsCombine.pol_r1.scan.MultiDimFit.mH0_zoom --formats pdf png
+		--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol_r1.tot_unc.scan.MultiDimFit.mH0.root@@g"` --filename higgsCombine.pol_r1.scan.MultiDimFit.mH0_zoom --formats pdf png \
+		--no-cache
+
 done | runParallel.py -n 8
 
