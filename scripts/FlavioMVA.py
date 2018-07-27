c7c1fef55a7948aac0b3cf7e927447a2794cd6e4
Christian Dappen <dappen@lx3b57.physik.rwth-aachen.de>
2018-07-27 15:49:44 +0200

    channel etm and mte included

--- scripts/FlavioMVA.py
+++ scripts/FlavioMVA.py
@@ -11,10 +11,11 @@ from math import cos, sin, acos, fabs
 
 ##Define constants
 version = ""
-sample_dir = "/net/scratch_cms3b/brunner/artus/AllSamples/merged/"
+sample_dir = "/net/scratch_cms3b/{}/artus/AllSamples/merged/".format(os.environ["USER"])
 tree_name = "{channel}_nominal/ntuple"
 input_file = "$CMSSW_BASE/src/FlavioOutput/BDT/MVA_trees_{sample_type}{cut_label}_{channel}.root"
 output_root = "$CMSSW_BASE/src/FlavioOutput/BDT/output" + str(version) + "{cut_label}_{channel}.root"
+output_result = "$CMSSW_BASE/src/FlavioOutput/BDT/outputB_{channel}.root"
 
 sig_name = "sig"
 bkg_name = "bkg"
@@ -45,18 +46,21 @@ parameter = {
 def parser():
 
 	parser = argparse.ArgumentParser(description = "Script for using TMVA method in LFV analysis", formatter_class=argparse.RawTextHelpFormatter)
+	
+	##NEU: Option channel waehlen
+	parser.add_argument("--channel", nargs="+", required = True, choices = ["em", "et", "mt", "etm", "mte"] , help = "Channel which should be analyzed") 
 
 	parser.add_argument("--create-input-trees", action = "store_true", default = False, help = "Do the splitting of background and signal samples into even and odd event number sample")
 	parser.add_argument("--training", action = "store_true", default = False, help = "Do the training on the background/signal samples")
 	parser.add_argument("--application", action = "store_true", default = False, help = "Use the trained BDT to apply on background/data and append it in the ROOT files")
 	parser.add_argument("--file-attach", action = "store", help = "Attach BDT score in ROOT File. Calling --application call this option for all files and submit it to the batch system.")
-	parser.add_argument("--results", action = "store", help = "Give .xml or .root output from TMVA to show results with the TMVA GUI")
+	parser.add_argument("--results", action = "store_true", help = "Give .xml or .root output from TMVA to show results with the TMVA GUI")
 
 	return parser.parse_args()
 	
 
 ##Create input trees for BDT method, splitting signal and background events for each channel in even and odd event numbered
-def create_input_trees():
+def create_input_trees(channelselection): 
 	import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.samples_run2_2016 as samples
 	import glob
 	
@@ -64,13 +68,23 @@ def create_input_trees():
 	mc_list = []
 
 	##Get file names using samples_run2_2016
-	for sample_list in [["ztt"], bkg_list[1:]]: #[sig_list, bkg_list]:
+	for sample_list in [sig_list, bkg_list]:
 		mc_set = set()
 		config = sample_settings.get_config(samples=[getattr(samples.Samples, sample) for sample in sample_list], channel = "em", category = None, cut_type = "lfv")
 		mc_set.update([glob.glob(sample_dir + mc_name)[0] for mc_name in [file_name for file_list in [config_string.split(" ") for config_string in config["files"]] for file_name in file_list]])
 		mc_list.append(list(mc_set))
+		
+	check_em = False 
 
-	for channel in ["em", "et", "mt"]:
+	for channel in channelselection: 
+		
+		if channel == "em" or channel =="etm" or channel == "mte": 
+			if check_em:
+				continue	
+			else:
+				check_em = True
+				channel = "em" 
+
 		##Create signal/background chain and read in all files
 		sig_chain = ROOT.TChain(tree_name.format(channel = channel))
 		bkg_chain = ROOT.TChain(tree_name.format(channel = channel))
@@ -107,17 +121,17 @@ def write_root_file(args):
 
 
 ##Do the training/evaluaton for the splitted samples for each channel
-def training():
+def training(channelselection): ##NEU: Argument welchen channel man moechte
 	##Config arguments for do_training function
 	pool = Pool(cpu_count())
 		
-	for channel in ["em", "et", "mt"]:
+	for channel in channelselection: 
 		config_A = [channel, "A"]
 		config_B = [channel, "B"]
 		sub_config = []
 
 		for sample_type, cut_label in zip([sig_name, sig_name, bkg_name, bkg_name], ["A", "B", "A", "B"]):
-			sub_config.append(input_file.format(sample_type = sample_type, cut_label = cut_label, channel=channel))
+			sub_config.append(input_file.format(sample_type = sample_type, cut_label = cut_label, channel = "em" if channel== "etm" or channel == "mte" else channel)) 
 			
 		pool.apply_async(do_training, args = (config_A + sub_config,))
 
@@ -134,23 +148,26 @@ def training():
 def do_training(args):
 	##Retrieve arguments
 	channel, cut_label, sig_train, sig_test, bkg_train, bkg_test = args
+	
+
+	treechannel = "em" if channel == "etm" or channel == "mte" else channel
 
 	##Get the input trees
 	file_sig_train = ROOT.TFile(sig_train, "READ")
 	tree_sig_train = ROOT.TTree()
-	file_sig_train.GetObject(channel, tree_sig_train)
+	file_sig_train.GetObject(treechannel, tree_sig_train)
 
 	file_sig_test = ROOT.TFile(sig_test, "READ")
 	tree_sig_test = ROOT.TTree()
-	file_sig_test.GetObject(channel, tree_sig_test)
+	file_sig_test.GetObject(treechannel, tree_sig_test)
 
 	file_bkg_train = ROOT.TFile(bkg_train, "READ")
 	tree_bkg_train = ROOT.TTree()
-	file_bkg_train.GetObject(channel, tree_bkg_train)
+	file_bkg_train.GetObject(treechannel, tree_bkg_train)
 
 	file_bkg_test = ROOT.TFile(bkg_test, "READ")
 	tree_bkg_test = ROOT.TTree()
-	file_bkg_test.GetObject(channel, tree_bkg_test)
+	file_bkg_test.GetObject(treechannel, tree_bkg_test)
 
 	##Create output file
 	output = ROOT.TFile(output_root.format(channel=channel, cut_label = cut_label), "RECREATE")
@@ -165,13 +182,13 @@ def do_training(args):
 	##Get baseline selection	
 	weights = ""
 	
-	for index, weight in enumerate(cuts.CutStringsDict().lfv(channel, "").values()[1:]):
+	for index, weight in enumerate(cuts.CutStringsDict().lfv(treechannel, "").values()[1:]):
 		weights = weights + "*" if index != 0 else weights
 		weights += weight
 
 	##Set signal/background event weights
-	factory.SetSignalWeightExpression("eventWeight*BDT_Ada_score<0*" + weights) #"(jetCorrectionWeight)*(eventWeight)*(lheZto{channel} > 0.5)*".format(channel=channel.upper()) + weights)
-	factory.SetBackgroundWeightExpression("eventWeight*BDT_Ada_score<0*" + weights)
+	factory.SetSignalWeightExpression("(jetCorrectionWeight)*(eventWeight)*(lheZto{channel} > 0.5)*".format(channel=channel[: -1].upper() if channel == "etm" or channel == "mte" else channel.upper() ) + weights)
+	factory.SetBackgroundWeightExpression("eventWeight*" + weights)
 
 	##Add parameter for training
 	for param in parameter.keys():
@@ -180,7 +197,7 @@ def do_training(args):
 	##Configure everything
 	sig_cut = ROOT.TCut("")
 	bkg_cut = ROOT.TCut("")
-	n_option =  ROOT.TString("nTrain_Signal=50000:nTest_Signal=50000:nTrain_Background=50000:nTest_Background=50000:SplitMode=Random",) #ROOT.TString("")
+	n_option =  ROOT.TString("") #ROOT.TString("nTrain_Signal=10000:nTest_Signal=10000:nTrain_Background=10000:nTest_Background=10000:SplitMode=Random",) 
  
 	#"!H:!V::NTrees=100:VarTransform=G,D:MinNodeSize=2.5%:MaxDepth=10:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=SDivSqrtSPlusB:nCuts=20"
 	#"!H:!V:VarTransform=G,D:NTrees=1000:MinNodeSize=2.5%:MaxDepth=2:BoostType=Grad:Shrinkage=0.1:UseBaggedBoost=True:GradBaggingFraction=0.7:SeparationType=GiniIndex:nCuts=20:NodePurityLimit=0.5"	
@@ -196,8 +213,10 @@ def do_training(args):
 	output.Close()
 
 
+	
+
 ##Function for creating grid control for the application of the BDT score
-def application():
+def application(channelselection): 
 	import HiggsAnalysis.KITHiggsToTauTau.plotting.configs.samples_run2_2016 as samples
 	import glob
 	import ConfigParser
@@ -205,17 +224,19 @@ def application():
 	from random import uniform
 	
 	sample_settings = samples.Samples()
-	mc_list = []
-	mc_set = set()
+    	mc_list = []
+    	mc_set = set()
 
-	##Get file names using samples_run2_2016
-	for sample_list in [["data"], bkg_list, sig_list]:
+    	##Get file names using samples_run2_2016
+    	for sample_list in [["data"], bkg_list, sig_list]:
 		for channel in ["em", "et", "mt"]:
-			config = sample_settings.get_config(samples=[getattr(samples.Samples, sample) for sample in sample_list], channel = channel, category = None, cut_type = "lfv")
-			mc_set.update([glob.glob(sample_dir + mc_name)[0] for mc_name in [file_name for file_list in [config_string.split(" ") for config_string in config["files"]] for file_name in file_list]])
-			
-	for file_name in list(mc_set):			
-		mc_list.append(file_name)
+	       	    	config = sample_settings.get_config(samples=[getattr(samples.Samples, sample) for sample in sample_list], channel = channel, category = None, cut_type = "lfv")
+	            	mc_set.update([glob.glob(sample_dir + mc_name)[0] for mc_name in [file_name for file_list in [config_string.split(" ") for config_string in config["files"]] for file_name in file_list]])
+            
+    	for file_name in list(mc_set):            
+	        mc_list.append(file_name)
+
+	#print mc_list
 
 	##Write bash script for grid control
 
@@ -236,7 +257,7 @@ def application():
 				[("task", "UserTask"), ("backend", "condor"), ("cmdargs", "-c -G"), ("workdir create", True), ("workdir", "/tmp/Flavio_work" + seed)],
 				[("wall time", "1:00"), ("max retry", 5)],
 				[("executable", "kappa.sh")],
-				[("parameters", "FILE_NAME"), ("FILE_NAME", " ".join(mc_list))]
+				[("parameters", "FILE_NAME CHANNEL"), ("FILE_NAME", " ".join(mc_list)), ("CHANNEL", "em")] 
 		]
 	
 		for section in sections: 	
@@ -263,7 +284,9 @@ def application():
 		pool.join()
 
 ##Function for attaching BDT score on root file
-def attach(file_name):
+def attach(file_name): 
+	import glob
+
 	print "File which is processed: " + file_name
 
 	##Prepare input variables and BDT_score variable
@@ -280,58 +303,70 @@ def attach(file_name):
 	for param, variable in zip(parameter, variables):
 		reader.AddVariable(param, variable)
 
-	for channel in ["em", "et", "mt"]:
-		method_A = "BDT{version}_A_{channel}".format(channel=channel, version=version)
-		method_B = "BDT{version}_B_{channel}".format(channel=channel, version=version)
+	for channel in ["em", "et", "mt", "etm", "mte"]: 
+
+		outputs = glob.glob(os.environ["CMSSW_BASE"] + "/src/FlavioOutput/BDT/output*")		
+		out_channel = []
+		for out in outputs:
+			out_channel.append(out[89:-5])		
+
+		if channel in out_channel:
+
 		
-		reader.BookMVA(method_A, os.environ["CMSSW_BASE"] + "/src/weights/BDT{version}_{channel}_A_BDT.weights.xml".format(channel = channel, version=version))
-		reader.BookMVA(method_B, os.environ["CMSSW_BASE"] + "/src/weights/BDT{version}_{channel}_B_BDT.weights.xml".format(channel = channel, version=version))
+			method_A = "BDT{version}_A_{channel}".format(channel=channel, version=version)
+			method_B = "BDT{version}_B_{channel}".format(channel=channel, version=version)
 		
-		for key in list_of_keys:
-			if channel in key[:2]:
-				root_file = ROOT.TFile(file_name, "UPDATE")
-				tree = root_file.Get(key + "/ntuple")
-				print "Channel which is evaluated: {key}".format(key = key)
+			reader.BookMVA(method_A, os.environ["CMSSW_BASE"] + "/src/weights/BDT{version}_{channel}_A_BDT.weights.xml".format(channel = channel, version=version))
+			reader.BookMVA(method_B, os.environ["CMSSW_BASE"] + "/src/weights/BDT{version}_{channel}_B_BDT.weights.xml".format(channel = channel, version=version))
+		
+			for key in list_of_keys:
+				channel_key = "em" if channel == "etm" or channel == "mte" else channel 
+				if channel_key in key[:2]:
+					root_file = ROOT.TFile(file_name, "UPDATE")
+					tree = root_file.Get(key + "/ntuple")
+					print "Channel which is evaluated: {key}".format(key = key)
 				
-				##Create (new) branch for BDT_score and fill it
-				branch = tree.Branch("BDT{version}_score".format(version=version), BDT_score, "BDT{version}_score/F".format(version=version))
-				index = 0
+					##Create (new) branch for BDT_score and fill it
+					branch_arg0 = "BDT{version}_score".format(version=version) + ("_{}".format(channel) if channel == "etm" or channel == "mte" else "") 
+					branch_arg2 = "BDT{version}_score/F".format(version=version) + ("_{}".format(channel) if channel == "etm" or channel == "mte" else "")
+					branch = tree.Branch(branch_arg0, BDT_score, branch_arg2)
 				
-				for index, event in enumerate(tree):
-					for index2, (param, variable) in enumerate(zip(parameter.values(), variables)):
-					#Try to get value for variable from tree, if it is user defined, the function in the constant section will be used
-						try:
-							variable[0] = param[0](*[event.__getattr__(var) if type(var) == str else event.__getattr__(var[0]).__getattribute__(var[1])() for var in param[1]])
+					for index, event in enumerate(tree):
+						for index2, (param, variable) in enumerate(zip(parameter.values(), variables)):
+						#Try to get value for variable from tree, if it is user defined, the function in the constant section will be used
+							try:
+								variable[0] = param[0](*[event.__getattr__(var) if type(var) == str else event.__getattr__(var[0]).__getattribute__(var[1])() for var in param[1]])
 	
-						except:
-							variable[0] = 0
+							except:
+								variable[0] = 0
 							
 
-					##Get score for event/odd event and fill the branch
-					if event.event % 2 == 0:
-						BDT_score[0] = reader.EvaluateMVA(method_B)
+						##Get score for event/odd event and fill the branch
+						if event.event % 2 == 0:
+							BDT_score[0] = reader.EvaluateMVA(method_B)
 	
-					if event.event % 2 == 1:
-						BDT_score[0] = reader.EvaluateMVA(method_A)
+						if event.event % 2 == 1:
+							BDT_score[0] = reader.EvaluateMVA(method_A)
 
-					if index % 10000 == 0:
-						print "Number of events evaluated: {index}".format(index = index)
- 
-					branch.Fill()
+						if index % 10000 == 0:
+							print "Number of events evaluated: {index}".format(index = index)
+ 	
+						branch.Fill()
 				
-				##Overwrite old tree and close file
-				root_file.cd(key)
-				tree.Write("", ROOT.TObject.kOverwrite)
-				root_file.Close()
-				print "Number of events evaluated in total: {index}".format(index = index)
-				print "BDT score was sucessfully applied in tree: " + key + "/ntuple"
+					##Overwrite old tree and close file
+					root_file.cd(key)
+					tree.Write("", ROOT.TObject.kOverwrite)
+					root_file.Close()
+					print "Number of events evaluated in total: {index}".format(index = index)
+					print "BDT score was sucessfully applied in tree: " + key + "/ntuple"
 
 
 
 ##Use TMVA Gui to show trainings results
-def show_results(file_name):
-	ROOT.TMVA.TMVAGui(file_name)
-	raw_input("Press Enter to exit")
+def show_results(channel):
+	for chan in channel:
+		ROOT.TMVA.TMVAGui(output_result.format(channel = chan))
+		raw_input("Press Enter to exit")
 			
 
 def main():
@@ -345,19 +380,18 @@ def main():
 		os.mkdir(os.environ["CMSSW_BASE"] + "/src/FlavioOutput/BDT")
 
 	if(args.create_input_trees):
-		create_input_trees()
+		create_input_trees(args.channel) 
 
 	if(args.training):	
-		training()
-
+		training(args.channel) 
 	if(args.application):
-		application()
+		application(args.channel) 
 
 	if(args.file_attach):
-		attach(args.file_attach)
+		attach(args.file_attach) 
 
 	if(args.results):
-		show_results(args.results)
+		show_results(args.channel) 
 
 
 if __name__ == "__main__":		
