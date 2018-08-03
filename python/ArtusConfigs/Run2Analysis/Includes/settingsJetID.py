bbfdce9b4fc155707d478bf66d1ac7e1daa85c62
Wolfschläger Dominik <dominik.wolfschlaeger@googlemail.com>
2018-08-03 14:32:33 +0200

    Use correct JetID for 2016.

--- python/ArtusConfigs/Run2Analysis/Includes/settingsJetID.py
+++ python/ArtusConfigs/Run2Analysis/Includes/settingsJetID.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import re
 import Artus.Utility.logger as logger
 log = logging.getLogger(__name__)
 
@@ -8,7 +9,10 @@ log = logging.getLogger(__name__)
 class Jet_ID(dict):
 	def __init__(self, nickname):
 		self["JetID"] = "loose"
-		self["JetIDVersion"] = "2015"
+		if re.search("Run2016|Spring16|Summer16", nickname):
+			self["JetIDVersion"] = "2016"
+		else:
+			self["JetIDVersion"] = "2015" 
 
 		self["PuJetIDs"] = []
 		self["PuJetIDFullDiscrName"] = "pileupJetIdfullDiscriminant"
