bd9794c93c27ad8cc0d2e7fa4eeae99a5cca4279
Wolfschläger Dominik <dominik.wolfschlaeger@googlemail.com>
2018-08-21 15:02:39 +0200

    Adapted scripts.

--- scripts/alpha_scan_with_muF_constrain.sh
+++ scripts/alpha_scan_with_muF_constrain.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 # Creates the workspaces and likelihoodscans for consraining ggH rate to SM value.
-# combineTool.py -M T2W -P CombineHarvester.CombinePdfs.CPMixture:CPMixture -i $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/{cmb,em,et,mt,tt}/* -o ws.root --parallel 8 --PO constrain_ggHYield
+combineTool.py -M T2W -P CombineHarvester.CombinePdfs.CPMixture:CPMixture -i $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/{cmb,em,et,mt,tt}/* -o ws.root --parallel 8 --PO constrain_ggHYield
 
 # alpha scan
 combineTool.py -m 125 -M MultiDimFit --setParameters muF=1,muV=1,alpha=0,f=0 --freezeParameters f,muF --setParameterRanges alpha=0,1 --points 20 --split-points 2 --parallel 10 --redefineSignalPOIs alpha -d $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/cmb/125/ws.root --algo grid -t -1 --there -n .alpha_ggFconstrain
+# Add single points into one 
+cd $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/cmb/125/
+hadd higgsCombine.alpha_ggFconstrain.MultiDimFit.mH125.root higgsCombine.alpha_ggFconstrain.POINTS.*.MultiDimFit.mH125.root
+# plot the  
+cd $CMSSW_BASE/src/
+python $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/scripts/plot1DScan.py --main="$CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/RWTH_dijet_mela3D_20180820_newOutputs_nocache/cmb/125/higgsCombine.alpha_ggFconstrain.MultiDimFit.mH125.root" --POI=alpha --output="CombineHarvester/HTTSMCP2016/output/RWTH_dijet_mela3D_20180820_newOutputs_nocache/cmb/125/alpha_ggFconstrain" --no-numbers  --no-box --x_title='#alpha_{t}(#frac{#pi}{2})' --y-max=17.0 --use-html-colors --title="#alpha_{t}(#mu_F=1)"
--- scripts/alpha_scans.sh
+++ scripts/alpha_scans.sh
@@ -5,6 +5,18 @@ combineTool.py -M T2W -P CombineHarvester.CombinePdfs.CPMixture:CPMixture -i $CM
 
 # alpha scan
 combineTool.py -m 125 -M MultiDimFit --setParameters muF=1,muV=1,alpha=0,f=0 --freezeParameters f --setParameterRanges alpha=0,1 --points 20 --split-points 2 --redefineSignalPOIs alpha -d $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/{{cmb,em,et,mt,tt}}/125/ws.root --algo grid -t -1 --there -n .alpha --parallel=10
-combineTool.py -m 125 -M MultiDimFit --setParameters muF=1,muV=1,alpha=0,f=0 --setParameterRanges alpha=0,1 --points 20 --split-points 2 --redefineSignalPOIs alpha -d $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/{{cmb,em,et,mt,tt}}/125/ws.root --algo grid -t -1 --there -n .alpha_f_floating --parallel=10
+cd $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/cmb/125/
+hadd higgsCombine.alpha.MultiDimFit.mH125.root higgsCombine.alpha.POINTS.*.MultiDimFit.mH125.root
+cd $CMSSW_BASE/src
+combineTool.py -m 125 -M MultiDimFit --setParameters muF=1,muV=1,alpha=0,f=0 --setParameterRanges alpha=0,1 --points 10 --split-points 1 --redefineSignalPOIs alpha -d $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/{{cmb,em,et,mt,tt}}/125/ws.root --algo grid -t -1 --there -n .alpha_f_floating --parallel=10
+cd $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/cmb/125/
+hadd higgsCombine.alpha_f_floating.MultiDimFit.mH125.root higgsCombine.alpha_f_floating.POINTS.*.MultiDimFit.mH125.root
+cd $CMSSW_BASE/src
 combineTool.py -m 125 -M MultiDimFit --setParameters muF=1,muV=1,alpha=0,f=0 --freezeParameters f --setParameterRanges muF=0,4 --points 20  --split-points 2 --redefineSignalPOIs muF -d $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/{{cmb,em,et,mt,tt}}/125/ws.root --algo grid -t -1 --there -n .muF --parallel=10
+cd $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/cmb/125/
+hadd higgsCombine.alpha.MultiDimFit.mH125.root higgsCombine.alpha.POINTS.*.MultiDimFit.mH125.root
+cd $CMSSW_BASE/src
 combineTool.py -m 125 -M MultiDimFit --setParameters muF=1,muV=1,alpha=0,f=0 --freezeParameters f --setParameterRanges alpha=0,1 --redefineSignalPOIs alpha,muF -d $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/{{cmb,em,et,mt,tt}}/125/ws.root --points 500 --split-points 50 --algo grid -t -1 --there -n .2DScan --parallel=10
+cd $CMSSW_BASE/src/CombineHarvester/HTTSMCP2016/output/$1/cmb/125/
+hadd higgsCombine.2DScan.MultiDimFit.mH125.root higgsCombine.2DScan.POINTS.*.MultiDimFit.mH125.root
+cd $CMSSW_BASE/src
