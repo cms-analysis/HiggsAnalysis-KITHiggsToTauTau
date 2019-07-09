e7ab10cef17a386ef4d07260c4f86a69f909ae58
jordy <jordy.degens@rwth-aachen.de>
2019-07-09 11:50:28 +0200

    jets corrected in kappa now, not needed for 2017

--- python/ArtusConfigs/Run2CPStudies/et.py
+++ python/ArtusConfigs/Run2CPStudies/et.py
@@ -76,8 +76,8 @@ class et_ArtusConfig(dict):
 				"producer:PolarisationQuantitiesSimpleFitProducer",
 				"producer:GenMatchedPolarisationQuantitiesProducer"
 			]
-			#if re.search("(Spring16|Summer16|Run2016|Embedding2016)",nickname):
-			self["Processors"] += ["producer:TaggedJetCorrectionsProducer"] #already applied in kappa for 2017 i believe, TODO in next skim they will be
+			if re.search("(Spring16|Summer16|Run2016|Embedding2016)",nickname):
+				self["Processors"] += ["producer:TaggedJetCorrectionsProducer"] #already applied in kappa for 2017 i believe, TODO in next skim they will be
 
 			self["Processors"] += ["producer:GroupedJetUncertaintyShiftProducer"] #TaggedJetUncertaintyShiftProducer is old
 			if not re.search("(LFV).*(?=(Spring16|Summer16))", nickname): self["Processors"] += ["producer:MELAProducer"]
