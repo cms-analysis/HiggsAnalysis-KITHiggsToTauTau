dde1a7b5b1f90f39cec4671a2f7db45c02377b5e
Lucas Wiens <wiens@physik.rwth-aachen.de>
2019-07-15 18:34:14 +0200

    Add a new quantities file for CP Final State spefific quantities
    To have a cleaner setup with files that are managable in size, a new
    quantities.py specific for the Final State Analysis has been added.

---
+++ python/ArtusConfigs/Run2CPFinalStateStudies/quantities.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import Artus.Utility.logger as logger
+log = logging.getLogger(__name__)
+
+import re
+
+import HiggsAnalysis.KITHiggsToTauTau.ArtusConfigs.Run2CPStudies.quantities as Run2CPQuantities
+
+class Quantities(Run2CPQuantities.Quantities):
+
+	def __init__(self):
+		Run2CPQuantities.Quantities.__init__(self)
+		# self.quantities = set()
+		# quantities = {"Quantities" : set()}
+		# self.quantities = set()
+
+	def build_quantities(self, nickname, channel):
+
+		#super(Quantities, self).build_quantities(nickname, channel)
+
+		print "build_quantities"
+		print "\n\n\n\n\n\n\n\n\n HALLO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+		self.quantities.update(self.recoCPFinalStateQuantities())
+		if channel == "GEN":
+			pass
+		else:
+			if re.search('(DY.?JetsToLL).*(?=(Spring16|Summer16|Summer17|Fall17))', nickname):
+				self.quantities.update(self.recoCPQuantities(melaQuantities=False))
+		# ************ datasets(groups, samples) common across all except mm channels are all the rest
+			else:
+				if not channel == "MM" and re.search('(HTo.*TauTau|H2JetsToTauTau|Higgs|JJHiggs).*(?=(Spring16|Summer16|Summer17|Fall17))', nickname):
+					self.quantities.update(self.recoCPQuantities(melaQuantities=True))
+
+	@classmethod
+	def recoCPFinalStateQuantities(klass, melaQuantities=True):
+
+		s = klass.recoCPQuantities(melaQuantities)
+
+		s += [
+			"helixQOverP_1",
+			"helixLambda_1",
+			"helixPhi_1",
+			"helixDxy_1",
+			"helixDsz_1",
+
+			"helixQOverP_2",
+			"helixLambda_2",
+			"helixPhi_2",
+			"helixDxy_2",
+			"helixDsz_2",
+			]
+		return s
