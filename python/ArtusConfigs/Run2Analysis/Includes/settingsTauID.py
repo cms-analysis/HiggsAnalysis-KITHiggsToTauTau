39216ae115af58a08b17395ed4851602f005525a
Michal Bluj <michal.bluj@cern.ch>
2019-11-28 12:54:53 +0100

    Adjust DM requirements with required tauID

--- python/ArtusConfigs/Run2Analysis/Includes/settingsTauID.py
+++ python/ArtusConfigs/Run2Analysis/Includes/settingsTauID.py
@@ -14,14 +14,16 @@ class Tau_ID(dict):
 		if re.search("(Run2017|Fall17|Embedding2017)", nickname):
 			self["TauDiscriminatorIsolationName"] = "byDeepTau2017v2p1VSjetraw"
 			self["TauIDType"] = "deepTau"
+			self["TauAllowedDMs"] = [0,1,2,10,11] # exclude DM 5 and 6
+			self["TauUseOldDMs"] = False
 			self["TauDiscriminators"] = {"byVVVLooseDeepTau2017v2p1VSjet", "byVVVLooseDeepTau2017v2p1VSe", "byVLooseDeepTau2017v2p1VSmu"}
 		else:
 			self["TauDiscriminatorIsolationName"] = "byIsolationMVArun2v1DBoldDMwLTraw"
 			self["TauIDType"] = ""
+			self["TauAllowedDMs"] = [0,1,2,10] # old DMs
+			self["TauUseOldDMs"] = True
 
 		self["TauElectronLowerDeltaRCut"] = -1.0
 		self["TauMuonLowerDeltaRCut"] = -1.0
 
 		self["TauID"] = "TauIDRecommendation13TeV"
-		self["TauAllowedDMs"] = [0,1,2,10,11] # exclude DM 5 and 6
-		self["TauUseOldDMs"] = False
