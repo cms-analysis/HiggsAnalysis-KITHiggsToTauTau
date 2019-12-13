267ad1c23b9d1fb71f54658a1a74f08bc5bb1fc9
Lucas Wiens <lucas.wiens@rwth-aachen.de>
2019-12-13 15:42:14 +0100

    Restructure and complete the IsomorphicMappingProducer
    Old calibration curves are deleted and new ones are added.
    The code now handles the calibration for IPs with refitted PV and
    refitted PV with BS. Other cases are ignored.

--- interface/Producers/IsomorphicMappingProducer.h
+++ interface/Producers/IsomorphicMappingProducer.h
@@ -14,51 +14,129 @@
 class IsomorphicMappingProducer : public ProducerBase<HttTypes> {
 	private:
 		bool m_isData;
+		bool m_isEmbedding;
+		std::string m_year;
+		std::string m_decayChannel;
+		std::string m_emb;
 
-		TGraph* ip_1_x_isomap = nullptr;
-		TGraph* ip_1_y_isomap = nullptr;
-		TGraph* ip_1_z_isomap = nullptr;
-		// Do this better
-		TGraph* ip_1_r_isomap = nullptr;
-		TGraph* ip_1_theta_isomap = nullptr;
-		TGraph* ip_1_phi_isomap = nullptr;
-		TGraph* ip_2_r_isomap = nullptr;
-		TGraph* ip_2_theta_isomap = nullptr;
-		TGraph* ip_2_phi_isomap = nullptr;
-		TGraph* iprPV_1_r_isomap = nullptr;
-		TGraph* iprPV_1_theta_isomap = nullptr;
-		TGraph* iprPV_1_phi_isomap = nullptr;
-		TGraph* iprPV_2_r_isomap = nullptr;
-		TGraph* iprPV_2_theta_isomap = nullptr;
-		TGraph* iprPV_2_phi_isomap = nullptr;
-		TGraph* iprPVBS_1_r_isomap = nullptr;
-		TGraph* iprPVBS_1_theta_isomap = nullptr;
-		TGraph* iprPVBS_1_phi_isomap = nullptr;
-		TGraph* iprPVBS_2_r_isomap = nullptr;
-		TGraph* iprPVBS_2_theta_isomap = nullptr;
-		TGraph* iprPVBS_2_phi_isomap = nullptr;
+		TGraph* m_ipHelrPV_r_prompt_isomap = nullptr;
+		TGraph* m_ipHelrPV_theta_prompt_isomap = nullptr;
+		TGraph* m_ipHelrPV_phi_prompt_isomap = nullptr;
+		TGraph* m_ipHelrPV_x_prompt_isomap = nullptr;
+		TGraph* m_ipHelrPV_y_prompt_isomap = nullptr;
+		TGraph* m_ipHelrPV_z_prompt_isomap = nullptr;
 
-		TGraph* ipHel_1_r_isomap = nullptr;
-		TGraph* ipHel_1_theta_isomap = nullptr;
-		TGraph* ipHel_1_phi_isomap = nullptr;
-		TGraph* ipHel_2_r_isomap = nullptr;
-		TGraph* ipHel_2_theta_isomap = nullptr;
-		TGraph* ipHel_2_phi_isomap = nullptr;
-		TGraph* ipHelrPV_1_r_isomap = nullptr;
-		TGraph* ipHelrPV_1_theta_isomap = nullptr;
-		TGraph* ipHelrPV_1_phi_isomap = nullptr;
-		TGraph* ipHelrPV_2_r_isomap = nullptr;
-		TGraph* ipHelrPV_2_theta_isomap = nullptr;
-		TGraph* ipHelrPV_2_phi_isomap = nullptr;
-		TGraph* ipHelrPVBS_1_r_isomap = nullptr;
-		TGraph* ipHelrPVBS_1_theta_isomap = nullptr;
-		TGraph* ipHelrPVBS_1_phi_isomap = nullptr;
-		TGraph* ipHelrPVBS_2_r_isomap = nullptr;
-		TGraph* ipHelrPVBS_2_theta_isomap = nullptr;
-		TGraph* ipHelrPVBS_2_phi_isomap = nullptr;
+		TGraph* m_ipHelrPV_r_muon_isomap = nullptr;
+		TGraph* m_ipHelrPV_theta_muon_isomap = nullptr;
+		TGraph* m_ipHelrPV_phi_muon_isomap = nullptr;
+		TGraph* m_ipHelrPV_x_muon_isomap = nullptr;
+		TGraph* m_ipHelrPV_y_muon_isomap = nullptr;
+		TGraph* m_ipHelrPV_z_muon_isomap = nullptr;
 
+		TGraph* m_ipHelrPV_r_pion_isomap = nullptr;
+		TGraph* m_ipHelrPV_theta_pion_isomap = nullptr;
+		TGraph* m_ipHelrPV_phi_pion_isomap = nullptr;
+		TGraph* m_ipHelrPV_x_pion_isomap = nullptr;
+		TGraph* m_ipHelrPV_y_pion_isomap = nullptr;
+		TGraph* m_ipHelrPV_z_pion_isomap = nullptr;
+
+		TGraph* m_ipHelrPVBS_r_prompt_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_theta_prompt_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_phi_prompt_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_x_prompt_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_y_prompt_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_z_prompt_isomap = nullptr;
+
+		TGraph* m_ipHelrPVBS_r_muon_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_theta_muon_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_phi_muon_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_x_muon_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_y_muon_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_z_muon_isomap = nullptr;
+
+		TGraph* m_ipHelrPVBS_r_pion_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_theta_pion_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_phi_pion_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_x_pion_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_y_pion_isomap = nullptr;
+		TGraph* m_ipHelrPVBS_z_pion_isomap = nullptr;
 
 	public:
+		TVector3 CalibrateIPHelrPVBS(TVector3 IP, int gen_match, bool isMuon) const {
+			TVector3 calibIP;
+			if(gen_match == 2 || gen_match == 1){
+				calibIP.SetMagThetaPhi(m_ipHelrPVBS_r_prompt_isomap->Eval(IP.Mag()),
+							m_ipHelrPVBS_theta_prompt_isomap->Eval(IP.Theta()),
+							m_ipHelrPVBS_phi_prompt_isomap->Eval(IP.Phi())
+				);
+			} else if ( (gen_match != 6) && (gen_match > 2) && isMuon ) {
+				calibIP.SetMagThetaPhi(m_ipHelrPVBS_r_muon_isomap->Eval(IP.Mag()),
+							m_ipHelrPVBS_theta_muon_isomap->Eval(IP.Theta()),
+							m_ipHelrPVBS_phi_muon_isomap->Eval(IP.Phi())
+				);
+			} else if ( (gen_match != 6) && (gen_match > 2) && !isMuon ) {
+				calibIP.SetMagThetaPhi(m_ipHelrPVBS_r_pion_isomap->Eval(IP.Mag()),
+							m_ipHelrPVBS_theta_pion_isomap->Eval(IP.Theta()),
+							m_ipHelrPVBS_phi_pion_isomap->Eval(IP.Phi())
+				);
+			} else {
+
+				calibIP.SetMagThetaPhi(IP.Mag(),
+							IP.Theta(),
+							IP.Phi()
+				);
+			}
+			return calibIP;
+		}
+
+		const TVector3 CalibrateIPHelrPV(TVector3 IP, int gen_match, bool isMuon) const {
+			TVector3 calibIP;
+			if(gen_match == 2 || gen_match == 1){
+				calibIP.SetMagThetaPhi(m_ipHelrPV_r_prompt_isomap->Eval(IP.Mag()),
+							m_ipHelrPV_theta_prompt_isomap->Eval(IP.Theta()),
+							m_ipHelrPV_phi_prompt_isomap->Eval(IP.Phi())
+				);
+			} else if ( (gen_match != 6) && (gen_match > 2) && isMuon ) {
+				calibIP.SetMagThetaPhi(m_ipHelrPV_r_muon_isomap->Eval(IP.Mag()),
+							m_ipHelrPV_theta_muon_isomap->Eval(IP.Theta()),
+							m_ipHelrPV_phi_muon_isomap->Eval(IP.Phi())
+				);
+			} else if ( (gen_match != 6) && (gen_match > 2) && !isMuon ) {
+				calibIP.SetMagThetaPhi(m_ipHelrPV_r_pion_isomap->Eval(IP.Mag()),
+							m_ipHelrPV_theta_pion_isomap->Eval(IP.Theta()),
+							m_ipHelrPV_phi_pion_isomap->Eval(IP.Phi())
+				);
+			} else {
+
+				calibIP.SetMagThetaPhi(IP.Mag(),
+							IP.Theta(),
+							IP.Phi()
+				);
+			}
+			return calibIP;
+		}
+		/*
+		TVector3 CalibrateIP(TVector3 IP, int gen_match, TGraph* ip_r_prompt_isomap, TGraph* ip_theta_prompt_isomap, TGraph* ip_phi_prompt_isomap, TGraph* ip_r_nonprompt_isomap, TGraph* ip_theta_nonprompt_isomap, TGraph* ip_phi_nonprompt_isomap){
+			TVector3 calibIP;
+			if(gen_match == 2 || gen_match == 1){
+				calibIP.SetMagThetaPhi(ip_r_prompt_isomap->Eval(IP.Mag()),
+							ip_theta_prompt_isomap->Eval(IP.Theta()),
+							ip_phi_prompt_isomap->Eval(IP.Phi())
+				);
+			} else if ( (gen_match != 6) && (gen_match > 2) ) {
+				calibIP.SetMagThetaPhi(ip_r_nonprompt_isomap->Eval(IP.Mag()),
+							ip_theta_nonprompt_isomap->Eval(IP.Theta()),
+							ip_phi_nonprompt_isomap->Eval(IP.Phi())
+				);
+			} else {
+				calibIP.SetMagThetaPhi(IP.Mag(),
+							IP.Theta(),
+							IP.Phi()
+				);
+			}
+			return calibIP;
+		}
+		*/
 
 		virtual std::string GetProducerId() const override;
 
